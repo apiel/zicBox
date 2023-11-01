f464a58bcc2cbd893afb04fba7d02ecab8cfcd78
Alexandre Piel <alexandre.piel@zooplus.com>
2023-11-01 16:37:21 +0100

    if statement

--- helpers/script.h
+++ helpers/script.h
@@ -6,6 +6,7 @@
 #include "mathParser.h"
 #include "trim.h"
 
+#include <stdexcept>
 #include <stdio.h> // printf
 #include <vector>
 
@@ -81,16 +82,48 @@ std::vector<string> getParams(char* paramsStr)
     return params;
 }
 
+void defaultCallback(char* key, std::vector<string> params, const char* filename, void (*callback)(char* key, std::vector<string> params, const char* filename))
+{
+    if (strcmp(key, "if") == 0) {
+        if (params.size() != 3) {
+            throw std::runtime_error("Invalid if statement, required 3 paremeters: if: $var1, =, $var3");
+        }
+
+        bool result = false;
+        if (params[1] == "==") {
+            result = params[0] == params[2];
+        } else if (params[1] == "!=") {
+            result = params[0] != params[2];
+        } else if (params[1] == ">") {
+            result = stod(params[0]) > stod(params[2]);
+        } else if (params[1] == "<") {
+            result = stod(params[0]) < stod(params[2]);
+        } else if (params[1] == ">=") {
+            result = stod(params[0]) >= stod(params[2]);
+        } else if (params[1] == "<=") {
+            result = stod(params[0]) <= stod(params[2]);
+        } else {
+            throw std::runtime_error("Invalid if statement operator: " + params[1]);
+        }
+
+        printf("------ if %s %s %s = %s\n", params[0].c_str(), params[1].c_str(), params[2].c_str(), result ? "true" : "false");
+
+        return;
+    }
+    callback(key, params, filename);
+}
+
 void parseScriptLine(char* line, const char* filename, void (*callback)(char* key, std::vector<string> params, const char* filename))
 {
+    uint16_t indentation = countLeadingChar(line, ' ');
+    line = line + indentation; // remove leading spaces
+
     // ignore comments and empty lines
     if (line[0] == '#' || line[0] == '\n') {
         return;
     }
 
     line = rtrim(line, '\n');
-    uint16_t indentation = countLeadingChar(line, ' ');
-    line = line + indentation; // remove leading spaces
 
     if (line[0] == '$') {
         setVariable(line);
@@ -103,7 +136,8 @@ void parseScriptLine(char* line, const char* filename, void (*callback)(char* ke
     }
 
     char* paramsStr = strtok(NULL, ":");
-    callback(key, getParams(paramsStr), filename);
+    std::vector<string> params = getParams(paramsStr);
+    defaultCallback(key, params, filename, callback);
 }
 
 }
