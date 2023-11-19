6c245c8ed63c5fe0eaf725120e7dcce1be11f0c4
Alexandre Piel <alexandre.piel@zooplus.com>
2023-11-19 20:03:44 +0100

    serialize

--- plugins/audio/utils/ValSerializeSndFile.h
+++ plugins/audio/utils/ValSerializeSndFile.h
@@ -56,8 +56,6 @@ public:
         }
         uint32_t chunkSize = findChunk(file);
 
-        printf("::::::::::::: chunkSize: %d\n", chunkSize);
-
         if (chunkSize) {
             fread(serialize, chunkSize, 1, file);
 
@@ -70,25 +68,9 @@ public:
         fclose(file);
     }
 
-    void saveSetting(FILE* file)
-    {
-        fwrite(VAL_SERIALIZE_CHUNK_ID, 4, 1, file);
-
-        // uint32_t serializeSize = sizeof(serialize);
-        uint32_t serializeSize = mapping.size() * sizeof(ValSerialize);
-        fwrite(&serializeSize, 4, 1, file);
-        fwrite(serialize, serializeSize, 1, file);
-
-        // if serializeSize is not multiple of 4 bytes then add missing bytes
-        uint32_t padding = 4 - (serializeSize % 4);
-        if (padding != 4) {
-            fwrite(&padding, 1, padding, file);
-        }
-    }
-
     void saveSetting(const char* filename)
     {
-        FILE* file = fopen(filename, "ab");
+        FILE* file = fopen(filename, "rb");
         if (!file) {
             printf("Error: could not open file %s for writing\n", filename);
             return;
@@ -101,35 +83,46 @@ public:
 
         uint32_t chunkSize = findChunk(file);
 
-        printf("::::::::::::: chunkSize: %d\n", chunkSize);
-        if (chunkSize) {
-            printf(":::::::::::::copy\n");
-            // if file exist make a copy
-            char tmpFilename[256];
-            sprintf(tmpFilename, "%s.tmp", filename);
-            FILE* tmpFile = fopen(tmpFilename, "wb");
+        long chunkPos = ftell(file);
 
-            long pos = ftell(file);
+        char tmpFilename[256];
+        sprintf(tmpFilename, "%s.tmp", filename);
+        FILE* tmpFile = fopen(tmpFilename, "wb");
+
+        fseek(file, 0, SEEK_SET);
+        for (long i = 0; i < chunkPos; i++) {
             char c;
-            for (long i = 0; i < pos; i++) {
-                fread(&c, 1, 1, file);
-                fwrite(&c, 1, 1, tmpFile);
-            }
+            fread(&c, 1, 1, file);
+            fwrite(&c, 1, 1, tmpFile);
+        }
 
-            saveSetting(tmpFile);
-            
-            fseek(file, chunkSize, SEEK_CUR);
+        uint32_t serializeSize = mapping.size() * sizeof(ValSerialize);
+        if (!chunkSize) {
+            fwrite(VAL_SERIALIZE_CHUNK_ID, 4, 1, tmpFile);
+            fwrite(&serializeSize, 4, 1, tmpFile);
+        }
+        fwrite(serialize, serializeSize, 1, tmpFile);
+        // if serializeSize is not multiple of 4 bytes then add missing bytes
+        uint32_t padding = 4 - (serializeSize % 4);
+        if (padding != 4) {
+            fwrite(&padding, 1, padding, tmpFile);
+        }
+
+        if (chunkSize) {
+            // could this be a problem if padding is different than 4?
+            // maybe padding should saved in serializeSize...
+            fseek(file, chunkPos, SEEK_CUR);
+            char c;
             while (fread(&c, 1, 1, file)) {
                 fwrite(&c, 1, 1, tmpFile);
             }
-
-            fclose(tmpFile);
-        } else {
-            fseek(file, 0, SEEK_END);
-            saveSetting(file);
         }
 
+        fclose(tmpFile);
         fclose(file);
+
+        remove(filename);
+        rename(tmpFilename, filename);
     }
 };
 
