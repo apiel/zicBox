15f9a1373af4c66e5fa622140e7e4d8b33717574
Alexandre Piel <alexandre.piel@zooplus.com>
2023-12-07 17:55:26 +0100

    fix type

--- waveshare/Config/dev_hardware_SPI.c
+++ waveshare/Config/dev_hardware_SPI.c
@@ -90,7 +90,7 @@ void DEV_HARDWARE_SPI_begin(char *SPI_device)
     }
     tr.bits_per_word = bits;
     
-    DEV_HARDWARE_SPI_Mode(SPI_MODE_0);
+    DEV_HARDWARE_SPI_Mode((SPIMode)SPI_MODE_0);
     DEV_HARDWARE_SPI_ChipSelect(SPI_CS_Mode_LOW);
     DEV_HARDWARE_SPI_SetBitOrder(SPI_BIT_ORDER_LSBFIRST);
     DEV_HARDWARE_SPI_setSpeed(20000000);
