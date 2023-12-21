a5ad2ff9eb9ce7726d0f9723270244047068e76d
Alexandre Piel <alexandre.piel@zooplus.com>
2023-12-21 19:25:08 +0100

    wip neo controller

---
+++ plugins/controllers/NeotrellisController.h
@@ -0,0 +1,120 @@
+#ifndef _NEO_CONTROLLER_H_
+#define _NEO_CONTROLLER_H_
+
+#include "controllerInterface.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include <errno.h> // Error integer and strerror() function
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+
+#include <thread>
+
+class NeotrellisController : public ControllerInterface {
+protected:
+    struct termios tty;
+    int port;
+    std::thread readThread;
+    bool loop = false;
+
+    void setAttributes()
+    {
+        tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+        tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+        tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
+        tty.c_cflag |= CS8; // 8 bits per byte (most common)
+        //   tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+        tty.c_lflag &= ~ICANON;
+        tty.c_lflag &= ~ECHO; // Disable echo
+        tty.c_lflag &= ~ECHOE; // Disable erasure
+        tty.c_lflag &= ~ECHONL; // Disable new-line echo
+        tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+        tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL); // Disable any special handling of received bytes
+
+        tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+        tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+
+        tty.c_cc[VMIN] = 1;
+        tty.c_cc[VTIME] = 5;
+    }
+
+    void readLoop()
+    {
+        loop = true;
+        while (loop) {
+            // read 1 byte from serial port
+            uint8_t byte;
+            int bytesRead = read(port, &byte, 1);
+            if (bytesRead == 1) {
+                if (byte == '$') {
+                    uint8_t key = read(port, &byte, 1);
+                    printf("pressed: %d\n", key);
+                } else if (byte == '!') {
+                    uint8_t key = read(port, &byte, 1);
+                    printf("released: %d\n", key);
+                } else {
+                    printf("unknown: %d\n", byte);
+                }
+            }
+        }
+    }
+
+    void openSerial(char* path)
+    {
+        // "/dev/ttyACM0"
+        port = open(path, O_RDWR | O_NOCTTY);
+
+        // Read in existing settings, and handle any error
+        if (tcgetattr(port, &tty) != 0) {
+            printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+            return;
+        }
+
+        setAttributes();
+        cfsetispeed(&tty, B115200);
+        cfsetospeed(&tty, B115200);
+        // cfsetispeed(&tty, B921600);
+        // cfsetospeed(&tty, B921600);
+
+        if (tcsetattr(port, TCSANOW, &tty) != 0) {
+            printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+            return;
+        }
+
+        readThread = std::thread(&NeotrellisController::readLoop, this);
+    }
+
+public:
+    NeotrellisController(Props& props)
+        : ControllerInterface(props)
+    {
+    }
+
+    ~NeotrellisController()
+    {
+        if (loop && readThread.joinable()) {
+            loop = false;
+            readThread.join();
+        }
+        if (port >= 0) {
+            close(port);
+        }
+    }
+
+    bool config(char* key, char* value)
+    {
+        if (strcmp(key, "OPEN") == 0) {
+            openSerial(value);
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif
