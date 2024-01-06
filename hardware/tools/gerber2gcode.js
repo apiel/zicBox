6b3e0d16a07513fc9bbd35fe6287338c07c54be6
Alexandre Piel <alexandre.piel@zooplus.com>
2024-01-06 21:34:27 +0100

    gcode to file

--- hardware/tools/gerber2gcode.js
+++ hardware/tools/gerber2gcode.js
@@ -9,14 +9,26 @@ const gerber = require('fs').readFileSync(file, 'utf8');
 const speed = process.argv[3] || 100;
 const passes = process.argv[4] || 1;
 
+let out = console.log;
+let outRelease = () => {};
+
+if (process.env.OUTPUT !== 'console') {
+    const outputFilepath = file.replace(/\.[^/.]+$/, '') + `_F${speed}_x${passes}.gcode`;
+    const outputFile = require('fs').createWriteStream(outputFilepath);
+    out = (line) => outputFile.write(line + '\n');
+    outRelease = () => {
+        outputFile.end();
+    };
+}
+
 // %MOIN*% -> G20
 // %MOMM*% -> G21
 // %FSLAX36Y36*% -> format X 3.6 Y 3.6
 
-console.log('M3; Constant Power Laser On');
+out('M3; Constant Power Laser On');
 
 // set speed
-console.log(`F${speed}`);
+out(`F${speed}`);
 
 const Zsafe = 'Z10';
 const ZlaserOn = 'Z0';
@@ -30,14 +42,14 @@ const lines = gerber.split('\n');
 
 for (let counter = 0; counter < passes; counter++) {
     if (counter) {
-        console.log(`; Pass ${counter + 1}`);
+        out(`; Pass ${counter + 1}`);
     }
 
     for (const line of lines) {
         if (line === '%MOIN*%') {
-            console.log('G20');
+            out('G20');
         } else if (line === '%MOMM*%') {
-            console.log('G21');
+            out('G21');
         } else if (line.startsWith('%FSLAX')) {
             // %FSLAX36Y36*%
             const [x, y] = line.substring(6, line.length - 2).split('Y');
@@ -45,11 +57,11 @@ for (let counter = 0; counter < passes; counter++) {
             ratioX = Number((0.1 ** xDecimal).toFixed(xDecimal)); // ** = Math.pow = exponentiation operator
             const yDecimal = Number(y[1]);
             ratioY = Number((0.1 ** yDecimal).toFixed(yDecimal));
-            // console.log({ ratioX, ratioY });
+            // out({ ratioX, ratioY });
         } else if (line.endsWith('D02*')) {
             // X4975000Y2200000D02*
             const [x, y] = line.substring(1, line.length - 4).split('Y');
-            console.log(`\nG0 X${x * ratioX} Y${y * ratioY} ${Zsafe}`);
+            out(`\nG0 X${x * ratioX} Y${y * ratioY} ${Zsafe}`);
         } else if (line.endsWith('D01*')) {
             // X4975000Y475000D01*
             // or
@@ -58,14 +70,14 @@ for (let counter = 0; counter < passes; counter++) {
             const [x, restY = ''] = l.substring(1, l.length - 4).split('Y');
             const [y, restI = ''] = restY.split('I');
             const [i, j = ''] = restI.split('J');
-            // console.log({ line, x, y, i, j });
+            // out({ line, x, y, i, j });
             if (i !== '' && j !== '') {
-                console.log(
-                    `G3 X${x * ratioX} Y${y * ratioY} I${i * ratioX} J${j * ratioY} ${ZlaserOn}`
-                );
+                out(`G3 X${x * ratioX} Y${y * ratioY} I${i * ratioX} J${j * ratioY} ${ZlaserOn}`);
             } else {
-                console.log(`G1 X${x * ratioX} Y${y * ratioY} ${ZlaserOn}`);
+                out(`G1 X${x * ratioX} Y${y * ratioY} ${ZlaserOn}`);
             }
         }
     }
 }
+
+outRelease();
