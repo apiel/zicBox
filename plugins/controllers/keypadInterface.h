24b8dff15277b9a7585d27ecf69bfb6964975d9e
Alexandre Piel <alexandre.piel@zooplus.com>
2024-01-17 22:08:46 +0100

    move keyboard logic in keboard interface

--- plugins/controllers/keypadInterface.h
+++ plugins/controllers/keypadInterface.h
@@ -3,16 +3,56 @@
 
 #include "controllerInterface.h"
 
-class KeypadInterface: public ControllerInterface{
+class KeypadInterface : public ControllerInterface {
 public:
-    
+    struct KeyMap {
+        uint8_t key;
+        int param;
+        std::function<uint8_t(int param)> color;
+        std::function<void(int8_t state, int param)> action;
+    };
+
+    struct KeyMapLayout {
+        std::string name;
+        std::vector<KeyMap> mapping;
+    };
+
+protected:
+    KeyMapLayout& activeLayout;
+    std::vector<KeyMapLayout> layouts = {
+        { "default", {} }
+    };
+
+    void onKeyPad(int key, int8_t state)
+    {
+        for (KeyMap keyMap : activeLayout.mapping) {
+            if (keyMap.key == key) {
+                keyMap.action(state, keyMap.param);
+                return;
+            }
+        }
+
+        printf("unhandled key %d\n", key);
+    }
+
+public:
     KeypadInterface(ControllerInterface::Props& props)
         : ControllerInterface(props)
+        , activeLayout(layouts[0])
     {
     }
 
-    virtual void setKeyColor(int id, uint8_t color) = 0;
     virtual void setButton(int id, uint8_t color) = 0;
+
+    void render()
+    {
+        // TODO instead to do this should just set the one missing from the list...
+        setButton(254, 254); // set all button off
+
+        for (KeyMap keyMap : activeLayout.mapping) {
+            setButton(keyMap.key, keyMap.color(keyMap.param));
+        }
+    }
 };
 
 #endif
