8b305092bc6a90d0d22ef0be9fd9366aa38ac3a7
Alexandre Piel <alexandre.piel@zooplus.com>
2024-01-27 20:16:35 +0100

    use libpd

--- plugins/audio/SynthPd.h
+++ plugins/audio/SynthPd.h
@@ -4,8 +4,8 @@
 #include "audioPlugin.h"
 #include "mapping.h"
 
-#include <z_libpd.h>
 #include <libgen.h>
+#include <z_libpd.h>
 
 /*md
 ## SynthPd
@@ -15,6 +15,15 @@ SynthPd is a plugin to play puredata patches.
 > [!NOTE]
 > - `TODO` process audio input, e.g. apply effect using PD
 */
+
+void pdprint(const char* s)
+{
+    if (s[0] == '\n') {
+        return;
+    }
+    printf("[PD print] %s\n", s);
+}
+
 class SynthPd : public Mapping {
 protected:
 public:
@@ -23,9 +32,15 @@ public:
     SynthPd(AudioPlugin::Props& props, char* _name)
         : Mapping(props, _name)
     {
+        libpd_set_printhook(pdprint);
         libpd_init();
         // 0 is input channel but ultimately we could also use pd to process input audio
         libpd_init_audio(0, props.channels, props.sampleRate);
+
+        // compute audio    [; pd dsp 1(
+        libpd_start_message(1); // one entry in list
+        libpd_add_float(1.0f);
+        libpd_finish_message("pd", "dsp");
 
         initValues();
     }
@@ -35,25 +50,30 @@ public:
     {
         /*md - `OPEN_PD` open puredata patch. */
         if (strcmp(key, "OPEN_PD") == 0) {
-            char *filename = basename(value);
-            char *folder = dirname(value);
+            char* filename = basename(value);
+            char* folder = dirname(value);
 
-            printf("opening %s in %s\n", filename, folder);
-
-            // if (!libpd_openfile(argv[1], argv[2])) {
-            //     debug("Error: could not open puredata file %s\n", filename);
-            // }
+            debug("[PD] Opening %s in %s\n", filename, folder);
+            if (!libpd_openfile(filename, folder)) {
+                debug("!!!! Error: could not open puredata file %s\n", filename);
+            }
             return true;
         }
 
         return AudioPlugin::config(key, value);
     }
 
+    uint8_t outbufPos = -1;
+    float outbuf[128];
     void sample(float* buf)
     {
-        float out = 0.0f;
+        if (outbufPos > 128) {
+            libpd_process_float(1, NULL, outbuf);
+            outbufPos = 0;
+        }
 
-        buf[track] = out;
+        buf[track] = outbuf[outbufPos];
+        outbufPos++;
     }
 
     void noteOn(uint8_t note, float velocity) override
@@ -61,10 +81,14 @@ public:
         if (velocity == 0) {
             return noteOff(note, velocity);
         }
+        debug("[PD] noteOn: %d %f\n", note, velocity);
+
+        libpd_noteon(1, note, velocity * 127);
     }
 
     void noteOff(uint8_t note, float velocity) override
     {
+        libpd_noteon(1, note, 0);
     }
 };
 
