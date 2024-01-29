72b7f59cf5d89bf0678e65439524b0b938a1de7d
Alexandre Piel <alexandre.piel@zooplus.com>
2024-01-29 22:06:50 +0100

    use controller id for mapping

--- plugins/components/base/KeypadLayout.h
+++ plugins/components/base/KeypadLayout.h
@@ -19,36 +19,38 @@ public:
     std::string name = "default";
 
     struct KeyMap {
+        uint16_t controllerId;
         uint8_t key;
         int param;
+        std::function<void(int8_t state, KeyMap& keymap)> action;
         uint8_t color;
         std::function<uint8_t(KeyMap& keymap)> getColor;
-        std::function<void(int8_t state, KeyMap& keymap)> action;
     };
 
 protected:
+    // FIXME move keypad ptr to KeyMap
     KeypadInterface* keypad;
-    std::function<void(int8_t state, int param, std::string action, uint8_t color)> addKeyMap;
+    std::function<void(uint16_t controllerId, int8_t state, int param, std::string action, uint8_t color)> addKeyMap;
 
 public:
     std::vector<KeyMap> mapping;
 
-    KeypadLayout(KeypadInterface* keypad, std::function<void(int8_t state, int param, std::string action, uint8_t color)> addKeyMap)
+    KeypadLayout(KeypadInterface* keypad, std::function<void(uint16_t controllerId, int8_t state, int param, std::string action, uint8_t color)> addKeyMap)
         : keypad(keypad)
         , addKeyMap(addKeyMap)
     {
     }
 
-    void onKey(int key, int8_t state)
+    void onKey(uint16_t id, int key, int8_t state)
     {
         for (KeyMap keyMap : mapping) {
-            if (keyMap.key == key) {
+            if (keyMap.controllerId == id && keyMap.key == key) {
                 keyMap.action(state, keyMap);
                 return;
             }
         }
 
-        printf("unhandled key %d\n", key);
+        printf("unhandled key %d for controller %d\n", key, id);
     }
 
     // Might want to render keypad in a central place
@@ -71,21 +73,34 @@ public:
         // TODO instead to do this should just set the one missing from the list...
         // setButton(254, 254); // set all button off
         for (KeyMap keyMap : mapping) {
+            // FIXME move keypad ptr to KeyMap
+            if (keyMap.controllerId == 0)
+                continue;
+
             keypad->setButton(keyMap.key, keyMap.getColor(keyMap));
         }
     }
 
     bool config(char* key, char* value)
     {
-        /*md - `KEYMAP: key action [param] [color]` */
+        /*md - `KEYMAP: controllerName key action [param] [color]` Map an action to a controller key. Use `Keyboard` as `controllerName` to use computer keyboard. */
         if (strcmp(key, "KEYMAP") == 0) {
-            uint8_t key = atoi(strtok(value, " "));
+            std::string controllerName = strtok(value, " ");
+            uint8_t key = atoi(strtok(NULL, " "));
             std::string action = strtok(NULL, " ");
             char* paramStr = strtok(NULL, " ");
             char* colorStr = strtok(NULL, " ");
             int param = paramStr != NULL ? atoi(paramStr) : -1;
             uint8_t color = colorStr != NULL ? atoi(colorStr) : 255;
-            addKeyMap(key, param, action, color);
+
+            uint16_t controllerId = -1;
+            if (strcmp(controllerName.c_str(), "Keyboard") == 0) {
+                controllerId = 0;
+            } else {
+                // FIXME get id from list of controllers
+                controllerId = keypad->id;
+            }
+            addKeyMap(controllerId, key, param, action, color);
         }
         return false;
     }
