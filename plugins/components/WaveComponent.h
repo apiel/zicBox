51299f36e700daee2a134a349aee0e673879c81b
Alexandre Piel <alexandre.piel@zooplus.com>
2024-01-30 19:45:15 +0100

    map keyboard to play kick

--- plugins/components/WaveComponent.h
+++ plugins/components/WaveComponent.h
@@ -1,10 +1,11 @@
 #ifndef _UI_COMPONENT_WAVE_H_
 #define _UI_COMPONENT_WAVE_H_
 
-#include "component.h"
 #include <string>
-
 #include <vector>
+
+#include "./base/KeypadLayout.h"
+#include "component.h"
 
 class WaveComponent : public Component {
 protected:
@@ -39,12 +40,29 @@ protected:
 
     int lastUpdateUi = -1;
 
+    KeypadLayout keypadLayout;
+
 public:
+    /*md **Keyboard actions**: */
+    void addKeyMap(KeypadInterface* controller, uint16_t controllerId, uint8_t key, int param, std::string action, uint8_t color)
+    {
+        /*md - `play` is used to play the sample. `KEYMAP: Keyboard 44 play 60` will trigger note on 60 when pressing space on keyboard. */
+        if (action == "play") {
+            keypadLayout.mapping.push_back({ controller, controllerId, key, param, [&](int8_t state, KeypadLayout::KeyMap& keymap) {
+                    if (state) {
+                        plugin.noteOn(keymap.param, 1.0f);
+                    } else {
+                        plugin.noteOff(keymap.param, 0.0f);
+                    } }, color, [&](KeypadLayout::KeyMap& keymap) { return keymap.color || 20; } });
+        }
+    }
+
     WaveComponent(ComponentInterface::Props props)
         : Component(props)
         , colors(getColorsFromColor(styles.colors.blue))
         , margin(styles.margin)
         , plugin(getPlugin("Kick23", track))
+        , keypadLayout(getController, [&](KeypadInterface* controller, uint16_t controllerId, int8_t key, int param, std::string action, uint8_t color) { addKeyMap(controller, controllerId, key, param, action, color); })
     {
         waveSize = { size.w - 2 * margin, size.h - 2 * margin };
         wavePosition = { position.x + margin, position.y + margin };
@@ -98,7 +116,16 @@ public:
             return true;
         }
 
+        if (keypadLayout.config(key, value)) {
+            return true;
+        }
+
         return false;
+    }
+
+    void onKey(uint16_t id, int key, int8_t state)
+    {
+        keypadLayout.onKey(id, key, state);
     }
 };
 
