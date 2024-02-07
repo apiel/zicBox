c6c0b7e6cc4a9d3d3fd64b5988a641ef0371a91c
Alexandre Piel <alexandre.piel@zooplus.com>
2024-02-07 17:37:37 +0100

    fm synth

--- plugins/audio/SynthFM.h
+++ plugins/audio/SynthFM.h
@@ -23,6 +23,7 @@ class SynthFM : public Mapping {
 protected:
     struct FMoperator {
         AdsrEnvelop envelop;
+        // should it be ratio instead of frequency
         float frequency = 440.0f; // Might not even need to keep here
         float stepIncrement = 0.0f;
         float index = 0.0f;
@@ -33,17 +34,23 @@ protected:
     // could be use make sample representation for a note duration
     float bufferUi[ZIC_FM_UI];
 
-    // Envelop envelop = Envelop({ { 0.0f, 0.0f }, { 1.0f, 0.01f }, { 0.3f, 0.4f }, { 0.0f, 1.0f } });
-
 public:
+    uint8_t currentAlgorithm = 0;
+    bool algorithm[1][ZIC_FM_OPS_COUNT - 1][ZIC_FM_OPS_COUNT] = {
+        { { 0, 0, 1, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 } }
+    };
+
     /*md **Values**: */
     /*md - `PITCH` set the pitch.*/
     Val& pitch = val(0, "PITCH", { "Pitch", .min = -24, .max = 24 }, [&](auto p) { setPitch(p.value); });
 
     SynthFM(AudioPlugin::Props& props, char* _name)
         : Mapping(props, _name) // clang-format on
-        , operators({ { props.sampleRate }, { props.sampleRate }, { props.sampleRate }, { props.sampleRate } })
     {
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            operators[i].envelop.setSampleRate(props.sampleRate);
+        }
+
         initValues();
 
         // Init sine LUT
@@ -54,6 +61,41 @@ public:
 
     void sample(float* buf)
     {
+        uint8_t outDivider = 0;
+        float out = 0.0f;
+        float mod = 0.0f;
+
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            FMoperator& op = operators[i];
+            float env = op.envelop.next();
+            if (env > 0.0f) {
+                op.index += op.stepIncrement;
+                while (op.index >= ZIC_FM_LUT_SIZE)
+                {
+                    op.index -= ZIC_FM_LUT_SIZE;
+                }
+                float s = sineLut[(int)op.index] * env;
+                if (i == ZIC_FM_OPS_COUNT - 1) {
+                    outDivider++;
+                    out += s;
+                    out = out / outDivider;
+                } else {
+                    bool isMod = false;
+                    for (int j = 0; j < ZIC_FM_OPS_COUNT; j++) {
+                        if (algorithm[currentAlgorithm][i][j]) {
+                            isMod = true;
+                            mod *= s;
+                        }
+                    }
+                    if (!isMod) {
+                        outDivider++;
+                        out += s;
+                    }
+                }
+            }
+        }
+
+        buf[track] = out;
     }
 
     void setPitch(float value)
@@ -64,6 +106,18 @@ public:
 
     void noteOn(uint8_t note, float _velocity) override
     {
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            operators[i].envelop.reset();
+            operators[i].index = 0.0f;
+            operators[i].stepIncrement = ZIC_FM_LUT_SIZE * operators[i].frequency / props.sampleRate;
+        }
+    }
+
+    void noteOff(uint8_t note, float _velocity) override
+    {
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            operators[i].envelop.release();
+        }
     }
 
     void* data(int id, void* userdata = NULL)
