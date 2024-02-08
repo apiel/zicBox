d1c374ca76fcfa3daf90114ae22da6d7c9ddda2d
Alexandre Piel <alexandre.piel@zooplus.com>
2024-02-08 22:40:30 +0100

    rounded rect

--- plugins/components/RectComponent.h
+++ plugins/components/RectComponent.h
@@ -12,6 +12,8 @@ protected:
     } colors;
 
     bool drawBorder = false;
+    bool drawBackground = false;
+    int radius = 0;
 
 public:
     RectComponent(ComponentInterface::Props props)
@@ -23,22 +25,51 @@ public:
 
     void render()
     {
-        draw.filledRect(position, size, colors.background);
-        if (drawBorder) {
-            draw.rect(position, size, colors.border);
+        if (radius) {
+            if (drawBackground) {
+                draw.filledRect({ position.x + radius, position.y }, { size.w - 2 * radius, size.h }, colors.background);
+                draw.filledRect({ position.x, position.y + radius }, { size.w, size.h - 2 * radius }, colors.background);
+                draw.filledPie({ position.x + radius, position.y + radius }, radius, 180, 270, colors.background);
+                draw.filledPie({ position.x + size.w - radius, position.y + radius }, radius, 270, 360, colors.background);
+                draw.filledPie({ position.x + radius, position.y + size.h - radius }, radius, 90, 180, colors.background);
+                draw.filledPie({ position.x + size.w - radius, position.y + size.h - radius }, radius, 0, 90, colors.background);
+            }
+            if (drawBorder) {
+                draw.arc({ position.x + radius, position.y + radius }, radius, 180, 270, colors.border);
+                draw.line({ position.x, position.y + radius }, { position.x, position.y + size.h - radius }, colors.border);
+                draw.arc({ position.x + size.w - radius, position.y + radius }, radius, 270, 360, colors.border);
+                draw.line({ position.x + radius, position.y }, { position.x + size.w - radius, position.y }, colors.border);
+                draw.arc({ position.x + radius, position.y + size.h - radius }, radius, 90, 180, colors.border);
+                draw.line({ position.x + radius, position.y + size.h }, { position.x + size.w - radius, position.y + size.h }, colors.border);
+                draw.arc({ position.x + size.w - radius, position.y + size.h - radius }, radius, 0, 90, colors.border);
+                draw.line({ position.x + size.w, position.y + size.h - radius }, { position.x + size.w, position.y + radius }, colors.border);
+            }
+        } else {
+            if (drawBackground) {
+                draw.filledRect(position, size, colors.background);
+            }
+            if (drawBorder) {
+                draw.rect(position, size, colors.border);
+            }
         }
     }
 
     bool config(char* key, char* value)
     {
-        if (strcmp(key, "COLOR") == 0) {
+        if (strcmp(key, "BACKGROUND") == 0) {
             colors.background = draw.getColor(value);
+            drawBackground = true;
             return true;
         }
 
         if (strcmp(key, "BORDER") == 0) {
             colors.border = draw.getColor(value);
             drawBorder = true;
+            return true;
+        }
+
+        if (strcmp(key, "RADIUS") == 0) {
+            radius = atoi(value);
             return true;
         }
 
