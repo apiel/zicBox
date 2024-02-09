897933072d7d3c9261b4995c308aed7cfddb8d19
Alexandre Piel <alexandre.piel@zooplus.com>
2024-02-09 20:22:30 +0100

    algo

--- plugins/components/FmAlgoComponent.h
+++ plugins/components/FmAlgoComponent.h
@@ -20,7 +20,7 @@
 <img src="https://raw.githubusercontent.com/apiel/zicBox/main/plugins/components/fm/algo11.png" />
 <img src="https://raw.githubusercontent.com/apiel/zicBox/main/plugins/components/fm/algo12.png" />
 
-Show a FM algorithm and change them.
+Show a FM algorithm and change them. The larger square operator are the carrier where audio is outputted.
 */
 class FmAlgoComponent : public Component {
 protected:
