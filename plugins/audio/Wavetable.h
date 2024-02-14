06b21e40827378bab0fde932efcc175656977e2a
apiel <alexandre.piel@gmail.com>
2024-02-14 19:35:42 +0100

    move wavetable in separate file

---
+++ plugins/audio/Wavetable.h
@@ -0,0 +1,86 @@
+#ifndef _WAVETABLE_H_
+#define _WAVETABLE_H_
+
+#include <sndfile.h>
+#include <string>
+
+#include "fileBrowser.h"
+
+#define ZIC_WAVETABLE_WAVEFORMS_COUNT 64
+
+class Wavetable {
+protected:
+    SF_INFO sfinfo;
+    SNDFILE* file = NULL;
+
+    uint64_t sampleCount = 2048; // should this be configurable?
+    static const uint64_t bufferSize = ZIC_WAVETABLE_WAVEFORMS_COUNT * 2048;
+    uint64_t bufferSampleCount = 0;
+    float bufferSamples[bufferSize];
+
+    uint64_t sampleStart = 0;
+
+public:
+    float sampleIndex = 0.0f;
+    float pitchMult = 1.0f;
+    FileBrowser fileBrowser = FileBrowser("./wavetables");
+
+    Wavetable()
+    {
+        memset(&sfinfo, 0, sizeof(sfinfo));
+        open(0, true);
+    }
+
+    float sample(float time, float* index, float amp, float freq)
+    {
+        (*index) += pitchMult * freq;
+        while ((*index) >= sampleCount) {
+            (*index) -= sampleCount;
+        }
+        return bufferSamples[(uint16_t)(*index) + sampleStart] * amp;
+    }
+
+    void close()
+    {
+        if (file) {
+            sf_close(file);
+        }
+    }
+
+    void open(std::string filename)
+    {
+        close();
+
+        if (!(file = sf_open(filename.c_str(), SFM_READ, &sfinfo))) {
+            printf("Error: could not open file %s\n", filename.c_str());
+        }
+        printf("Audio file %s sampleCount %ld sampleRate %d\n", filename.c_str(), (long)sfinfo.frames, sfinfo.samplerate);
+
+        bufferSampleCount = sf_read_float(file, bufferSamples, bufferSize);
+        sampleCount = bufferSampleCount / (float)ZIC_WAVETABLE_WAVEFORMS_COUNT;
+
+        // sampleDurationCounter = -1; // set counter to the maximum
+        // sampleDurationCounter = sampleCountDuration;
+        // updateUi(NULL);
+    }
+
+       void open(int position, bool force)
+    {
+        if (force || position != fileBrowser.position) {
+            std::string filepath = fileBrowser.getFilePath(position);
+            printf("KICK23_SAMPLE_SELECTOR: %d %s\n", position, filepath.c_str());
+            open(filepath);
+        }
+    } 
+
+    void morph(float pct)
+    {
+        sampleStart = pct * bufferSampleCount;
+        uint64_t max = bufferSampleCount / ZIC_WAVETABLE_WAVEFORMS_COUNT * (ZIC_WAVETABLE_WAVEFORMS_COUNT - 1); // TODO make this better :p
+        if (sampleStart > max) {
+            sampleStart = max;
+        }
+    }
+};
+
+#endif
