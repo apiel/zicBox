68d671a76425aa01fdb64e23e451687da8fde4fc
Alexandre Piel <alexandre.piel@zooplus.com>
2024-02-20 22:23:36 +0100

    lookup table noise

--- plugins/audio/lookupTable.h
+++ plugins/audio/lookupTable.h
@@ -4,6 +4,9 @@
 #include <math.h>
 
 class LookupTable {
+protected:
+    int noiseIndex = 0;
+
 public:
     // const static int size = 2048;
     const static int size = 4096;
@@ -16,6 +19,14 @@ public:
             sine[i] = sinf((float)i / (float)size * 2.0f * M_PI);
             noise[i] = 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f;
         }
+    }
+
+    float getNoise()
+    {
+        if (noiseIndex >= size) {
+            noiseIndex = 0;
+        }
+        return noise[noiseIndex++];
     }
 };
 
