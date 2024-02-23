e5936d88a49a8c289604f42df08ef06d8150fad3
Alexandre Piel <alexandre.piel@zooplus.com>
2024-02-23 22:45:29 +0100

    loadConfigPlugin

--- config.h
+++ config.h
@@ -2,26 +2,29 @@
 #define _UI_CONFIG_H_
 
 #include "controllers.h"
+#include "event.h"
 #include "host.h"
 #include "styles.h"
 #include "viewManager.h"
-#include "event.h"
 
 #include "dustscript/dustscript.h"
+#include "helpers/configPlugin.h"
 
-void uiScriptCallback(char* key, char* value, const char* filename, uint8_t indentation, DustScript& instance)
+void uiScriptCallback(char* key, char* value, const char* filename, uint8_t indentation, DustScript& instance);
+
+void uiScriptCallback(char* key, char* value, const char* filename)
 {
     if (strcmp(key, "print") == 0) {
         printf(">> LOG: %s\n", value);
-/*md
-### INCLUDE
+        /*md
+        ### INCLUDE
 
-A `.ui` file can include another `.ui` file, using `INCLUDE: path/of/the/file.ui`. As soon as this `INCLUDE` is called, it will continue into the child file till his end, to finally come back to the original file.
+        A `.ui` file can include another `.ui` file, using `INCLUDE: path/of/the/file.ui`. As soon as this `INCLUDE` is called, it will continue into the child file till his end, to finally come back to the original file.
 
-```coffee
-INCLUDE: path/of/the/file.ui
-```
-*/
+        ```coffee
+        INCLUDE: path/of/the/file.ui
+        ```
+        */
     } else if (strcmp(key, "INCLUDE") == 0) {
         char fullpath[512];
         getFullpath(value, filename, fullpath);
@@ -37,39 +40,46 @@ INCLUDE: path/of/the/file.ui
         styles.screen.h = atoi(strtok(NULL, " "));
     } else if (strcmp(key, "PLUGIN_CONTROLLER") == 0) {
         loadPluginController(value, filename);
-/*md
-### SET_COLOR
+        /*md
+        ### SET_COLOR
 
-`SET_COLOR` give the possibility to customize the pre-defined color for the UI. To change a color, use `SET_COLOR: name_of_color #xxxxxx`.
+        `SET_COLOR` give the possibility to customize the pre-defined color for the UI. To change a color, use `SET_COLOR: name_of_color #xxxxxx`.
 
-```coffee
-SET_COLOR: overlay #00FFFF
-```
+        ```coffee
+        SET_COLOR: overlay #00FFFF
+        ```
 
-In this example, we change the `overlay` color to `#00FFFF`.
+        In this example, we change the `overlay` color to `#00FFFF`.
 
-```cpp
-    .colors = {
-        .background = {0x21, 0x25, 0x2b, 255},  // #21252b
-        .overlay = {0xd1, 0xe3, 0xff, 0x1e},    // #d1e3ff1e
-        .on = {0x00, 0xb3, 0x00, 255},          // #00b300
-        .white = {0xff, 0xff, 0xff, 255},       // #ffffff
-        .blue = {0xad, 0xcd, 0xff, 255},        // #adcdff
-        .red = {0xff, 0x8d, 0x99, 255},         // #ff8d99
-    },
-```
+        ```cpp
+            .colors = {
+                .background = {0x21, 0x25, 0x2b, 255},  // #21252b
+                .overlay = {0xd1, 0xe3, 0xff, 0x1e},    // #d1e3ff1e
+                .on = {0x00, 0xb3, 0x00, 255},          // #00b300
+                .white = {0xff, 0xff, 0xff, 255},       // #ffffff
+                .blue = {0xad, 0xcd, 0xff, 255},        // #adcdff
+                .red = {0xff, 0x8d, 0x99, 255},         // #ff8d99
+            },
+        ```
 
-> This list might be outdated, to get the list of existing colors, look at `./styles.h`
-*/
+        > This list might be outdated, to get the list of existing colors, look at `./styles.h`
+        */
     } else if (strcmp(key, "SET_COLOR") == 0) {
         char* name = strtok(value, " ");
         char* color = strtok(NULL, " ");
         ViewManager::get().draw.setColor(name, color);
+    } else if (strcmp(key, "LOAD_CONFIG_PLUGIN") == 0) {
+        loadConfigPlugin(value, filename, uiScriptCallback);
     } else if (pluginControllerConfig(key, value) || EventHandler::get().config(key, value)) {
         return;
     } else {
         ViewManager::get().config(key, value, filename);
     }
+}
+
+void uiScriptCallback(char* key, char* value, const char* filename, uint8_t indentation, DustScript& instance)
+{
+    uiScriptCallback(key, value, filename);
 }
 
 void loadUiConfig(const char* filename)
---
+++ helpers/configPlugin.h
@@ -0,0 +1,35 @@
+#ifndef _CONFIG_PLUGIN_H
+#define _CONFIG_PLUGIN_H
+
+#include "getFullpath.h"
+
+#include <dlfcn.h>
+#include <stdio.h>
+
+void loadConfigPlugin(char* path, const char* filename, void (*callback)(char* command, char* params, const char* filename))
+{
+    char fullpath[512];
+    getFullpath(path, filename, fullpath);
+    void* handle = dlopen(fullpath, RTLD_LAZY);
+
+    if (!handle) {
+        printf("Cannot open library: %s\n", dlerror());
+        return;
+    }
+
+    dlerror();
+
+    double (*configParser)(const char* filename, void (*callback)(char* command, char* params, const char* filename));
+    configParser = (double (*)(const char* filename, void (*callback)(char* command, char* params, const char* filename))) dlsym(handle, "config");
+    const char* dlsym_error = dlerror();
+    if (dlsym_error) {
+        printf("Cannot load symbol: %s\n", dlsym_error);
+        dlclose(handle);
+        return;
+    }
+
+    configParser(filename, callback);
+    dlclose(handle);
+}
+
+#endif
