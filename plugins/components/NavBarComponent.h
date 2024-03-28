7dc4629a393851ed299474cb7f03b8beec29ba2c
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-28 22:26:19 +0100

    sub item

--- plugins/components/NavBarComponent.h
+++ plugins/components/NavBarComponent.h
@@ -15,9 +15,11 @@ class NavBarComponent : public Component {
 protected:
     Size arrowSize = { 10, 10 };
     Size itemSize = { 0, 0 };
+    int itemsFix = 0;
+    Size subItemSize = { 0, 0 };
+    int subItemsFix = 0;
     int fontSize = 10;
     int textTopMargin = 2;
-    int itemsFix = 0;
     int itemPos = 0;
 
     KeypadLayout keypadLayout;
@@ -40,6 +42,43 @@ protected:
         itemSize = { (size.w - 2 * (arrowSize.w + 1)) / 3 - 1, size.h };
         textTopMargin = (arrowSize.h - fontSize) / 2;
         itemsFix = (size.w - (((itemSize.w + 1) * 3) + (arrowSize.w * 2) + 1));
+    }
+
+    void renderMain()
+    {
+        draw.filledRect(position, arrowSize, colors.background);
+        draw.aalines({ { position.x + arrowSize.w - 3, position.y + 1 },
+                         { position.x + 3, position.y + (int)(arrowSize.h * 0.5) },
+                         { position.x + arrowSize.w - 3, position.y + arrowSize.h - 2 } },
+            colors.font);
+
+        draw.filledRect({ position.x + size.w - arrowSize.w, position.y }, arrowSize, colors.background);
+        draw.aalines({ { position.x + size.w - arrowSize.w + 3, position.y + 1 },
+                         { position.x + size.w - 3, position.y + (int)(arrowSize.h * 0.5) },
+                         { position.x + size.w - arrowSize.w + 3, position.y + arrowSize.h - 2 } },
+            colors.font);
+
+        int xPos = position.x + (arrowSize.w + 1);
+        for (int i = 0; i < 3; i++) {
+            // Because width is not always exactly dividable by 3 pixelwise
+            // we need to add some fix in the middle item
+            Size _itemSize = { itemSize.w + (i == 1 ? itemsFix : 0), itemSize.h };
+            drawItem({ xPos, position.y },
+                _itemSize,
+                items[(i + itemPos) % items.size()].text);
+            xPos += _itemSize.w + 1;
+        }
+    }
+
+    void renderSub()
+    {
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 5; col++) {
+                Point pos = { position.x + col * (subItemSize.w + 1), position.y + row * subItemSize.h };
+                draw.filledRect(pos, subItemSize, colors.background);
+                draw.textCentered({ pos.x + subItemSize.w / 2, pos.y }, "track" + std::to_string(row * 5 + col + 1), colors.font, 9);
+            }
+        }
     }
 
     struct Colors {
@@ -71,32 +110,15 @@ public:
         colors.font = props.draw.styles.colors.grey;
         arrowSize = { size.h, size.h };
         setSizes();
+        subItemSize = { (size.w - 4) / 5, size.h / 2 };
+        subItemsFix = (size.w - (((subItemSize.w + 1) * 5) - 1));
     }
 
     void render()
     {
-        draw.filledRect(position, arrowSize, colors.background);
-        draw.aalines({ { position.x + arrowSize.w - 3, position.y + 1 },
-                         { position.x + 3, position.y + (int)(arrowSize.h * 0.5) },
-                         { position.x + arrowSize.w - 3, position.y + arrowSize.h - 2 } },
-            colors.font);
-
-        draw.filledRect({ position.x + size.w - arrowSize.w, position.y }, arrowSize, colors.background);
-        draw.aalines({ { position.x + size.w - arrowSize.w + 3, position.y + 1 },
-                         { position.x + size.w - 3, position.y + (int)(arrowSize.h * 0.5) },
-                         { position.x + size.w - arrowSize.w + 3, position.y + arrowSize.h - 2 } },
-            colors.font);
-
-        int xPos = position.x + (arrowSize.w + 1);
-        for (int i = 0; i < 3; i++) {
-            // Because width is not always exactly dividable by 3 pixelwise
-            // we need to add some fix in the middle item
-            Size _itemSize = { itemSize.w + (i == 1 ? itemsFix : 0), itemSize.h };
-            drawItem({ xPos, position.y },
-                _itemSize,
-                items[(i + itemPos) % items.size()].text);
-            xPos += _itemSize.w + 1;
-        }
+        // clear(); // should only clear when switching from main to sub
+        // renderMain();
+        renderSub();
     }
 
     bool config(char* key, char* value)
