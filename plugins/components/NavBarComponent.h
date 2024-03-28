7bc8fbcc4392caaf614e8350786d0dae98538f1c
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-28 18:53:00 +0100

    navbar

--- plugins/components/NavBarComponent.h
+++ plugins/components/NavBarComponent.h
@@ -15,6 +15,7 @@ protected:
     Size itemSize = { 0, 0 };
     int fontSize = 10;
     int textTopMargin = 2;
+    int itemsMarginRight = 0;
 
     void drawItem(int x, int y, std::string text)
     {
@@ -36,24 +37,25 @@ public:
         arrowSize = { props.size.h, props.size.h };
         itemSize = { (props.size.w - 2 * (arrowSize.w + 1)) / 3 - 1, props.size.h };
         textTopMargin = (arrowSize.h - fontSize) / 2;
+        itemsMarginRight = (size.w - (((itemSize.w + 1) * 3) + ((arrowSize.w + 1) * 2))) / 2;
     }
 
     void render()
     {
         draw.filledRect(position, arrowSize, colors.background);
         draw.aalines({ { position.x + arrowSize.w - 3, position.y + 1 },
-                       { position.x + 3, position.y + (int)(arrowSize.h * 0.5) },
-                       { position.x + arrowSize.w - 3, position.y + arrowSize.h - 2 } },
+                         { position.x + 3, position.y + (int)(arrowSize.h * 0.5) },
+                         { position.x + arrowSize.w - 3, position.y + arrowSize.h - 2 } },
             colors.font);
 
         draw.filledRect({ position.x + size.w - arrowSize.w, position.y }, arrowSize, colors.background);
         draw.aalines({ { position.x + size.w - arrowSize.w + 3, position.y + 1 },
-                       { position.x + size.w - 3, position.y + (int)(arrowSize.h * 0.5) },
-                       { position.x + size.w - arrowSize.w + 3, position.y + arrowSize.h - 2 } },
+                         { position.x + size.w - 3, position.y + (int)(arrowSize.h * 0.5) },
+                         { position.x + size.w - arrowSize.w + 3, position.y + arrowSize.h - 2 } },
             colors.font);
 
         for (int i = 0; i < 3; i++) {
-            drawItem(position.x + (arrowSize.w + 1) + i * (itemSize.w + 1), position.y, "Item " + std::to_string(i));
+            drawItem(position.x + (arrowSize.w + 1) + i * (itemSize.w + 1) + itemsMarginRight, position.y, "Item " + std::to_string(i));
         }
     }
 
