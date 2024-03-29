9d7eb4765f17954dbb27b0bb4abf6edfc29c37d7
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-29 15:45:16 +0100

    mpr

--- plugins/controllers/Mpr121Controller.h
+++ plugins/controllers/Mpr121Controller.h
@@ -181,7 +181,7 @@ public:
                 // it if *is* touched and *wasnt* touched before, alert!
                 if ((currtouched & _BV(i)) && !(lasttouched & _BV(i))) {
                     // uint16_t value = filteredData(i);
-                    // printf("[%x] %d touched: %d\n", address, i, value);
+                    printf("[%x] %d touched: %d\n", address, i, value);
                     controller->onKey(address, i, 1);
 
                 }
