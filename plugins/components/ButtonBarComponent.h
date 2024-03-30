68e151fd3457ae490b40c6057abccb3926b95286
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-30 10:53:07 +0100

    btn bar

--- plugins/components/ButtonBarComponent.h
+++ plugins/components/ButtonBarComponent.h
@@ -21,6 +21,7 @@ protected:
     int textLeftMargin = 0;
 
     KeypadLayout keypadLayout;
+    int8_t activeItem = 1;
 
     struct Item {
         std::string text;
@@ -47,32 +48,43 @@ protected:
             return;
         }
 
-        draw.filledRect(pos, itemSize, colors.background);
-        draw.textCentered({ pos.x + textLeftMargin, pos.y }, items[index].text, colors.font, fontSize);
+        Color background = colors.background;
+        Color fontColor = colors.font;
+        if (index == activeItem) {
+            background = colors.activeBackground;
+            fontColor = colors.activeFont;
+        }
+
+        draw.filledRect(pos, itemSize, background);
+        draw.textCentered({ pos.x + textLeftMargin, pos.y }, items[index].text, fontColor, fontSize);
     }
 
+    // int8_t previousActiveItem = 1; // to restore after pressing shift
     void handleButton(int8_t id, int8_t state)
     {
-        printf("handleButton: %d %d\n", id, state);
-        // renderNext();
+        // printf("handleButton: %d %d\n", id, state);
+        activeItem = id;
+        renderNext();
     }
 
     struct Colors {
         Color background;
         Color font;
+        Color activeBackground;
+        Color activeFont;
     } colors;
 
 public:
     /*md **Keyboard actions**: */
     void addKeyMap(KeypadInterface* controller, uint16_t controllerId, uint8_t key, int param, std::string action, uint8_t color)
     {
-        // /*md - `btnLeft` to select left item. */
-        // if (action == "btnLeft") {
-        //     keypadLayout.mapping.push_back(
-        //         { controller, controllerId, key, param,
-        //             [&](int8_t state, KeypadLayout::KeyMap& keymap) { handleButton(0, state); },
-        //             color, [&](KeypadLayout::KeyMap& keymap) { return keymap.color == 255 ? 10 : keymap.color; } });
-        // }
+        /*md - `item` to select item. */
+        if (action == "item") {
+            keypadLayout.mapping.push_back(
+                { controller, controllerId, key, param,
+                    [&](int8_t state, KeypadLayout::KeyMap& keymap) { handleButton(keymap.param, state); },
+                    color, [&](KeypadLayout::KeyMap& keymap) { return keymap.color == 255 ? 10 : keymap.color; } });
+        }
     }
 
     ButtonBarComponent(ComponentInterface::Props props)
@@ -81,6 +93,8 @@ public:
     {
         colors.background = props.draw.lighten(props.draw.styles.colors.background, 0.2);
         colors.font = props.draw.styles.colors.grey;
+        colors.activeBackground = props.draw.lighten(colors.background, 0.4);
+        colors.activeFont = props.draw.lighten(colors.font, 0.4);
         itemSize = { (size.w / itemColumnCount) - 1, (size.h / itemRowCount) - 1 };
         textTopMargin = (itemSize.h - fontSize) / 2;
         textLeftMargin = itemSize.w / 2; // center
@@ -101,12 +115,14 @@ public:
         /*md - `BACKGROUND: #333333` set the background color and activate background drawing. */
         if (strcmp(key, "BACKGROUND") == 0) {
             colors.background = draw.getColor(value);
+            colors.activeBackground = draw.lighten(colors.background, 0.4);
             return true;
         }
 
         /*md - `FONT_COLOR: #ffffff` set the font color. */
         if (strcmp(key, "FONT_COLOR") == 0) {
             colors.font = draw.getColor(value);
+            colors.activeFont = draw.lighten(colors.font, 0.4);
             return true;
         }
 
