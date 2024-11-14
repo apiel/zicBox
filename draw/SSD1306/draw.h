818f45a8c5279edfc764d54477b5a21e02b0d3d4
apiel <alexandre.piel@gmail.com>
2024-11-14 17:05:35 +0100

    pixel char

--- draw/SSD1306/draw.h
+++ draw/SSD1306/draw.h
@@ -3,7 +3,9 @@
 
 #include "../../fonts/Sinclair_M.h"
 #include "../../fonts/Sinclair_S.h"
-#include "../../fonts/Tiny.h"
+#include "../../fonts/BigFont.h"
+#include "../../fonts/ArialNormal.h"
+#include "../../fonts/ArialBold.h"
 #include "../../helpers/i2c.h"
 #include "../../log.h"
 #include "../../plugins/components/drawInterface.h"
@@ -328,10 +330,8 @@ public:
         // filledPie({ 100, 32 }, 10, 4, 6);
         circle({ 100, 32 }, 10);
 
-        // drawChar(10, 10, 'A', Sinclair_S, 3.0);
-        // drawChar(10, 10, 'A', TinyFont);
-        // drawChar(10, 10, 'A', Sinclair_S);
-        drawChar({ 10, 10 }, 'B', Sinclair_M);
+        drawChar({ 10, 10 }, 'A', ArialBold);
+        // drawChar({ 10, 10 }, 'B', Sinclair_M);
 
         filledRect({ 58, 10 }, { 20, 20 });
         drawChar({ 60, 10 }, 'B', Sinclair_S, 2.0, SSD1306_BLACK);
