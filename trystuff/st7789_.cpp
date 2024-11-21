a82e9cd8bf57b6cdc231f5531aac8eb1da5aba96
apiel <alexandre.piel@gmail.com>
2024-11-21 16:01:52 +0100

    ststuff

--- trystuff/st7789_.cpp
+++ trystuff/st7789_.cpp
@@ -34,57 +34,24 @@
 // RPi BOARD (Physical not BCM) Pin #23 (SCLK) -> Display SCL
 // RPi BOARD (Physical not BCM) Pin #19 (MOSI) -> Display SDA
 
-// #define GPIO_TFT_DATA_CONTROL 6
-// #define GPIO_TFT_RESET_PIN 5
-// #define GPIO_TFT_BACKLIGHT 13
-
 #define SPI_BUS_CLOCK_DIVISOR 20
 
-#define DISPLAY_WIDTH 240
-#define DISPLAY_HEIGHT 240
-
-#define BCM2835_GPIO_BASE 0x200000 // Address to GPIO register file
 #define BCM2835_SPI0_BASE 0x204000 // Address to SPI0 register file
-#define BCM2835_TIMER_BASE 0x3000 // Address to System Timer register file
 
 #define BCM2835_SPI0_CS_RXF 0x00100000 // Receive FIFO is full
 #define BCM2835_SPI0_CS_RXR 0x00080000 // FIFO needs reading
 #define BCM2835_SPI0_CS_TXD 0x00040000 // TXD TX FIFO can accept Data
 #define BCM2835_SPI0_CS_RXD 0x00020000 // RXD RX FIFO contains Data
 #define BCM2835_SPI0_CS_DONE 0x00010000 // Done transfer Done
-#define BCM2835_SPI0_CS_ADCS 0x00000800 // Automatically Deassert Chip Select
-#define BCM2835_SPI0_CS_INTR 0x00000400 // Fire interrupts on RXR?
-#define BCM2835_SPI0_CS_INTD 0x00000200 // Fire interrupts on DONE?
-#define BCM2835_SPI0_CS_DMAEN 0x00000100 // Enable DMA transfers?
 #define BCM2835_SPI0_CS_TA 0x00000080 // Transfer Active
 #define BCM2835_SPI0_CS_CLEAR 0x00000030 // Clear FIFO Clear RX and TX
 #define BCM2835_SPI0_CS_CLEAR_RX 0x00000020 // Clear FIFO Clear RX
 #define BCM2835_SPI0_CS_CLEAR_TX 0x00000010 // Clear FIFO Clear TX
 #define BCM2835_SPI0_CS_CPOL 0x00000008 // Clock Polarity
 #define BCM2835_SPI0_CS_CPHA 0x00000004 // Clock Phase
-#define BCM2835_SPI0_CS_CS 0x00000003 // Chip Select
-
-#define BCM2835_SPI0_CS_RXF_SHIFT 20
-#define BCM2835_SPI0_CS_RXR_SHIFT 19
-#define BCM2835_SPI0_CS_TXD_SHIFT 18
-#define BCM2835_SPI0_CS_RXD_SHIFT 17
-#define BCM2835_SPI0_CS_DONE_SHIFT 16
-#define BCM2835_SPI0_CS_ADCS_SHIFT 11
-#define BCM2835_SPI0_CS_INTR_SHIFT 10
-#define BCM2835_SPI0_CS_INTD_SHIFT 9
-#define BCM2835_SPI0_CS_DMAEN_SHIFT 8
-#define BCM2835_SPI0_CS_TA_SHIFT 7
-#define BCM2835_SPI0_CS_CLEAR_RX_SHIFT 5
-#define BCM2835_SPI0_CS_CLEAR_TX_SHIFT 4
-#define BCM2835_SPI0_CS_CPOL_SHIFT 3
-#define BCM2835_SPI0_CS_CPHA_SHIFT 2
-#define BCM2835_SPI0_CS_CS_SHIFT 0
 
 #define GPIO_SPI0_MOSI 10 // Pin P1-19, MOSI when SPI0 in use
-#define GPIO_SPI0_MISO 9 // Pin P1-21, MISO when SPI0 in use
 #define GPIO_SPI0_CLK 11 // Pin P1-23, CLK when SPI0 in use
-#define GPIO_SPI0_CE0 8 // Pin P1-24, CE0 when SPI0 in use
-#define GPIO_SPI0_CE1 7 // Pin P1-26, CE1 when SPI0 in use
 
 #define DISPLAY_SPI_DRIVE_SETTINGS (1 | BCM2835_SPI0_CS_CPOL | BCM2835_SPI0_CS_CPHA)
 
