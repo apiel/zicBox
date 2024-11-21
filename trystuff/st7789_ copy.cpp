869e685223a60a7d30183b9619c6f5b7b376c3d3
apiel <alexandre.piel@gmail.com>
2024-11-21 16:02:53 +0100

    ststuff

--- trystuff/st7789_ copy.cpp
+++ trystuff/st7789_ copy.cpp
@@ -14,6 +14,7 @@
 #include <sys/mman.h> // mmap, munmap
 #include <unistd.h> // usleep
 #include "../helpers/gpio.h"
+#include "../helpers/st7789.h"
 
 #define HAS_BCM 1 // comment for testing on non-BCM hardware (desktops, etc.)
 #ifdef HAS_BCM
@@ -33,57 +34,24 @@
 // RPi BOARD (Physical not BCM) Pin #23 (SCLK) -> Display SCL
 // RPi BOARD (Physical not BCM) Pin #19 (MOSI) -> Display SDA
 
-// #define GPIO_TFT_DATA_CONTROL 6
-// #define GPIO_TFT_RESET_PIN 5
-// #define GPIO_TFT_BACKLIGHT 13
-
 #define SPI_BUS_CLOCK_DIVISOR 20
 
-#define DISPLAY_WIDTH 240
-#define DISPLAY_HEIGHT 240
-
-#define BCM2835_GPIO_BASE 0x200000 // Address to GPIO register file
 #define BCM2835_SPI0_BASE 0x204000 // Address to SPI0 register file
-#define BCM2835_TIMER_BASE 0x3000 // Address to System Timer register file
 
 #define BCM2835_SPI0_CS_RXF 0x00100000 // Receive FIFO is full
 #define BCM2835_SPI0_CS_RXR 0x00080000 // FIFO needs reading
 #define BCM2835_SPI0_CS_TXD 0x00040000 // TXD TX FIFO can accept Data
 #define BCM2835_SPI0_CS_RXD 0x00020000 // RXD RX FIFO contains Data
 #define BCM2835_SPI0_CS_DONE 0x00010000 // Done transfer Done
-#define BCM2835_SPI0_CS_ADCS 0x00000800 // Automatically Deassert Chip Select
-#define BCM2835_SPI0_CS_INTR 0x00000400 // Fire interrupts on RXR?
-#define BCM2835_SPI0_CS_INTD 0x00000200 // Fire interrupts on DONE?
-#define BCM2835_SPI0_CS_DMAEN 0x00000100 // Enable DMA transfers?
 #define BCM2835_SPI0_CS_TA 0x00000080 // Transfer Active
 #define BCM2835_SPI0_CS_CLEAR 0x00000030 // Clear FIFO Clear RX and TX
 #define BCM2835_SPI0_CS_CLEAR_RX 0x00000020 // Clear FIFO Clear RX
 #define BCM2835_SPI0_CS_CLEAR_TX 0x00000010 // Clear FIFO Clear TX
 #define BCM2835_SPI0_CS_CPOL 0x00000008 // Clock Polarity
 #define BCM2835_SPI0_CS_CPHA 0x00000004 // Clock Phase
-#define BCM2835_SPI0_CS_CS 0x00000003 // Chip Select
-
-#define BCM2835_SPI0_CS_RXF_SHIFT 20
-#define BCM2835_SPI0_CS_RXR_SHIFT 19
-#define BCM2835_SPI0_CS_TXD_SHIFT 18
-#define BCM2835_SPI0_CS_RXD_SHIFT 17
-#define BCM2835_SPI0_CS_DONE_SHIFT 16
-#define BCM2835_SPI0_CS_ADCS_SHIFT 11
-#define BCM2835_SPI0_CS_INTR_SHIFT 10
-#define BCM2835_SPI0_CS_INTD_SHIFT 9
-#define BCM2835_SPI0_CS_DMAEN_SHIFT 8
-#define BCM2835_SPI0_CS_TA_SHIFT 7
-#define BCM2835_SPI0_CS_CLEAR_RX_SHIFT 5
-#define BCM2835_SPI0_CS_CLEAR_TX_SHIFT 4
-#define BCM2835_SPI0_CS_CPOL_SHIFT 3
-#define BCM2835_SPI0_CS_CPHA_SHIFT 2
-#define BCM2835_SPI0_CS_CS_SHIFT 0
 
 #define GPIO_SPI0_MOSI 10 // Pin P1-19, MOSI when SPI0 in use
-#define GPIO_SPI0_MISO 9 // Pin P1-21, MISO when SPI0 in use
 #define GPIO_SPI0_CLK 11 // Pin P1-23, CLK when SPI0 in use
-#define GPIO_SPI0_CE0 8 // Pin P1-24, CE0 when SPI0 in use
-#define GPIO_SPI0_CE1 7 // Pin P1-26, CE1 when SPI0 in use
 
 #define DISPLAY_SPI_DRIVE_SETTINGS (1 | BCM2835_SPI0_CS_CPOL | BCM2835_SPI0_CS_CPHA)
 
@@ -152,16 +120,6 @@ void sendCmd(uint8_t cmd, uint8_t* payload, uint32_t payloadSize)
     WaitForPolledSPITransferToFinish();
 }
 
-void sendCmdOnly(uint8_t cmd)
-{
-    sendCmd(cmd, NULL, 0);
-}
-
-void sendCmdData(uint8_t cmd, uint8_t data)
-{
-    sendCmd(cmd, &data, 1);
-}
-
 int InitSPI()
 {
 #ifdef HAS_BCM
@@ -198,51 +156,8 @@ int InitSPI()
     return 0;
 }
 
-#define DISPLAY_SET_CURSOR_X 0x2A
-#define DISPLAY_SET_CURSOR_Y 0x2B
-#define DISPLAY_WRITE_PIXELS 0x2C
-
-#define BYTESPERPIXEL 2
-
-void sendAddr(uint8_t cmd, uint16_t addr0, uint16_t addr1)
-{
-    uint8_t addr[4] = { (uint8_t)(addr0 >> 8), (uint8_t)(addr0 & 0xFF), (uint8_t)(addr1 >> 8), (uint8_t)(addr1 & 0xFF) };
-    sendCmd(cmd, addr, 4);
-}
-
-void drawPixel(uint16_t x, uint16_t y, uint16_t color)
-{
-    uint8_t data[BYTESPERPIXEL] = { color >> 8, color & 0xFF };
-
-    sendAddr(DISPLAY_SET_CURSOR_X, (uint16_t)x, (uint16_t)x);
-    sendAddr(DISPLAY_SET_CURSOR_Y, (uint16_t)y, (uint16_t)y);
-    sendCmd(DISPLAY_WRITE_PIXELS, data, 2);
-}
-
-void drawFillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color)
-{
-    int yPos;
-    uint16_t size = w * BYTESPERPIXEL;
-    uint8_t pixels[size];
-    uint8_t pixel[BYTESPERPIXEL] = { color >> 8, color & 0xFF };
-
-    for (uint16_t i = 0; i < size; i += BYTESPERPIXEL) {
-        pixels[i] = pixel[0];
-        pixels[i + 1] = pixel[1];
-    }
-
-    for (yPos = 0; yPos < h; ++yPos) {
-        sendAddr(DISPLAY_SET_CURSOR_X, x, x + w);
-        sendAddr(DISPLAY_SET_CURSOR_Y, y + yPos, y + yPos);
-        sendCmd(DISPLAY_WRITE_PIXELS, pixels, size);
-    }
-}
-
 void InitSPIDisplay()
 {
-    uint8_t madctl = 0;
-    uint8_t data[4] = { 0, 0, (uint8_t)(240 >> 8), (uint8_t)(240 & 0xFF) };
-
     printf("Resetting display at reset GPIO pin %d\n", GPIO_TFT_RESET_PIN);
     setGpioMode(GPIO_TFT_RESET_PIN, 1);
     setGpio(GPIO_TFT_RESET_PIN, 1);
@@ -258,55 +173,8 @@ void InitSPIDisplay()
     spi->clk = 34;
     __sync_synchronize();
 
-    sendCmdOnly(0x11 /*Sleep Out*/);
-    usleep(120 * 1000);
-    sendCmdData(0x3A /*COLMOD: Pixel Format Set*/, 0x05 /*16bpp*/);
-    usleep(20 * 1000);
-
-#define MADCTL_COLUMN_ADDRESS_ORDER_SWAP (1 << 6)
-#define MADCTL_ROW_ADDRESS_ORDER_SWAP (1 << 7)
-#define MADCTL_ROTATE_180_DEGREES (MADCTL_COLUMN_ADDRESS_ORDER_SWAP | MADCTL_ROW_ADDRESS_ORDER_SWAP)
-/* RGB/BGR Order ('0' = RGB, '1' = BGR) */
-#define ST7789_MADCTL_RGB 0x00
-#define ST7789_MADCTL_BGR 0x08
-
-    madctl |= MADCTL_ROW_ADDRESS_ORDER_SWAP;
-    madctl |= ST7789_MADCTL_BGR; // Because we swap order, we need to use BGR...
-    madctl ^= MADCTL_ROTATE_180_DEGREES;
-    sendCmdData(0x36 /*MADCTL: Memory Access Control*/, madctl);
-    usleep(10 * 1000);
-
-    sendCmdData(0xBA /*DGMEN: Enable Gamma*/, 0x04);
-
-    sendCmdOnly(0x21 /*Display Inversion On*/);
-    // sendCmd(0x20 /*Display Inversion Off*/);
-
-    sendCmdOnly(0x13 /*NORON: Partial off (normal)*/);
-    usleep(10 * 1000);
-    // The ST7789 controller is actually a unit with 320x240 graphics memory area, but only 240x240 portion
-    // of it is displayed. Therefore if we wanted to swap row address mode above, writes to Y=0...239 range will actually land in
-    // memory in row addresses Y = 319-(0...239) = 319...80 range. To view this range, we must scroll the view by +80 units in Y
-    // direction so that contents of Y=80...319 is displayed instead of Y=0...239.
-    if ((madctl & MADCTL_ROW_ADDRESS_ORDER_SWAP)) {
-        sendCmd(0x37 /*VSCSAD: Vertical Scroll Start Address of RAM*/, data, 4);
-    }
-
-    // drawFillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0); // clear screen
-    srand(time(NULL));
-    uint16_t randomColor = rand() % 0xFFFFFF;
-    printf("randomColor = 0x%06X\n", randomColor);
-    drawFillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, randomColor); // clear screen
-
-    for (int i = 0; i < 100; i++) {
-        drawPixel(i, i, 0xFFFF00);
-        drawPixel(rand() % DISPLAY_WIDTH, rand() % DISPLAY_HEIGHT, 0xFFFFFF);
-    }
-
-    // setGpioMode(GPIO_TFT_BACKLIGHT, 1);
-    // setGpio(GPIO_TFT_BACKLIGHT);
-
-    sendCmdOnly(/*Display ON*/ 0x29);
-    usleep(100 * 1000);
+    ST7789 st7789([](uint8_t cmd, uint8_t* data, uint32_t len) { sendCmd(cmd, data, len); }, 240, 240);
+    st7789.init();
 
     //     // And speed up to the desired operation speed finally after init is done.
     usleep(10 * 1000); // Delay a bit before restoring CLK, or otherwise this has been observed to cause the display not init if done back to back after the clear operation above.
