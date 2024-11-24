bbbe30494b288c39282e15789b52f3eee05755ac
apiel <alexandre.piel@gmail.com>
2024-11-24 18:32:00 +0100

    pie

--- plugins/components/Pixel/Encoder2Component.h
+++ plugins/components/Pixel/Encoder2Component.h
@@ -85,7 +85,7 @@ protected:
         int val = 280 * value->pct();
 
         if (val < 280) {
-            draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { colors.barBackground, .antiAliasing = true });
+            draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { colors.barBackground });
         }
         if (val > 0) {
             int endAngle = 130 + val;
@@ -94,7 +94,7 @@ protected:
             }
             draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, endAngle, { colors.bar });
         }
-        draw.filledEllipse({ knobCenter.x, knobCenter.y - marginTop }, insideRadius, insideRadius, { colors.background });
+        draw.filledCircle({ knobCenter.x, knobCenter.y - marginTop }, insideRadius, { colors.background });
     }
 
     void renderCenteredBar()
@@ -246,12 +246,16 @@ public:
         // draw.filledCircle({ knobCenter.x, knobCenter.y - marginTop }, radius, { colors.barBackground });
 
         if (value != NULL) {
-            renderEncoder();
+            // renderEncoder();
         }
 
-        // draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { colors.barBackground, .antiAliasing = true });
+        // draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { styles.colors.white });
+        draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, radius, 0, 180, { styles.colors.white });
+
         // draw.filledPie({ knobCenter.x, knobCenter.y - marginTop }, insideRadius -2, 130, 50, { colors.barBackground, .antiAliasing = true });
         // draw.arc({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { colors.barBackground, .antiAliasing = true, .thickness = 3 });
+
+        // draw.arc({ knobCenter.x, knobCenter.y - marginTop }, radius, 0, 180, { colors.barBackground, .antiAliasing = true, .thickness = 3 });
 
         // draw.arc({ knobCenter.x, knobCenter.y - marginTop }, radius, 130, 50, { colors.barBackground, .antiAliasing = true, .thickness = 3 });
     }
