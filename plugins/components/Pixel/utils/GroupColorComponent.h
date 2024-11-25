81330af4c2c7e6a256d75344290b855e26c5e0a3
apiel <alexandre.piel@gmail.com>
2024-11-25 22:03:51 +0100

    toggle color

---
+++ plugins/components/Pixel/utils/GroupColorComponent.h
@@ -0,0 +1,48 @@
+#ifndef _GROUP_COLOR_COMPONENT_H_
+#define _GROUP_COLOR_COMPONENT_H_
+
+#include "../../component.h"
+#include "./ToggleColor.h"
+
+class GroupColorComponent : public Component {
+protected:
+    bool isActive = true;
+    float inactiveColorRatio = 0.5f;
+
+    std::vector<ToggleColor*> colors;
+
+    void updateColors()
+    {
+        for (auto& color : colors) {
+            color->toggle(isActive);
+        }
+    }
+
+    void setInactiveColorRatio(float ratio)
+    {
+        inactiveColorRatio = ratio;
+        for (auto& color : colors) {
+            color->darkness(inactiveColorRatio);
+        }
+    }
+
+public:
+    GroupColorComponent(ComponentInterface::Props props, std::vector<ToggleColor*> colors)
+        : Component(props)
+        , colors(colors)
+    {
+    }
+
+    bool config(char* key, char* value)
+    {
+        if (strcmp(key, "INACTIVE_COLOR_RATIO") == 0) {
+            inactiveColorRatio = atof(value);
+            setInactiveColorRatio(inactiveColorRatio);
+            return true;
+        }
+
+        return false;
+    }
+};
+
+#endif
