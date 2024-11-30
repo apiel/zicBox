8d310a7a87c93f7c49f48da2e2fa7ef75d2211ab
apiel <alexandre.piel@gmail.com>
2024-11-30 18:58:12 +0100

    scroll

--- plugins/components/container/ScrollGroupContainer.h
+++ plugins/components/container/ScrollGroupContainer.h
@@ -32,30 +32,30 @@ public:
         }
         int heightSum = 0;
         for (int i = 0; i <= lastGroupId; i++) {
+            int minY = 99999999;
             int maxY = 0;
-            int maxHeight = 0;
             for (void* c : *components) {
                 ComponentInterface* component = (ComponentInterface*)c;
                 if (component->group == i) {
-                    if (component->size.h > maxHeight) {
-                        maxHeight = component->position.y + component->size.h;
-                    }
                     if (component->position.y + component->size.h > maxY) {
                         maxY = component->position.y + component->size.h;
                     }
+                    if (component->position.y < minY) {
+                        minY = component->position.y;
+                    }
                 }
             }
-            if (maxHeight > size.h) {
-                // yPositionPerGroup.push_back(-(maxHeight - size.h));
-                // printf("yPositionPerGroup.push_back: %d\n", -(maxHeight - size.h));
-                // yPositionPerGroup.push_back(-(size.h - ( maxHeight - maxY)));
-                yPositionPerGroup.push_back(-60);
-                printf("yPositionPerGroup.push_back (%d > %d): %d\n", maxHeight, size.h, yPositionPerGroup.back());
+            printf("[%d] minY: %d maxY: %d size.h: %d (%d) pos.y: %d\n", i, minY, maxY, size.h, (int)(size.h * 0.5), originPosition.y);
+            if (maxY > size.h * 0.6) {
+                yPositionPerGroup.push_back(-(minY - size.h * 0.6));
+                printf("push: %d\n", yPositionPerGroup.back());
             } else {
                 yPositionPerGroup.push_back(0);
-                printf("yPositionPerGroup.push_back (%d !> %d): %d\n", maxHeight, size.h, 0);
             }
         }
+
+        // yPositionPerGroup[2] = -60;
+        // yPositionPerGroup[3] = -60;
     }
 
     bool updateCompontentPosition(Point initialPosition, Size componentSize, Point& relativePosition) override
