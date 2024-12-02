da8e40310ffcd848b7bbb276a39aae775de5ac74
apiel <alexandre.piel@gmail.com>
2024-12-02 19:28:32 +0100

    distortion2

--- plugins/audio/lookupTable.h
+++ plugins/audio/lookupTable.h
@@ -12,18 +12,21 @@ protected:
 public:
     const static int size = LOOKUP_TABLE_SIZE;
     float sine[size];
+    float sine2[size];
+    float tanh[size];
     float noise[size];
 
-    LookupTable()
-    {
+    LookupTable() {
         for (int i = 0; i < size; i++) {
-            sine[i] = sinf((float)i / (float)size * 2.0f * M_PI);
-            noise[i] = 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f;
+            sine2[i] = sinf((float)i / (float)(size - 1) * 2.0f * M_PI); // Map index to [0, 2π)
+            float normalizedX = (float)i / (float)(size - 1) * 2.0f - 1.0f; // Map index to [-1.0, 1.0]
+            sine[i] = sinf(normalizedX * M_PI);
+            tanh[i] = std::tanh(normalizedX); // Precompute tanh
+            noise[i] = 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f; // Random noise [-1.0, 1.0]
         }
     }
 
-    float getNoise()
-    {
+    float getNoise() {
         if (noiseIndex >= size) {
             noiseIndex = 0;
         }
