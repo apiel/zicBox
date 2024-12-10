55cbf3f25b0b05be183cbdd8272fd4092c5ed0df
apiel <alexandre.piel@gmail.com>
2024-12-10 21:45:19 +0100

    BaseGraphComponent

--- plugins/components/Pixel/GraphComponent.h
+++ plugins/components/Pixel/GraphComponent.h
@@ -1,10 +1,7 @@
 #ifndef _UI_PIXEL_COMPONENT_GRAPH_H_
 #define _UI_PIXEL_COMPONENT_GRAPH_H_
 
-#include "../../../helpers/range.h"
-#include "../component.h"
-#include "../utils/color.h"
-#include "utils/GroupColorComponent.h"
+#include "utils/BaseGraphComponent.h"
 
 /*md
 ## GraphComponent
@@ -15,18 +12,7 @@ Show a representation of data points and provide a way to control them.
 
 */
 
-class GraphComponent : public GroupColorComponent {
-    AudioPlugin* plugin = NULL;
-
-    bool filled = true;
-    bool outline = true;
-
-    Color bgColor;
-
-    ToggleColor fillColor;
-    ToggleColor outlineColor;
-    ToggleColor textColor;
-
+class GraphComponent : public BaseGraphComponent {
     struct EncoderParam {
         int id = -1;
         ValueInterface* value = NULL;
@@ -36,72 +22,45 @@ class GraphComponent : public GroupColorComponent {
 
     uint8_t dataId = -1;
 
-    int waveformHeight = 30;
-    int waveformY = 0;
-
-    void renderGraph()
+public:
+    GraphComponent(ComponentInterface::Props props)
+        : BaseGraphComponent(props)
     {
+    }
+
+    std::vector<Title> getTitles() override
+    {
+        std::vector<Title> titles = {};
+        for (auto& encoder : encoders) {
+            if (encoder.value) {
+                if (encoder.value->props().type == VALUE_STRING || encoder.toString) {
+                    titles.push_back({ encoder.value->string() });
+                } else if (encoder.value->props().unit != NULL) {
+                    titles.push_back({ std::to_string(encoder.value->get()), encoder.value->props().unit });
+                } else {
+                    titles.push_back({ std::to_string(encoder.value->get()) });
+                }
+            }
+        }
+        return titles;
+    }
+
+    std::vector<Point> getPoints() override
+    {
+        std::vector<Point> points = {};
         if (dataId != -1) {
-            std::vector<Point> relativePoints;
             float halfHeight = waveformHeight * 0.5;
-            relativePoints.push_back({ relativePosition.x, (int)(waveformY + halfHeight) });
+            points.push_back({ 0, (int)(halfHeight) });
             for (int i = 0; i < size.w; i++) {
                 float index = i / (float)(size.w - 1);
                 float* value = (float*)plugin->data(dataId, &index);
                 if (value != NULL) {
-                    Point point = { i, (int)(*value * halfHeight + halfHeight) };
-                    relativePoints.push_back({ point.x + relativePosition.x, point.y + waveformY });
+                    points.push_back({ i, (int)(*value * halfHeight + halfHeight) });
                 }
             }
-            relativePoints.push_back({ relativePosition.x + size.w, (int)(waveformY + halfHeight) });
-            if (filled) {
-                draw.filledPolygon(relativePoints, { fillColor.color });
-            }
-            if (outline) {
-                draw.lines(relativePoints, { outlineColor.color });
-            }
+            points.push_back({ size.w, (int)(halfHeight) });
         }
-    }
-
-    void renderTitles()
-    {
-        int cellWidth = size.w / encoders.size();
-        int x = relativePosition.x + cellWidth * 0.5;
-        for (int i = 0; i < encoders.size(); i++) {
-            ValueInterface* value = encoders[i].value;
-            if (value) {
-                if (value->props().type == VALUE_STRING || encoders[i].toString) {
-                    draw.textCentered({ x + cellWidth * i, relativePosition.y }, value->string(), 8, { textColor.color });
-                } else if (value->props().unit != NULL) {
-                    // could be great make unit in a different color
-                    draw.textCentered({ x + cellWidth * i, relativePosition.y }, std::to_string(value->get()) + value->props().unit, 8, { textColor.color });
-                } else {
-                    draw.textCentered({ x + cellWidth * i, relativePosition.y }, std::to_string(value->get()), 8, { textColor.color });
-                }
-            }
-        }
-    }
-
-public:
-    GraphComponent(ComponentInterface::Props props)
-        : GroupColorComponent(props, { { "FILL_COLOR", &fillColor }, { "OUTLINE_COLOR", &outlineColor }, { "TEXT_COLOR", &textColor } })
-        , bgColor(styles.colors.background)
-        , textColor(styles.colors.text, inactiveColorRatio)
-        , fillColor(styles.colors.primary, inactiveColorRatio)
-        , outlineColor(lighten(styles.colors.primary, 0.5), inactiveColorRatio)
-    {
-        updateColors();
-        waveformHeight = size.h - 9;
-    }
-
-    void render() override
-    {
-        if (updatePosition()) {
-            waveformY = relativePosition.y + 8;
-            draw.filledRect(relativePosition, size, { bgColor });
-            renderGraph();
-            renderTitles();
-        }
+        return points;
     }
 
     void onEncoder(int id, int8_t direction) override
@@ -118,14 +77,17 @@ public:
     }
 
     /*md **Config**: */
+    /*md - `PLUGIN: plugin_name` set plugin target */
+    /*md - `OUTLINE: true/false` is if the envelop should be outlined (default: true). */
+    /*md - `FILLED: true/false` is if the envelop should be filled (default: true). */
+    /*md - `BACKGROUND_COLOR: color` is the background color of the component. */
+    /*md - `FILL_COLOR: color` is the color of the envelop. */
+    /*md - `OUTLINE_COLOR: color` is the color of the envelop outline. */
+    /*md - `TEXT_COLOR1: color` is the color of the value. */
+    /*md - `TEXT_COLOR2: color` is the color of the unit. */
+    /*md - `INACTIVE_COLOR_RATIO: 0.0 - 1.0` is the ratio of darkness for the inactive color (default: 0.5). */
     bool config(char* key, char* value)
     {
-        /*md - `PLUGIN: plugin_name` set plugin target */
-        if (strcmp(key, "PLUGIN") == 0) {
-            plugin = &getPlugin(value, track);
-            return true;
-        }
-
         /*md - `DATA_ID: data_id` is the data id to get the shape/graph to draw.*/
         if (strcmp(key, "DATA_ID") == 0) {
             dataId = plugin->getDataId(value);
@@ -143,29 +105,7 @@ public:
             return true;
         }
 
-        /*md - `OUTLINE: true/false` is if the envelop should be outlined (default: true). */
-        if (strcmp(key, "OUTLINE") == 0) {
-            outline = strcmp(value, "true") == 0;
-            return true;
-        }
-
-        /*md - `FILLED: true/false` is if the envelop should be filled (default: true). */
-        if (strcmp(key, "FILLED") == 0) {
-            filled = strcmp(value, "true") == 0;
-            return true;
-        }
-
-        /*md - `BACKGROUND_COLOR: color` is the background color of the component. */
-        if (strcmp(key, "BACKGROUND_COLOR") == 0) {
-            bgColor = draw.getColor(value);
-            return true;
-        }
-
-        /*md - `FILL_COLOR: color` is the color of the envelop. */
-        /*md - `OUTLINE_COLOR: color` is the color of the envelop outline. */
-        /*md - `TEXT_COLOR: color` is the color of the text. */
-        /*md - `INACTIVE_COLOR_RATIO: 0.0 - 1.0` is the ratio of darkness for the inactive color (default: 0.5). */
-        return GroupColorComponent::config(key, value);
+        return BaseGraphComponent::config(key, value);
     }
 };
 
---
+++ plugins/components/Pixel/utils/BaseGraphComponent.h
@@ -0,0 +1,128 @@
+#ifndef _UI_PIXEL_COMPONENT_BASE_GRAPH_H_
+#define _UI_PIXEL_COMPONENT_BASE_GRAPH_H_
+
+#include "helpers/range.h"
+#include "plugins/components/component.h"
+#include "plugins/components/utils/color.h"
+#include "./GroupColorComponent.h"
+
+class BaseGraphComponent : public GroupColorComponent {
+protected:
+    AudioPlugin* plugin = NULL;
+
+    bool filled = true;
+    bool outline = true;
+
+    Color bgColor;
+
+    ToggleColor fillColor;
+    ToggleColor outlineColor;
+    ToggleColor textColor1;
+    ToggleColor textColor2;
+
+    int waveformHeight = 30;
+    int waveformY = 0;
+
+    void renderGraph()
+    {
+        std::vector<Point> relativePoints = getPoints();
+        if (relativePoints.size() > 2) {
+            float halfHeight = waveformHeight * 0.5;
+
+            for (auto& point : relativePoints) {
+                point.y += waveformY;
+                point.x += relativePosition.x;
+            }
+            if (filled) {
+                draw.filledPolygon(relativePoints, { fillColor.color });
+            }
+            if (outline) {
+                draw.lines(relativePoints, { outlineColor.color });
+            }
+        }
+    }
+
+    void renderTitles()
+    {
+        std::vector<Title> titles = getTitles();
+        int cellWidth = size.w / titles.size();
+        int x = relativePosition.x + cellWidth * 0.5;
+        for (int i = 0; i < titles.size(); i++) {
+            // draw.textCentered({ x + cellWidth * i, relativePosition.y }, titles[i][0], 8, { textColor1.color });
+            // draw.textCentered({ x + cellWidth * i, relativePosition.y }, titles[i][1], 8, { textColor2.color });
+            int len = titles[i].text1.length() + titles[i].text2.length();
+            int w = len * 8;
+            int x2 = draw.text({ (int)(x + cellWidth * i - w * 0.5), relativePosition.y }, titles[i].text1, 8, { textColor1.color });
+            draw.text({ x2 + w, relativePosition.y }, titles[i].text2, 8, { textColor2.color });
+        }
+    }
+
+public:
+    BaseGraphComponent(ComponentInterface::Props props)
+        : GroupColorComponent(props, { { "FILL_COLOR", &fillColor }, { "OUTLINE_COLOR", &outlineColor }, { "TEXT_COLOR1", &textColor1 }, { "TEXT_COLOR2", &textColor2 } })
+        , bgColor(styles.colors.background)
+        , textColor1(styles.colors.text, inactiveColorRatio)
+        , textColor2(darken(styles.colors.text, 0.5), inactiveColorRatio)
+        , fillColor(styles.colors.primary, inactiveColorRatio)
+        , outlineColor(lighten(styles.colors.primary, 0.5), inactiveColorRatio)
+    {
+        updateColors();
+        waveformHeight = size.h - 9;
+    }
+
+    struct Title {
+        std::string text1;
+        std::string text2 = "";
+    };
+
+    virtual std::vector<Title> getTitles() = 0;
+    virtual std::vector<Point> getPoints() = 0;
+
+    void render() override
+    {
+        if (updatePosition()) {
+            waveformY = relativePosition.y + 8;
+            draw.filledRect(relativePosition, size, { bgColor });
+            renderGraph();
+            renderTitles();
+        }
+    }
+
+    bool config(char* key, char* value)
+    {
+        if (strcmp(key, "PLUGIN") == 0) {
+            plugin = &getPlugin(value, track);
+            return true;
+        }
+
+        if (strcmp(key, "OUTLINE") == 0) {
+            outline = strcmp(value, "true") == 0;
+            return true;
+        }
+
+        if (strcmp(key, "FILLED") == 0) {
+            filled = strcmp(value, "true") == 0;
+            return true;
+        }
+
+        if (strcmp(key, "BACKGROUND_COLOR") == 0) {
+            bgColor = draw.getColor(value);
+            return true;
+        }
+
+        return GroupColorComponent::config(key, value);
+    }
+};
+
+/*.md **Config**: */
+/*.md - `PLUGIN: plugin_name` set plugin target */
+/*.md - `OUTLINE: true/false` is if the envelop should be outlined (default: true). */
+/*.md - `FILLED: true/false` is if the envelop should be filled (default: true). */
+/*.md - `BACKGROUND_COLOR: color` is the background color of the component. */
+/*.md - `FILL_COLOR: color` is the color of the envelop. */
+/*.md - `OUTLINE_COLOR: color` is the color of the envelop outline. */
+/*.md - `TEXT_COLOR1: color` is the color of the text1. */
+/*.md - `TEXT_COLOR2: color` is the color of the text2. */
+/*.md - `INACTIVE_COLOR_RATIO: 0.0 - 1.0` is the ratio of darkness for the inactive color (default: 0.5). */
+
+#endif
