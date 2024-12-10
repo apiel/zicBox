95106e99318d783e4c49c54b549602ec260706bc
apiel <alexandre.piel@gmail.com>
2024-12-10 18:35:05 +0100

    skip dustscript.so and make it part of the build

--- plugins/config/DustConfig.cpp
+++ plugins/config/DustConfig.cpp
@@ -1,33 +1,8 @@
-#include "../../dustscript/dustscript.h"
-
-#include <string>
-#include <vector>
-
-struct Var {
-    std::string key;
-    std::string value;
-};
+#include "./DustConfig.h"
 
 extern "C" {
-void config(std::string filename, void (*callback)(char* command, char* params, const char* filename), std::vector<Var> variables)
+void config(std::string filename, void (*callback)(char* command, char* params, const char* filename), std::vector<DustVar> variables)
 {
-    std::vector<DustScript::Variable> vars = { { "$IS_RPI",
-#ifdef IS_RPI
-        "true"
-#else
-        "false"
-#endif
-    } };
-
-    for (int i = 0; i < variables.size(); i++) {
-        vars.push_back({ "$" + variables[i].key, variables[i].value });
-    }
-
-    DustScript::load(
-        filename.c_str(),
-        [&callback](char* key, char* value, const char* filename, uint8_t indentation, DustScript& instance) {
-            callback(key, value, filename);
-        },
-        { .variables = vars });
+    dustConfig(filename, callback, variables);
 }
 }
