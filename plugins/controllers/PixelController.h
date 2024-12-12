5288d8a9fec1ed69d7c8857edf6bb337ec490973
apiel <alexandre.piel@gmail.com>
2024-12-12 15:39:40 +0100

    pixel controller

---
+++ plugins/controllers/PixelController.h
@@ -0,0 +1,73 @@
+#ifndef _PIXEL_CONTROLLER_H_
+#define _PIXEL_CONTROLLER_H_
+
+#include "controllerInterface.h"
+#include "helpers/controller.h"
+#include "helpers/gpio.h"
+
+#include <stdio.h>
+#include <thread>
+#include <vector>
+
+class PixelController : public ControllerInterface {
+protected:
+    std::thread loopThread;
+    bool loopRunning = true;
+
+    struct Key {
+        uint8_t pin;
+        int key;
+        uint8_t lastState = 0;
+    };
+
+    std::vector<Key> keys = {
+        { 13, getKeyCode("'q'") }, // pin 33 = gpio 13
+        { 7, getKeyCode("'w'") }, // pin 26 = gpio 7
+        { 25, getKeyCode("'e'") }, // pin 22 = gpio 25
+        { 23, getKeyCode("'r'") }, // pin 16 = gpio 23
+        { 8, getKeyCode("'t'") },// pin 8 = gpio 14 
+        { 16, getKeyCode("'a'") },// pin 36 = gpio 16 
+        { 12, getKeyCode("'s'") },// pin 32 = gpio 12
+        { 8, getKeyCode("'d'") },// pin 24 = gpio 8
+        { 24, getKeyCode("'f'") },// pin 18 = gpio 24
+        { 15, getKeyCode("'g'") },// pin 10 = gpio 15
+    };
+
+public:
+    PixelController(Props& props, uint16_t id)
+        : ControllerInterface(props, id)
+    {
+        if (initGpio() != -1) {
+            for (auto& key : keys) {
+                setGpioMode(key.pin, GPIO_OUTPUT);
+                key.lastState = getGpio(key.pin);
+            }
+
+            loopThread = std::thread(&PixelController::loop, this);
+        }
+    }
+
+    ~PixelController()
+    {
+        loopRunning = false;
+    }
+
+    void loop()
+    {
+        while (loopRunning) {
+            for (auto key : keys) {
+                uint8_t state = getGpio(key.pin);
+                if (state != key.lastState) {
+                    key.lastState = state;
+                    // controller->onKey(controller->id, key.key, state);
+                    printf("key [%d] state changed %d\n", key.key, state);
+                }
+                printf("[%d]=%d ", key.key, state);
+            }
+            printf("\n");
+            std::this_thread::sleep_for(1000ms);
+        }
+    }
+};
+
+#endif
