9c45f62d1d6b500a8c41964c5081cdb2753b784b
apiel <alexandre.piel@gmail.com>
2024-12-13 12:38:02 +0100

    cleanup

--- plugins/controllers/pixelController.cpp
+++ plugins/controllers/pixelController.cpp
@@ -1,149 +1,36 @@
-// g++ pixelController.cpp -o pixelController -I../.. -pthread && ./pixelController
+// g++ pixelController.cpp -o pixelController -I../.. -pthread && mv ./pixelController ~/pixelController && ~/pixelController
 
-// g++ pixelController.cpp -o pixelController -I../.. -pthread -lpigpio -lrt -DPIGPIO=1 && ./pixelController
+// g++ pixelController.cpp -o pixelControllerPigpio -I../.. -pthread -lpigpio -lrt -DPIGPIO=1 && mv ./pixelControllerPigpio ~/pixelControllerPigpio && sudo ~/pixelControllerPigpio
 
-#ifdef PIGPIO
-// sudo apt-get install libpigpio-dev
-#include <pigpio.h>
-#else
-#include "helpers/gpio.h"
-#endif
+#include "../../helpers/GpioKey.h"
 
-#include <stdio.h>
-#include <thread>
 #include <unistd.h>
-#include <vector>
-
-class PixelController {
-protected:
-    std::thread loopThread;
-    bool loopRunning = true;
-
-    struct Key {
-        uint8_t pin;
-        int key;
-        uint8_t lastState = 0;
-    };
-
-    std::vector<Key> keys = {
-        { 13, 0 }, // pin 33 = gpio 13
-        { 7, 1 }, // pin 26 = gpio 7
-        { 25, 2 }, // pin 22 = gpio 25
-        { 23, 3 }, // pin 16 = gpio 23
-        { 14, 4 }, // pin 8 = gpio 14
-        { 16, 5 }, // pin 36 = gpio 16
-        { 12, 6 }, // pin 32 = gpio 12
-        { 8, 7 }, // pin 24 = gpio 8
-        { 24, 8 }, // pin 18 = gpio 24
-        { 15, 9 }, // pin 10 = gpio 15
-    };
-
-public:
-    PixelController()
-    {
-#ifdef PIGPIO
-        if (gpioInitialise() < 0) {
-            return;
-        }
-#else
-        if (initGpio() == -1) {
-            return;
-        }
-#endif
-        for (auto& key : keys) {
-#ifdef PIGPIO
-            gpioSetMode(key.pin, PI_INPUT);
-            gpioSetPullUpDown(key.pin, PI_PUD_UP);
-            key.lastState = gpioRead(key.pin);
-#else
-            // gpioSetMode(key.pin, GPIO_INPUT);
-            // setGpio(key.pin);
-            gpioSetMode(key.pin, GPIO_INPUT);
-            gpioSetPullUpDown(key.pin);
-            key.lastState = gpioRead(key.pin);
-#endif
-        }
-
-        loopThread = std::thread(&PixelController::loop, this);
-    }
-
-    ~PixelController()
-    {
-        loopRunning = false;
-    }
-
-    void loop()
-    {
-        while (loopRunning) {
-            for (auto& key : keys) {
-#ifdef PIGPIO
-                uint8_t state = gpioRead(key.pin);
-#else
-                uint8_t state = gpioRead(key.pin);
-#endif
-                if (state != key.lastState) {
-                    key.lastState = state;
-                    // controller->onKey(controller->id, key.key, state);
-                    // printf("key [%d] state changed %d\n", key.key, state);
-                }
-                printf("[%d]=%d", key.pin, state);
-            }
-            printf("\n");
-            // std::this_thread::sleep_for(1000ms);
-            usleep(100);
-        }
-    }
-};
-
-// class PixelController {
-// protected:
-//     std::thread loopThread;
-//     bool loopRunning = true;
-
-//     struct Key {
-//         uint8_t pin;
-//         int key;
-//         uint8_t lastState = 0;
-//     };
-
-//     std::vector<Key> keys = {
-//         { 13, 0 }, // pin 33 = gpio 13
-//         { 7, 1 }, // pin 26 = gpio 7
-//         { 25, 2 }, // pin 22 = gpio 25
-//         { 23, 3 }, // pin 16 = gpio 23
-//         { 14, 4 }, // pin 8 = gpio 14
-//         { 16, 5 }, // pin 36 = gpio 16
-//         { 12, 6 }, // pin 32 = gpio 12
-//         { 8, 7 }, // pin 24 = gpio 8
-//         { 24, 8 }, // pin 18 = gpio 24
-//         { 15, 9 }, // pin 10 = gpio 15
-//     };
-
-// public:
-//     PixelController()
-//     {
-//         gpioInitialise();
-//         for (auto& key : keys) {
-//             gpioSetMode(key.pin, PI_INPUT);
-//             gpioSetPullUpDown(key.pin, PI_PUD_UP);
-//             gpioSetAlertFuncEx(key.pin, pulseEx, this);
-//         }
-//     }
-
-//     static void pulseEx(int gpio, int level, uint32_t tick, void* user)
-//     {
-//         // RotaryEncoder* self = (RotaryEncoder*)user;
-//         // self->pulse(gpio, level, tick);
-//         printf("key [%d] state changed %d\n", gpio, level);
-//     }
-// };
 
 int main(int argc, char* argv[])
 {
-    new PixelController();
+    GpioKey gpioKey({
+                        { 13, getKeyCode("'q'") }, // pin 33 = gpio 13
+                        { 7, getKeyCode("'w'") }, // pin 26 = gpio 7
+                        { 25, getKeyCode("'e'") }, // pin 22 = gpio 25
+                        { 23, getKeyCode("'r'") }, // pin 16 = gpio 23
+                        { 14, getKeyCode("'t'") }, // pin 8 = gpio 14
+                        { 16, getKeyCode("'a'") }, // pin 36 = gpio 16
+                        { 12, getKeyCode("'s'") }, // pin 32 = gpio 12
+                        { 8, getKeyCode("'d'") }, // pin 24 = gpio 8
+                        { 24, getKeyCode("'f'") }, // pin 18 = gpio 24
+                        { 15, getKeyCode("'g'") }, // pin 10 = gpio 15
+                    },
+        [](GpioKey::Key key, uint8_t state) {
+            //   printf("gpio%d key [%d] state changed %d\n", key.gpio, key.key, state);
+        });
+
+    if (gpioKey.init() == -1) {
+        return -1;
+    }
+    gpioKey.startThread();
 
     while (true) {
-        usleep(1);
+        usleep(1000);
     }
     return 0;
 }
