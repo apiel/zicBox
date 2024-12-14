bbc8b8e55353f094d6680f55edcf72dd2484c397
apiel <alexandre.piel@gmail.com>
2024-12-14 10:01:57 +0100

    fm2

--- plugins/audio/SynthFM2_2.h
+++ plugins/audio/SynthFM2_2.h
@@ -1,28 +1,21 @@
-#ifndef _SYNTH_FM_H_
-#define _SYNTH_FM_H_
+#ifndef _SYNTH_FM2_H_
+#define _SYNTH_FM2_H_
 
-#include "../../helpers/range.h"
 #include "audioPlugin.h"
+#include "helpers/range.h"
 #include "mapping.h"
 #include "utils/AdsrEnvelop.h"
 
 #define ZIC_FM_OPS_COUNT 4
 
-/*md
-## SynthFM
-
-Fm synth engine using 4 operators.
-
-Work in progress
-*/
-class SynthFM : public Mapping {
+class SynthFM2 : public Mapping {
 protected:
     struct FMoperator {
         Val attack;
         Val decay;
         Val sustain;
         Val release;
-        Val freq;
+        Val ratio; // Ratio for determining frequency based on mainFreq
         Val feedback;
         AdsrEnvelop envelop;
         float pitchedFreq = 0.0f;
@@ -36,7 +29,7 @@ protected:
             { 20.0f, "DECAY_0", { "Decay 1", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setDecay(p.value, 0); } },
             { 0.8f, "SUSTAIN_0", { "Sustain 1", .unit = "%" }, [&](auto p) { setSustain(p.value, 0); } },
             { 50.0f, "RELEASE_0", { "Release 1", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setRelease(p.value, 0); } },
-            { 440.0f, "FREQUENCY_0", { "Frequency 1", .min = 0.01, .max = 9000.0, .step = 1.05, .unit = "Hz", .incrementationType = VALUE_INCREMENTATION_EXP }, [&](auto p) { setFrequency(p.value, 0); } },
+            { 1.0f, "RATIO_0", { "Ratio 1", .min = 0.01, .max = 10.0, .step = 0.01, .unit = "x" }, [&](auto p) { setRatio(p.value, 0); } },
             { 0.0f, "FEEDBACK_0", { "Feedback 1", .unit = "%" } },
         },
         {
@@ -44,7 +37,7 @@ protected:
             { 20.0f, "DECAY_1", { "Decay 2", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setDecay(p.value, 1); } },
             { 0.8f, "SUSTAIN_1", { "Sustain 2", .unit = "%" }, [&](auto p) { setSustain(p.value, 1); } },
             { 50.0f, "RELEASE_1", { "Release 2", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setRelease(p.value, 1); } },
-            { 440.0f, "FREQUENCY_1", { "Frequency 2", .min = 0.01, .max = 9000.0, .step = 1.05, .unit = "Hz", .incrementationType = VALUE_INCREMENTATION_EXP }, [&](auto p) { setFrequency(p.value, 1); } },
+            { 1.0f, "RATIO_1", { "Ratio 2", .min = 0.01, .max = 10.0, .step = 0.01, .unit = "x" }, [&](auto p) { setRatio(p.value, 1); } },
             { 0.0f, "FEEDBACK_1", { "Feedback 2", .unit = "%" } },
         },
         {
@@ -52,7 +45,7 @@ protected:
             { 20.0f, "DECAY_2", { "Decay 3", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setDecay(p.value, 2); } },
             { 0.8f, "SUSTAIN_2", { "Sustain 3", .unit = "%" }, [&](auto p) { setSustain(p.value, 2); } },
             { 50.0f, "RELEASE_2", { "Release 3", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setRelease(p.value, 2); } },
-            { 440.0f, "FREQUENCY_2", { "Frequency 3", .min = 0.01, .max = 9000.0, .step = 1.05, .unit = "Hz", .incrementationType = VALUE_INCREMENTATION_EXP }, [&](auto p) { setFrequency(p.value, 2); } },
+            { 1.0f, "RATIO_2", { "Ratio 3", .min = 0.01, .max = 10.0, .step = 0.01, .unit = "x" }, [&](auto p) { setRatio(p.value, 2); } },
             { 0.0f, "FEEDBACK_2", { "Feedback 3", .unit = "%" } },
         },
         {
@@ -60,17 +53,28 @@ protected:
             { 20.0f, "DECAY_3", { "Decay 4", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setDecay(p.value, 3); } },
             { 0.8f, "SUSTAIN_3", { "Sustain 4", .unit = "%" }, [&](auto p) { setSustain(p.value, 3); } },
             { 50.0f, "RELEASE_3", { "Release 4", .min = 1.0, .max = 5000.0, .step = 20, .unit = "ms" }, [&](auto p) { setRelease(p.value, 3); } },
-            { 440.0f, "FREQUENCY_3", { "Frequency 4", .min = 0.01, .max = 9000.0, .step = 1.05, .unit = "Hz", .incrementationType = VALUE_INCREMENTATION_EXP }, [&](auto p) { setFrequency(p.value, 3); } },
+            { 1.0f, "RATIO_3", { "Ratio 4", .min = 0.01, .max = 10.0, .step = 0.01, .unit = "x" }, [&](auto p) { setRatio(p.value, 3); } },
             { 0.0f, "FEEDBACK_3", { "Feedback 4", .unit = "%" } },
         }
     };
 
-    void setFrequency(float value, unsigned int opIndex)
+    void setRatio(float value, unsigned int opIndex)
     {
-        operators[opIndex].freq.setFloat(value);
-        // operators[opIndex].stepIncrement = operators[opIndex].frequency.get() * 2.0f * M_PI / sampleRate;
-        operators[opIndex].pitchedFreq = operators[opIndex].freq.get() * pitchRatio;
+        operators[opIndex].ratio.setFloat(value);
+        updateOperatorFrequency(opIndex);
+    }
+
+    void updateOperatorFrequency(unsigned int opIndex)
+    {
+        operators[opIndex].pitchedFreq = mainFreq.get() * operators[opIndex].ratio.get();
         operators[opIndex].stepIncrement = props.lookupTable->size * operators[opIndex].pitchedFreq / props.sampleRate;
+    }
+
+    void updateOperatorFrequencies()
+    {
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            updateOperatorFrequency(i);
+        }
     }
 
     void setAttack(float ms, unsigned int opIndex)
@@ -102,13 +106,7 @@ protected:
     float pitchRatio = 1.0f;
 
 public:
-    // Apply modulation when true
-    // First operator does receive modulation input
-    // And last operator is in any case the carrier, so doesnt send modulation
-    // This is why we have [ZIC_FM_OPS_COUNT - 1][ZIC_FM_OPS_COUNT - 1]
     bool algorithm[12][ZIC_FM_OPS_COUNT - 1][ZIC_FM_OPS_COUNT - 1] = {
-        //     D            C            B
-        //  C, B, A      C, B, A      C, B, A
         { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } },
         { { 0, 1, 0 }, { 0, 1, 0 }, { 0, 0, 1 } },
         { { 0, 0, 1 }, { 0, 1, 0 }, { 0, 0, 1 } },
@@ -123,17 +121,16 @@ public:
         { { 0, 1, 0 }, { 0, 1, 0 }, { 0, 0, 0 } },
     };
 
-    /*md **Values**: */
-    /*md - `ALGO` set the FM algorithm.*/
     Val& algo = val(1, "ALGO", { "Algorithm", .min = 1, .max = sizeof(algorithm) / sizeof(algorithm[0]) });
+    Val& mainFreq = val(440.0f, "FREQUENCY", { "Frequency", .min = 20.0, .max = 5000.0, .step = 1.0, .unit = "Hz" });
 
-    SynthFM(AudioPlugin::Props& props, char* _name)
+    SynthFM2(AudioPlugin::Props& props, char* _name)
         : Mapping(props, _name, {
                                     // clang-format off
-             &operators[0].attack, &operators[0].decay, &operators[0].sustain, &operators[0].release, &operators[0].freq, &operators[0].feedback,
-             &operators[1].attack, &operators[1].decay, &operators[1].sustain, &operators[1].release, &operators[1].freq, &operators[1].feedback,
-             &operators[2].attack, &operators[2].decay, &operators[2].sustain, &operators[2].release, &operators[2].freq, &operators[2].feedback,
-             &operators[3].attack, &operators[3].decay, &operators[3].sustain, &operators[3].release, &operators[3].freq, &operators[3].feedback,
+                    &operators[0].attack, &operators[0].decay, &operators[0].sustain, &operators[0].release, &operators[0].ratio, &operators[0].feedback,
+                    &operators[1].attack, &operators[1].decay, &operators[1].sustain, &operators[1].release, &operators[1].ratio, &operators[1].feedback,
+                    &operators[2].attack, &operators[2].decay, &operators[2].sustain, &operators[2].release, &operators[2].ratio, &operators[2].feedback,
+                    &operators[3].attack, &operators[3].decay, &operators[3].sustain, &operators[3].release, &operators[3].ratio, &operators[3].feedback,
                                     // clang-format on
                                 })
     {
@@ -142,94 +139,96 @@ public:
         }
 
         initValues();
+
+        updateOperatorFrequencies();
     }
 
-    void sample(float* buf)
+    void noteOn(uint8_t note, float velocityValue) override
     {
-        uint8_t outDivider = 0;
-        float out = 0.0f;
+        baseNote = note;
+        velocity = velocityValue;
+        pitchRatio = powf(2.0f, (note - 69) / 12.0f); // Convert MIDI note to frequency ratio
+        // setMainFrequency(440.0f * pitchRatio);
+        updateOperatorFrequencies();
 
         for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
-            operators[i].mod = 0.0f;
-        }
-
-        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
-            FMoperator& op = operators[i];
-            float env = op.envelop.next();
-            if (env > 0.0f) {
-                if (op.mod == 0.0 && op.feedbackMod == 0.0) {
-                    op.index += op.stepIncrement;
-                } else {
-                    float freq = op.pitchedFreq + op.pitchedFreq * op.mod + op.pitchedFreq * op.feedbackMod;
-                    float inc = op.stepIncrement + props.lookupTable->size * freq / props.sampleRate; // TODO optimize with precomputing: props.lookupTable->size * op.freq.get() / props.sampleRate
-                    op.index += inc;
-                    // printf("[op %d] freq: %f, inc: %f, mod %f \n", i, freq, inc, mod);
-                    // mod = 0.0f;
-                }
-                while (op.index >= props.lookupTable->size) {
-                    op.index -= props.lookupTable->size;
-                }
-                float s = props.lookupTable->sine[(int)op.index] * env;
-
-                if (op.feedback.get() > 0.0f) {
-                    op.feedbackMod = s * op.feedback.pct();
-                }
-
-                if (i == ZIC_FM_OPS_COUNT - 1) { // Last operator can only be the carrier
-                    outDivider++;
-                    out += s;
-                    out = out / outDivider;
-                } else {
-                    bool isMod = false;
-                    for (int j = 0; j < ZIC_FM_OPS_COUNT - 1; j++) {
-                        if (algorithm[(uint8_t)(algo.get() - 1)][i][j]) {
-                            isMod = true;
-                            // j + 1 because the first operator doesnt receive modulation
-                            if (operators[j + 1].mod == 0.0f) {
-                                operators[j + 1].mod = s;
-                            } else {
-                                operators[j + 1].mod *= s;
-                            }
-                        }
-                    }
-                    if (!isMod) {
-                        outDivider++;
-                        out += s;
-                    }
-                }
-            }
-        }
-
-        buf[track] = out * velocity;
-    }
-
-    void noteOn(uint8_t note, float _velocity) override
-    {
-        velocity = _velocity;
-        pitchRatio = pow(2.0f, (note - baseNote) / 12.0f);
-        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            // operators[i].envelop.noteOn();
             operators[i].envelop.reset();
             operators[i].index = 0.0f;
-            operators[i].pitchedFreq = operators[i].freq.get() * pitchRatio;
-            operators[i].stepIncrement = props.lookupTable->size * operators[i].pitchedFreq / props.sampleRate;
         }
     }
 
     void noteOff(uint8_t note, float _velocity) override
     {
+        // printf("fm2 note off\n");
         for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            // operators[i].envelop.noteOff();
             operators[i].envelop.release();
         }
+    }
+
+    float process()
+    {
+        float output[ZIC_FM_OPS_COUNT] = { 0.0f, 0.0f, 0.0f, 0.0f };
+        bool anyActive = false;
+
+        // Calculate output for each operator
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            FMoperator& op = operators[i];
+            float env = op.envelop.next();
+            // printf("[%d] env: %f\n", i + 1, env);
+            if (env > 0.0f) {
+                anyActive = true;
+                if (op.feedback.get() > 0.0f) {
+                    op.feedbackMod = op.mod * op.feedback.pct();
+                }
+
+                float sampleValue = props.lookupTable->sine[(int)(op.index + op.feedbackMod)];
+                op.mod = sampleValue * env;
+
+                op.index += op.stepIncrement;
+                while (op.index >= props.lookupTable->size) {
+                    op.index -= props.lookupTable->size;
+                }
+
+                output[i] = operators[i].mod;
+            }
+        }
+
+        if (!anyActive) {
+            return 0.0f;
+        }
+
+        // Combine operator outputs based on the selected algorithm
+        float finalOutput = 0.0f;
+        auto& algoMatrix = algorithm[(uint8_t)(algo.get() - 1)];
+        for (int i = 0; i < ZIC_FM_OPS_COUNT - 1; i++) {
+            for (int j = 0; j < ZIC_FM_OPS_COUNT - 1; j++) {
+                if (algoMatrix[i][j]) {
+                    output[i] += output[j];
+                }
+            }
+        }
+
+        // Sum up all outputs
+        for (int i = 0; i < ZIC_FM_OPS_COUNT; i++) {
+            finalOutput += output[i];
+        }
+
+        return finalOutput;
+    }
+
+    void sample(float* buf)
+    {
+        buf[track] = process() * velocity;
     }
 
     enum DATA_ID {
         ALGO,
     };
 
-    /*md **Data ID**: */
     uint8_t getDataId(std::string name) override
     {
-        /*md - `ALGO` return current algorithm */
         if (name == "ALGO")
             return ALGO;
         return atoi(name.c_str());
@@ -246,4 +245,4 @@ public:
     }
 };
 
-#endif
+#endif // _SYNTH_FM2_H_
