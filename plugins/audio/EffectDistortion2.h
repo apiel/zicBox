ebaf93823eaaf7f86d4ee5c702700c36438ffb87
apiel <alexandre.piel@gmail.com>
2024-12-14 17:40:07 +0100

    distortion cleanup

--- plugins/audio/EffectDistortion2.h
+++ plugins/audio/EffectDistortion2.h
@@ -16,10 +16,16 @@ EffectDistortion2 plugin is used to apply distortion effect on audio buffer.
 
 class EffectDistortion2 : public Mapping {
 public:
+    /*md **Values**: */
+    /*md - `LEVEL` to set distortion mix level. */
     Val& level = val(50.0, "LEVEL", { "Dist. Level", .min = 0.0, .max = 100.0, .step = 1.0, .unit = "%" });
+    /*md - `DRIVE` to set distortion drive. */
     Val& drive = val(50.0, "DRIVE", { "Drive", .min = 0.0, .max = 100.0, .step = 1.0, .unit = "%" });
-    Val& compress = val(50.0, "COMPRESS", { "Compression", .min = 0.0, .max = 100.0, .step = 1.0, .unit = "%" });
+    /*md - `COMPRESS` to set distortion compression. */
+    Val& compress = val(50.0, "COMPRESS", { "Compression", .type = VALUE_CENTERED, .min = -100.0, .max = 100.0, .step = 1.0, .unit = "%" });
+    /*md - `BASS` to set bass boost. */
     Val& bass = val(50.0, "BASS", { "Bass Boost", .min = 0.0, .max = 100.0, .step = 1.0, .unit = "%" });
+    /*md - `WAVESHAPE` to set waveshape. */
     Val& waveshape = val(0.0, "WAVESHAPE", { "Waveshape", .min = 0.0, .max = 100.0, .step = 1.0, .unit = "%" });
 
     float prevInput1 = 0.0f, prevOutput1 = 0.0f; // State for pass 1
@@ -51,7 +57,7 @@ public:
         // Get parameters
         float levelAmount = level.pct();
         float driveAmount = drive.pct();
-        float compressAmount = compress.pct();
+        float compressAmount = compress.pct() * 2 - 1.0f;
         float bassBoostAmount = bass.pct();
         float waveshapeAmount = waveshape.pct();
 
@@ -88,6 +94,9 @@ protected:
 
     float applyBoost(float input, float bassBoostAmount, float& prevInput, float& prevOutput)
     {
+        if (bassBoostAmount == 0.0f) {
+            return input;
+        }
         float bassFreq = 0.2f + 0.8f * bassBoostAmount;
         float bassBoosted = (1.0f - bassFreq) * prevOutput + bassFreq * (input + prevInput) * 0.5f;
         prevInput = input;
@@ -99,11 +108,17 @@ protected:
 
     float applyDrive(float input, float driveAmount)
     {
+        if (driveAmount == 0.0f) {
+            return input;
+        }
         return tanhLookup(input * (1.0f + driveAmount * 5.0f));
     }
 
     float applyCompression(float input, float compressAmount)
     {
+        if (compressAmount == 0.0f) {
+            return input;
+        }
         if (input > 0.0f) {
             return std::pow(input, 1.0f - compressAmount * 0.8f);
         }
