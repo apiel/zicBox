d686847c7b0f653f34bcde42e8ff86e03252b91f
apiel <alexandre.piel@gmail.com>
2024-12-15 11:18:04 +0100

    lua

--- config/pixel/libs/component/text.lua
+++ config/pixel/libs/component/text.lua
@@ -1,18 +1,18 @@
 local ui = require "config/pixel/libs/ui"
 
 --- Create a text component
---- @param params {text: string } Params:
---- - text: The text to display
+--- @param params {TEXT: string } Params:
+--- - TEXT The text to display
 --- @param position {x: number, y: number, w: number, h: number} The position of the component.
---- @param options {center: boolean, fontSize: number, font: string, backgroundColor: string, color: string} Options:
---- - center: Whether to center the text (default: false)
---- - fontSize: The font size (default: 8)
---- - font: The font name (optional)
---- - backgroundColor: The background color (optional)
---- - color: The text color (optional)
+--- @param options {CENTERED: boolean, FONT_SIZE: number, FONT: string, BACKGROUND_COLOR: string, COLOR: string} Options:
+--- - CENTERED Whether to center the text (default: false)
+--- - FONT_SIZE The font size (default: 8)
+--- - FONT The font name (optional)
+--- - BACKGROUND_COLOR The background color (optional)
+--- - COLOR The text color (optional)
 local function text(params, position, options)
     zic("COMPONENT", "Text " .. ui.getComponentPosition(position))
-    zic("TEXT", params.text)
+    ui.parseParams(params, { "TEXT" })
     ui.parseOptions(options)
 end
 
