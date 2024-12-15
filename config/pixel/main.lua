3ba71a206c2e729da902bd205ad2f5a146dd94bb
apiel <alexandre.piel@gmail.com>
2024-12-15 17:56:29 +0100

    lua

--- config/pixel/main.lua
+++ config/pixel/main.lua
@@ -1,5 +1,41 @@
+local core = require("config/pixel/libs/core")
+
+---------------------------- AUDIO -----------------------------
 
 -- require("config/pixel/audio/main")
-zic("LOAD_CONFIG", "config/pixel/audio/main.cfg plugins/config/build/x86/libzic_DustConfig.so")
+core.loadDustConfig("config/pixel/audio/main.cfg")
+
+--------------------------- CONTROLLER -------------------------
+
+-- PLUGIN_CONTROLLER: MidiEncoder ./plugins/controllers/$buildDir/libzic_MidiEncoderController.so
+--   DEVICE: Arduino Leonardo:Arduino Leonardo MIDI 1
+--   ENCODER_TARGET: 7 0
+--   ENCODER_TARGET: 6 1
+--   ENCODER_TARGET: 5 2
+--   ENCODER_TARGET: 4 3
+--   ENCODER_TARGET: 3 4
+--   ENCODER_TARGET: 2 5
+--   ENCODER_TARGET: 1 6
+--   ENCODER_TARGET: 0 7
+
+-- core.controller(
+--     "MidiEncoder",
+--     "./plugins/controllers/$buildDir/libzic_MidiEncoderController.so", 
+--     {},
+--     {},
+--     {
+--         DEVICE = "Arduino Leonardo:Arduino Leonardo MIDI 1",
+--         ENCODER_TARGET = "7 0",
+--         ENCODER_TARGET = "6 1",
+--         ENCODER_TARGET = "5 2",
+--         ENCODER_TARGET = "4 3",
+--         ENCODER_TARGET = "3 4",
+--         ENCODER_TARGET = "2 5",
+--         ENCODER_TARGET = "1 6",
+--         ENCODER_TARGET = "0 7",
+--     }
+-- )
+
+------------------------------ UI ------------------------------
 
 require("config/pixel/ui/main")
--- config/pixel/ui/main.lua
+++ config/pixel/ui/main.lua
@@ -8,3 +8,91 @@ ScreenHeight = 240
 ui.setScreenSize(ScreenWidth, ScreenHeight)
 
 ui.view("Main")
+
+
+
+
+
+
+
+local text = require("config/pixel/libs/component/text")
+text(
+    { TEXT = "Hello world" },
+    { x = 0, y = 0, w = ScreenWidth, h = 8 },
+    { CENTERED = true, FONT_SIZE = 24 }
+)
+
+local adsr = require("config/pixel/libs/component/adsr")
+adsr(
+    { PLUGIN = "FM", VALUES = "ATTACK_0 DECAY_0 SUSTAIN_0 RELEASE_0" },
+    { x = 0, y = 20, w = ScreenWidth, h = 50 },
+    { TRACK = 5, GROUP = 1 }
+)
+
+local encoder3 = require("config/pixel/libs/component/encoder3")
+encoder3(
+    { ENCODER_ID = 0, VALUE = "MMFilter CUTOFF" },
+    { x = 0, y = 80, w = 50, h = 50 },
+    { TRACK = 2 }
+)
+
+local fmAlgo = require("config/pixel/libs/component/fmAlgo")
+fmAlgo(
+    { ENCODER_ID = 1, VALUE = "FM ALGO" },
+    { x = 60, y = 80, w = 50, h = 50 },
+    { TRACK = 4 }
+)
+
+local graph = require("config/pixel/libs/component/graph")
+graph(
+    { PLUGIN = "SynthDrum23", DATA_ID = "WAVEFORM" },
+    { x = 120, y = 80, w = 200, h = 50 },
+    { TRACK = 1, GROUP = 1, ENCODERS = { "0 WAVEFORM_TYPE", "1 SHAPE", "2 MACRO" } }
+)
+
+local keyInfoBar = require("config/pixel/libs/component/keyInfoBar")
+keyInfoBar(
+    {
+        ROW1 = "1 2 3 4 Shift",
+        ROW2 = "&icon::arrowDown &icon::musicNote::pixelated Live &icon::pause &icon::play"
+    },
+    { x = 0, y = 130 },
+    {
+        KEYMAPS = {
+            "Keyboard 'q' incGroup:-1",
+            "Keyboard 'w' setView:Sequencer1",
+            "Keyboard 'e' noteOn:SynthDrum23:58",
+            "Keyboard 'r' noteOn:SynthDrum23:59",
+            "Keyboard 't' shift:254:1:0",
+            "Keyboard 'a' incGroup:+1",
+            "Keyboard 's' noteOn:SynthDrum23:60",
+            "Keyboard 'd' noteOn:FM:61",
+            "Keyboard 'f' noteOn:FM:62",
+            { key = "g", action = "playPause" }
+        }
+    }
+)
+
+local stepEdit = require("config/pixel/libs/component/stepEdit")
+stepEdit(
+    {
+        DATA = { pluginName = "Sequencer", dataId = "GET_STEP", stepIndex = 0 },
+        COUNTER_DATA_ID = "STEP_COUNTER"
+    },
+    { x = 220, y = 180, w = 80, h = 25 },
+    {
+        GROUP = 0,
+        TRACK = 1,
+        GROUP_RANGE = { 0, 7 },
+        SEQUENCE_DATA_ID = "IS_PLAYING",
+        GLOBAL_SHIFT = 254
+    }
+)
+
+local drumEnvelop = require("config/pixel/libs/component/drumEnvelop")
+drumEnvelop(
+    { PLUGIN = "SynthDrum23", ENVELOP_DATA_ID = "0" },
+    { x = 0, y = 180, w = 200, h = 50 },
+    { GROUP = 0, TRACK = 1 }
+)
+
