5f62e343c6ad986a6cba205acd630c9cf4665e76
apiel <alexandre.piel@gmail.com>
2024-12-15 17:56:29 +0100

    lua

--- config/pixel/libs/core.lua
+++ config/pixel/libs/core.lua
@@ -13,37 +13,48 @@ local function parseValue(value)
     return value
 end
 
-
-local function parseKeyValue(key, value)
-    -- Handle KEYMAPS
-    if key == "KEYMAPS" then
-        if type(value) == "table" then
-            for _, v in ipairs(value) do
-                if type(v) == "string" then
-                    zic("KEYMAP", v)
-                else
-                    local controllerId = v.controller and v.controller or "Keyboard"
-                    local k = type(v.key) == "number" and v.key or "'" .. v.key .. "'"
-                    local action2 = v.action2 and " " .. v.action2 or ""
-                    zic("KEYMAP", controllerId .. " " .. k .. " " .. v.action .. action2)
-                end
+local function parseKeyMap(value)
+    local keyValues = {}
+    if type(value) == "table" then
+        for _, v in ipairs(value) do
+            if type(v) == "string" then
+                table.insert(keyValues, { key = "KEYMAP", value = v })
+            else
+                local controllerId = v.controller and v.controller or "Keyboard"
+                local k = type(v.key) == "number" and v.key or "'" .. v.key .. "'"
+                local action2 = v.action2 and " " .. v.action2 or ""
+                table.insert(keyValues,
+                    { key = "KEYMAP", value = controllerId .. " " .. k .. " " .. v.action .. action2 })
             end
         end
-        return
     end
+    return keyValues
+end
 
-    print(key .. ": " .. parseValue(value))
-    zic(key, parseValue(value))
+local function parseKeyValue(key, value)
+    if key == "KEYMAPS" then
+        return parseKeyMap(value)
+    end
+    return { { key = key, value = parseValue(value) } }
+end
+
+local function tableMerge(target, source)
+    for _, item in ipairs(source) do
+        table.insert(target, item)
+    end
+    return target
 end
 
 --- Parse options and apply them to zic configurations
 --- @param options { [string]: string | boolean | number | table } Options to apply
 function core.parseOptions(options)
+    local keyValues = {}
     if options ~= nil then
         for key, value in pairs(options) do
-            parseKeyValue(key, value)
+            tableMerge(keyValues, parseKeyValue(key, value))
         end
     end
+    return keyValues
 end
 
 --- Parse params and apply them to zic configurations
@@ -54,12 +65,14 @@ function core.parseParams(params, mandatory)
         error("Params cannot be nil", 2)
     end
 
+    local keyValues = {}
     for _, key in ipairs(mandatory) do
         if params[key] == nil then
             error("Mandatory param " .. key .. " is missing", 2)
         end
-        parseKeyValue(key, params[key])
+        tableMerge(keyValues, parseKeyValue(key, params[key]))
     end
+    return keyValues
 end
 
 --- Add a controller to the zic configuration
@@ -74,6 +87,8 @@ function core.controller(name, pluginPath, mandatoryParams, params, options)
     core.parseOptions(options)
 end
 
+--- Build the plateform
+--- @return string The plateform
 function core.buildPlateform()
     return IS_RPI and "arm" or "x86"
 end
@@ -82,6 +97,14 @@ end
 --- @param scriptPath string The path of the script
 function core.loadDustConfig(scriptPath)
     zic("LOAD_CONFIG", scriptPath .. " plugins/config/build/" .. core.buildPlateform() .. "/libzic_DustConfig.so")
+end
+
+--- Apply keyValues to zic
+--- @param keyValues table values to apply
+function core.zicKeyValues(keyValues)
+    for _, keyValue in ipairs(keyValues) do
+        zic(keyValue.key, keyValue.value)
+    end
 end
 
 return core
--- config/pixel/libs/ui.lua
+++ config/pixel/libs/ui.lua
@@ -35,12 +35,12 @@ end
 --- @param name string The name of the component
 --- @param mandatoryParams table Mandatory params key in right order
 --- @param params { [string]: string | boolean | number | table } Params to apply
---- @param position {x: number, y: number, w: number, h: number} | {x: number, y: number} The position of the component.
+--- @param position {x: number, y: number, w: number, h: number} | {x: number, y: number} The position of the component
 --- @param options { [string]: string | boolean | number | table } Options to apply
 function ui.component(name, mandatoryParams, params, position, options)
     zic("COMPONENT", name .. " " .. ui.parsePosition(position))
-    core.parseParams(params, mandatoryParams)
-    core.parseOptions(options)
+    core.zicKeyValues(core.parseParams(params, mandatoryParams))
+    core.zicKeyValues(core.parseOptions(options))
 end
 
 return ui
