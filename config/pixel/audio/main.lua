f00e823f0565583af9684f77ed697bb572983136
apiel <alexandre.piel@gmail.com>
2024-12-16 15:47:18 +0100

    lua audio plugin

--- config/pixel/audio/main.lua
+++ config/pixel/audio/main.lua
@@ -1,5 +1,6 @@
 local core = require("config/pixel/libs/core")
 local audio = require("config/pixel/libs/audio")
+local drum23 = require("config/pixel/audio/drum23_track")
 
 audio.pluginAlias("AudioOutput", "libzic_AudioOutputPulse.so")
 audio.pluginAlias("SynthDrum23", "libzic_SynthDrum23.so")
@@ -17,6 +18,11 @@ audio.pluginAlias("Tempo", "libzic_Tempo.so")
 audio.pluginAlias("SynthFM", "libzic_SynthFM2.so")
 
 audio.plugin("Tempo")
+
+-- # Drum tracks
+drum23(1)
+drum23(2)
+drum23(3)
 
 -- require("config/pixel/audio/main")
 core.loadDustConfig("config/pixel/audio/main.cfg")
