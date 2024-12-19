756eaac2cb4412fa686fa6239cf80c3dd962f203
apiel <alexandre.piel@gmail.com>
2024-12-19 20:18:58 +0100

    visibility

--- config/pixel/libs/containers/visibility.lua
+++ config/pixel/libs/containers/visibility.lua
@@ -3,10 +3,16 @@ local ui = require("config/pixel/libs/ui")
 
 --- Visibility is a container that show/hide the components for a given group index.
 --- @param name string The name of the container
---- @param group number The group index
-local function container(name, group)
+--- @param options { VISIBILITY_GROUP: number } | nil Options:
+--- - VISIBILITY_GROUP set the group index
+local function container(name, options)
     zic("CONTAINER", "VisibilityContainer " .. name .. " " .. ui.parsePosition({ 0, 0 }))
-    zic("VISIBILITY_GROUP", group)
+    if options ~= nil then
+        -- core.zic(core.parseOptions(options))
+        if options.VISIBILITY_GROUP ~= nil then
+            zic("VISIBILITY_GROUP", options.VISIBILITY_GROUP)
+        end
+    end
 end
 
 return container
