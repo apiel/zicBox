baff8c187f6411ae74da7133ad13da405e5e2365
apiel <alexandre.piel@gmail.com>
2024-12-23 13:37:20 +0100

    env config

--- config/pixel/320x240/ui/main.lua
+++ config/pixel/320x240/ui/main.lua
@@ -1,9 +1,9 @@
 local ui = require("config/pixel/libs/ui")
-local mainKeyboard = require("config/pixel/ui/keyboard_main")
-local sequencer = require("config/pixel/ui/sequencer")
-local drum23Synth = require("config/pixel/ui/drum23_synth")
-local fmSynth = require("config/pixel/ui/fm_synth")
-local drumSample = require("config/pixel/ui/drumSample")
+local mainKeyboard = require("config/pixel/320x240/ui/keyboard_main")
+local sequencer = require("config/pixel/320x240/ui/sequencer")
+local drum23Synth = require("config/pixel/320x240/ui/drum23_synth")
+local fmSynth = require("config/pixel/320x240/ui/fm_synth")
+local drumSample = require("config/pixel/320x240/ui/drumSample")
 
 ui.setWindowPosition(400, 500)
 
