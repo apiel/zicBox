631d8ad2c6774b91d528e7d8971a32e576311b51
apiel <alexandre.piel@gmail.com>
2024-12-23 13:24:10 +0100

    env config

--- pixel.lua
+++ pixel.lua
@@ -1,1 +1,6 @@
-require "config/pixel/main"
+local ok = pcall(require, "pixel_env")
+
+if not ok then
+    print("There is no pixel_env.lua, use default config.")
+    require "config/pixel/main"
+end
