454d2f2edde0cc1812af5791638beea611c415ff
apiel <alexandre.piel@gmail.com>
2024-12-23 15:23:46 +0100

    320x170

---
+++ config/pixel/320x170/ui/drum23_synth.lua
@@ -0,0 +1,177 @@
+local scrollGroup = require("config/pixel/libs/containers/scrollGroup")
+local text = require("config/pixel/libs/component/text")
+local encoder3 = require("config/pixel/libs/component/encoder3")
+local graph = require("config/pixel/libs/component/graph")
+local drumEnvelop = require("config/pixel/libs/component/drumEnvelop")
+
+local function drum23_synth(track)
+  scrollGroup("Scroll", { x = 0, y = 0, w = ScreenWidth, h = 214 }, {
+    SCROLL_TO_CENTER = true,
+  })
+
+  local sepHeight = 12
+  local rowHeight = 55
+
+  -- # set to - rowHeight so we easily schuffle thing around
+  local y = -(rowHeight)
+  local group = -1
+
+
+  -- #   Drum
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Drum (" .. track .. ")" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  encoder3(
+    { ENCODER_ID = 0, VALUE = "Volume VOLUME" },
+    { x = 0, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "secondary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 1, VALUE = "MMFilter CUTOFF" },
+    { x = W1_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "primary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 2, VALUE = "MMFilter RESONANCE" },
+    { x = W2_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "primary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Distortion LEVEL" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "tertiary", TRACK = track }
+  )
+
+  -- #   Distortion
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Distortion" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  encoder3(
+    { ENCODER_ID = 0, VALUE = "Distortion WAVESHAPE" },
+    { x = 0, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "tertiary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 1, VALUE = "Distortion DRIVE" },
+    { x = W1_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "tertiary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 2, VALUE = "Distortion COMPRESS" },
+    { x = W2_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "tertiary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Distortion BASS" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "tertiary", TRACK = track }
+  )
+
+  -- #   Waveform
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Waveform" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  graph(
+    { PLUGIN = "Drum23", DATA_ID = "WAVEFORM" },
+    { x = 0, y = y, w = W3_4 - 2, h = 50 },
+    { GROUP = group, TRACK = track, ENCODERS = { "0 WAVEFORM_TYPE", "1 SHAPE", "2 MACRO" } }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Drum23 GAIN_CLIPPING" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+
+  -- #   Amp envelope
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Amplitude" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  drumEnvelop(
+    { PLUGIN = "Drum23", ENVELOP_DATA_ID = "0" },
+    { x = 0, y = y, w = W3_4 - 2, h = 50 },
+    { GROUP = group, TRACK = track, ENCODERS = { "0 PHASE", "1 TIME", "2 MODULATION" } }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Drum23 DURATION" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+
+  -- #   Frequency envelope
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Frequency" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  drumEnvelop(
+    { PLUGIN = "Drum23", ENVELOP_DATA_ID = "4" },
+    { x = 0, y = y, w = W3_4 - 2, h = 50 },
+    { GROUP = group, TRACK = track, ENCODERS = { "0 PHASE", "1 TIME", "2 MODULATION" } }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Drum23 PITCH" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "secondary", TRACK = track }
+  )
+
+  -- #   Click
+  group = group + 1
+  y = y + rowHeight
+  text(
+    { TEXT = "Click transient" },
+    { x = 0, y = y, w = ScreenWidth, h = 8 },
+    { CENTERED = true, GROUP = group }
+  )
+
+  y = y + sepHeight
+  encoder3(
+    { ENCODER_ID = 0, VALUE = "Drum23 CLICK" },
+    { x = 0, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 1, VALUE = "Drum23 CLICK_DURATION" },
+    { x = W1_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 2, VALUE = "Drum23 CLICK_CUTOFF" },
+    { x = W2_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+  encoder3(
+    { ENCODER_ID = 3, VALUE = "Drum23 CLICK_RESONANCE" },
+    { x = W3_4, y = y, w = W1_4, h = 50 },
+    { GROUP = group, COLOR = "quaternary", TRACK = track }
+  )
+end
+
+return drum23_synth
---
+++ config/pixel/320x170/ui/drumSample.lua
@@ -0,0 +1,203 @@
+local ui = require("config/pixel/libs/ui")
+local text = require("config/pixel/libs/component/text")
+local encoder3 = require("config/pixel/libs/component/encoder3")
+local keyInfoBar = require("config/pixel/libs/component/keyInfoBar")
+local visibility = require("config/pixel/libs/containers/visibility")
+
+local function row(group, track, y)
+    local h = 15
+    ui.component("SeqSynthBar",
+        { "SEQ_PLUGIN" },
+        { SEQ_PLUGIN = "Sequencer", },
+        { x = 0, y = y, w = ScreenWidth, h = h }, {
+            GROUP = group,
+            TRACK = track,
+            NAME_COLOR = "#00b300",
+            NAME_PLUGIN = "DrumSample BROWSER",
+            VOLUME_PLUGIN = "Volume VOLUME",
+            KEYMAPS = {
+                { key = "q", action = "noteOn:DrumSample:57" },
+                -- { key = "e", action = ".stepToggle" },
+
+                { key = "a", action = ".left" },
+                { key = "d", action = ".right" },
+            }
+        })
+
+
+    return y + h + 2
+end
+
+local function progressBar(group, track, y)
+    local h = 5
+    ui.component("SeqProgressBar",
+        { "SEQ_PLUGIN" },
+        { SEQ_PLUGIN = "Sequencer 11", },
+        { x = 0, y = y, w = ScreenWidth, h = h }, {
+            GROUP = group,
+            TRACK = track,
+            ACTIVE_COLOR = "#00b300",
+            VOLUME_PLUGIN = "VolumeDrive VOLUME",
+            KEYMAPS = {
+                { key = "q", action = "noteOn:DrumSample:57:11" },
+                -- { key = "e", action = ".stepToggle" },
+
+                -- { key = "a", action = ".left" },
+                -- { key = "d", action = ".right" },
+            }
+        })
+
+    return y + h + 4
+end
+
+local function encoders(group, track, y)
+    visibility(
+        "DrumSample" .. track,
+        { x = 0, y = y, w = ScreenWidth, h = 50 },
+        { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 SHOW 0" }
+    )
+
+    encoder3(
+        { ENCODER_ID = 0, VALUE = "Volume VOLUME" },
+        { x = 0, y = 0, w = W1_4, h = 50 },
+        { COLOR = "primary", TRACK = track, GROUP = group }
+    )
+    encoder3(
+        { ENCODER_ID = 1, VALUE = "DrumSample START" },
+        { x = W1_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "secondary", TRACK = track, GROUP = group }
+    )
+    encoder3(
+        { ENCODER_ID = 2, VALUE = "DrumSample END" },
+        { x = W2_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "tertiary", TRACK = track, GROUP = group }
+    )
+    encoder3(
+        { ENCODER_ID = 3, VALUE = "DrumSample BROWSER" },
+        { x = W3_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "quaternary", TRACK = track, TYPE = "NUMBER", GROUP = group }
+    )
+
+    visibility(
+        "DrumSample" .. track,
+        { x = 0, y = y, w = ScreenWidth, h = 50 },
+        { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 HIDE 0" }
+    )
+
+    encoder3(
+        { ENCODER_ID = 0, VALUE = "Sequencer STEP_VELOCITY" },
+        { x = 0, y = 0, w = W1_4, h = 50 },
+        { COLOR = "primary", TRACK = track, GROUP = group }
+    )
+    encoder3(
+        { ENCODER_ID = 1, VALUE = "Sequencer STEP_CONDITION" },
+        { x = W1_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "secondary", TRACK = track, GROUP = group }
+    )
+    encoder3(
+        { ENCODER_ID = 2, VALUE = "Sequencer STEP_ENABLED" },
+        { x = W2_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "tertiary", TRACK = track, GROUP = group }
+    )
+    -- encoder3(
+    --     { ENCODER_ID = 3, VALUE = "Volume VOLUME" },
+    --     { x = W3_4, y = 0, w = W1_4, h = 50 },
+    --     { COLOR = "quaternary", TRACK = track, GROUP = group }
+    -- )
+end
+
+local function encodersMaster(group, track, y)
+    visibility("DrumSampleMaster", { x = 0, y = y, w = ScreenWidth, h = 50 }, { VISIBILITY_GROUP = group })
+
+    encoder3(
+        { ENCODER_ID = 0, VALUE = "VolumeDrive VOLUME" },
+        { x = 0, y = 0, w = W1_4, h = 50 },
+        { COLOR = "tertiary", TRACK = track, GROUP = group, USE_SECOND_COLOR = 0.5 }
+    )
+    encoder3(
+        { ENCODER_ID = 1, VALUE = "VolumeDrive DRIVE" },
+        { x = W1_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "secondary", TRACK = track, GROUP = group, TYPE = "TWO_VALUES" }
+    )
+    encoder3(
+        { ENCODER_ID = 2, VALUE = "MMFilter CUTOFF" },
+        { x = W2_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "primary", TRACK = track, GROUP = group, TYPE = "TWO_SIDED" }
+    )
+    encoder3(
+        { ENCODER_ID = 3, VALUE = "MMFilter RESONANCE" },
+        { x = W3_4, y = 0, w = W1_4, h = 50 },
+        { COLOR = "primary", TRACK = track, GROUP = group }
+    )
+end
+
+local function drumSample(track)
+    text(
+        { TEXT = "Drum Sample(1)" },
+        { x = 0, y = 0, w = ScreenWidth, h = 8 },
+        { CENTERED = true }
+    )
+
+    keyInfoBar(
+        {
+            "&icon::musicNote::pixelated &icon::arrowUp::filled ...",
+            "&icon::arrowLeft::filled &icon::arrowDown::filled &icon::arrowRight::filled"
+        }, KeyInfoPosition,
+        {
+            SHIFT_VISIBILITY = { index = 254, value = 0 },
+            KEYMAPS = {
+                { key = "w", action = "incGroup:-1" },
+                { key = "e", action = "shift:254:1:0" },
+
+                { key = "s", action = "incGroup:+1" },
+
+                -- { key = "f", action = "noteOn:DrumSample:62" },
+                -- { key = "g", action = "playPause" }
+            }
+        }
+    )
+
+    keyInfoBar(
+        {
+            "&icon::play::filled &icon::toggle::rect ...",
+            "Menu Sub Master"
+        }, KeyInfoPosition,
+        {
+            SHIFT_VISIBILITY = { index = 254, value = 1 },
+            KEYMAPS = {
+                { key = "q", action = "playPause" },
+                -- { key = "w", action = "setView:Track2" }, // stepToggle
+                { key = "e", action = "shift:254:1:0" },
+                { key = "a", action = "setView:menu" },
+                { key = "s", action = "setView:sub" .. track },
+                { key = "d", action = "setView:Master" },
+            }
+        }
+    )
+
+    local y = 0
+
+    y = 70
+    y = progressBar(0, track, y)
+    y = row(1, 11, y)
+    y = row(2, 12, y)
+    y = row(3, 13, y)
+    -- y = row(4, 14, y)
+    -- y = row(5, 15, y)
+    -- y = row(6, 16, y)
+    -- y = row(7, 17, y)
+    -- y = row(8, 18, y)
+
+    y = 15
+    encodersMaster(0, track, y)
+    encoders(1, 11, y)
+    encoders(2, 12, y)
+    encoders(3, 13, y)
+    -- encoders(4, 14, y)
+    -- encoders(5, 15, y)
+    -- encoders(6, 16, y)
+    -- encoders(7, 17, y)
+    -- encoders(8, 18, y)
+end
+
+return drumSample
