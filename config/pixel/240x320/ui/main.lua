0bd7e800f4a0f2b59ce3793d457cf6286d02e2b2
apiel <alexandre.piel@gmail.com>
2024-12-25 22:10:21 +0100

    seqSynthBar

--- config/pixel/240x320/ui/main.lua
+++ config/pixel/240x320/ui/main.lua
@@ -3,7 +3,7 @@ local ui = require("config/pixel/libs/ui")
 -- local sequencer = require("config/pixel/240x320/ui/sequencer")
 -- local drum23Synth = require("config/pixel/240x320/ui/drum23_synth")
 -- local fmSynth = require("config/pixel/240x320/ui/fm_synth")
-local drumSample = require("config/pixel/240x320/ui/drumSample")
+local mainView = require("config/pixel/240x320/ui/mainView")
 
 ui.setWindowPosition(400, 500)
 
@@ -29,54 +29,5 @@ ui.addZoneEncoder({ W1_4, 0, W1_4, ScreenHeight })
 ui.addZoneEncoder({ W1_2, 0, W1_4, ScreenHeight })
 ui.addZoneEncoder({ W3_4, 0, W1_4, ScreenHeight })
 
-local track = 0
-
--- - Track Sample (1)
-track = 1
-ui.view("Track" .. track) -- , { COMPONENTS_TRACK = track }
-drumSample(track)
-
--- -- - Track Drum23 (2)
--- track = 2
--- ui.view("Track" .. track, { COMPONENTS_TRACK = track })
--- mainKeyboard(track, "Drum23")
--- drum23Synth(track)
-
--- ui.view("Sequencer" .. track, { COMPONENTS_TRACK = track })
--- sequencer(track)
-
--- -- - Track Drum23 (3)
--- track = 3
--- ui.view("Track" .. track, { COMPONENTS_TRACK = track })
--- mainKeyboard(track, "Drum23")
--- drum23Synth(track)
-
--- ui.view("Sequencer" .. track, { COMPONENTS_TRACK = track })
--- sequencer(track)
-
--- -- - Track Drum23 (4)
--- track = 4
--- ui.view("Track" .. track, { COMPONENTS_TRACK = track })
--- mainKeyboard(track, "Drum23")
--- drum23Synth(track)
-
--- ui.view("Sequencer" .. track, { COMPONENTS_TRACK = track })
--- sequencer(track)
-
--- -- # - Track FM (5)
--- track = 5
--- ui.view("Track" .. track, { COMPONENTS_TRACK = track })
--- mainKeyboard(track, "FM")
--- fmSynth(track)
-
--- ui.view("Sequencer" .. track, { COMPONENTS_TRACK = track })
--- sequencer(track)
-
--- -- # - Track FM (6)
--- track = 6
--- ui.view("Track" .. track, { COMPONENTS_TRACK = track })
--- mainKeyboard(track, "FM")
--- fmSynth(track)
-
--- ui.view("Sequencer" .. track, { COMPONENTS_TRACK = track })
--- sequencer(track)
+ui.view("Main")
+mainView()
