dec9a01cde16d333f678921cf55acbd02ec10915
apiel <alexandre.piel@gmail.com>
2024-12-26 11:27:07 +0100

    valueComponent

--- config/pixel/240x320/ui/mainView/values.lua
+++ config/pixel/240x320/ui/mainView/values.lua
@@ -11,10 +11,35 @@ local function bottomRight() return { x = W2_4, y = valueH + 1, w = W2_4 - 2, h 
 
 local values = {}
 
-local function primary(options)
+local function base(options, group, track, encoderId)
+    options.ENCODER_ID = encoderId
+    options.TRACK = track
+    options.GROUP = group
+    return options
+end
+
+local function primary(options, group, track, encoderId)
     options.LABEL_COLOR = "primary"
     options.BAR_COLOR = PrimaryBar
-    return options
+    return base(options, group, track, encoderId)
+end
+
+local function secondary(options, group, track, encoderId)
+    options.LABEL_COLOR = "secondary"
+    options.BAR_COLOR = SecondaryBar
+    return base(options, group, track, encoderId)
+end
+
+local function tertiary(options, group, track, encoderId)
+    options.LABEL_COLOR = "tertiary"
+    options.BAR_COLOR = TertiaryBar
+    return base(options, group, track, encoderId)
+end
+
+local function quaternary(options, group, track, encoderId)
+    options.LABEL_COLOR = "quaternary"
+    options.BAR_COLOR = QuaternaryBar
+    return base(options, group, track, encoderId)
 end
 
 function values.main(group, track, y)
@@ -24,14 +49,10 @@ function values.main(group, track, y)
         { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 SHOW_WHEN 0" }
     )
 
-    value("Volume VOLUME", topLeft(),
-        { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("DrumSample START", bottomLeft(),
-        primary({ ENCODER_ID = 1, TRACK = track, GROUP = group }))
-    value("DrumSample BROWSER", topRight(),
-        { LABEL_COLOR = "secondary", BAR_COLOR = SecondaryBar, ENCODER_ID = 2, TRACK = track, GROUP = group, SHOW_LABEL = false, })
-    value("DrumSample END", bottomRight(),
-        { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
+    value("Volume VOLUME", topLeft(), tertiary({}, group, track, 0))
+    value("DrumSample START", bottomLeft(), primary({}, group, track, 1))
+    value("DrumSample BROWSER", topRight(), secondary({ SHOW_LABEL = false, }, group, track, 2))
+    value("DrumSample END", bottomRight(), quaternary({}, group, track, 3))
 
     visibility(
         "DrumSample" .. track,
@@ -39,35 +60,19 @@ function values.main(group, track, y)
         { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 SHOW_WHEN_OVER 0" }
     )
 
-    value("Sequencer STEP_VELOCITY", topLeft(),
-        { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("Sequencer STEP_CONDITION", bottomLeft(),
-        { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
-    value("Sequencer STEP_ENABLED", topRight(),
-        { LABEL_COLOR = "secondary", BAR_COLOR = SecondaryBar, ENCODER_ID = 2, TRACK = track, GROUP = group })
-    -- value("Volume VOLUME", bottomRight(),
-    --     { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
+    value("Sequencer STEP_VELOCITY", topLeft(), tertiary({}, group, track, 0))
+    value("Sequencer STEP_CONDITION", bottomLeft(), primary({}, group, track, 1))
+    value("Sequencer STEP_ENABLED", topRight(), secondary({}, group, track, 2))
+    -- value("Volume VOLUME", bottomRight(), quaternary({}, group, track, 3))
 end
 
 function values.master(group, track, y)
     visibility("DrumSampleMaster", { x = 0, y = y, w = ScreenWidth, h = valueH * 2 + 2 }, { VISIBILITY_GROUP = group })
 
-    value("VolumeDrive VOLUME", topLeft(),
-        { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("VolumeDrive DRIVE", bottomLeft(),
-        { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
-    value("MMFilter CUTOFF", topRight(),
-        {
-            LABEL_COLOR = "secondary",
-            BAR_COLOR = SecondaryBar,
-            USE_STRING_VALUE = true,
-            SHOW_LABEL = false,
-            ENCODER_ID = 2,
-            TRACK = track,
-            GROUP = group
-        })
-    value("MMFilter RESONANCE", bottomRight(),
-        { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
+    value("VolumeDrive VOLUME", topLeft(), tertiary({}, group, track, 0))
+    value("VolumeDrive DRIVE", bottomLeft(), primary({}, group, track, 1))
+    value("MMFilter CUTOFF", topRight(), secondary({ USE_STRING_VALUE = true, SHOW_LABEL = false }, group, track, 2))
+    value("MMFilter RESONANCE", bottomRight(), quaternary({}, group, track, 3))
 end
 
 return values
