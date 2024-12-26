ca8e35e331baf2e7665bd50faa910bc13865ade2
apiel <alexandre.piel@gmail.com>
2024-12-26 11:15:05 +0100

    valueComponent

--- config/pixel/240x320/ui/mainView/values.lua
+++ config/pixel/240x320/ui/mainView/values.lua
@@ -1,10 +1,17 @@
 local visibility = require("config/pixel/libs/containers/visibility")
 local value = require("config/pixel/libs/component/value")
 
-local values = {}
-
 -- local valueH = 11
 local valueH = 25
+
+local values = {}
+
+local function primary(options)
+    options.LABEL_COLOR = "primary"
+    options.BAR_COLOR = PrimaryBar
+    return options
+end
+
 function values.main(group, track, y)
     visibility(
         "DrumSample" .. track,
@@ -15,7 +22,7 @@ function values.main(group, track, y)
     value("Volume VOLUME", { x = 0, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
     value("DrumSample START", { x = 0, y = valueH + 1, w = W2_4 - 2, h = valueH },
-        { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
+        primary({ ENCODER_ID = 1, TRACK = track, GROUP = group }))
     value("DrumSample BROWSER", { x = W2_4, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "secondary", BAR_COLOR = SecondaryBar, ENCODER_ID = 2, TRACK = track, GROUP = group, SHOW_LABEL = false, })
     value("DrumSample END", { x = W2_4, y = valueH + 1, w = W2_4 - 2, h = valueH },
