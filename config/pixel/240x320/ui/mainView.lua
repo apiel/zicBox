ad056aa44f9b5e3f0b08f03bfcc054faf90ff65f
apiel <alexandre.piel@gmail.com>
2024-12-26 10:43:33 +0100

    valueComponent

--- config/pixel/240x320/ui/mainView.lua
+++ config/pixel/240x320/ui/mainView.lua
@@ -51,46 +51,47 @@ local function progressBar(group, track, y)
     return y + 4 + progressH
 end
 
+local valueH = 11
 local function encoders(group, track, y)
     visibility(
         "DrumSample" .. track,
-        { x = 0, y = y, w = ScreenWidth, h = 24 },
+        { x = 0, y = y, w = ScreenWidth, h = valueH * 2 + 2 },
         { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 SHOW_WHEN 0" }
     )
 
-    value("Volume VOLUME", { x = 0, y = 0, w = W2_4 - 2, h = 11 },
+    value("Volume VOLUME", { x = 0, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("DrumSample START", { x = 0, y = 12, w = W2_4 - 2, h = 11 },
+    value("DrumSample START", { x = 0, y = 12, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
-    value("DrumSample BROWSER", { x = W2_4, y = 0, w = W2_4 - 2, h = 11 },
+    value("DrumSample BROWSER", { x = W2_4, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "secondary", BAR_COLOR = SecondaryBar, ENCODER_ID = 2, TRACK = track, GROUP = group, SHOW_LABEL = false, })
-    value("DrumSample END", { x = W2_4, y = 12, w = W2_4 - 2, h = 11 },
+    value("DrumSample END", { x = W2_4, y = 12, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
 
     visibility(
         "DrumSample" .. track,
-        { x = 0, y = y, w = ScreenWidth, h = 24 },
+        { x = 0, y = y, w = ScreenWidth, h = valueH * 2 + 2 },
         { VISIBILITY_GROUP = group, VISIBILITY_CONTEXT = "10 SHOW_WHEN_OVER 0" }
     )
 
-    value("Sequencer STEP_VELOCITY", { x = 0, y = 0, w = W2_4 - 2, h = 11 },
+    value("Sequencer STEP_VELOCITY", { x = 0, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("Sequencer STEP_CONDITION", { x = 0, y = 12, w = W2_4 - 2, h = 11 },
+    value("Sequencer STEP_CONDITION", { x = 0, y = 12, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
-    value("Sequencer STEP_ENABLED", { x = W2_4, y = 0, w = W2_4 - 2, h = 11 },
+    value("Sequencer STEP_ENABLED", { x = W2_4, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "secondary", BAR_COLOR = SecondaryBar, ENCODER_ID = 2, TRACK = track, GROUP = group })
-    -- value("Volume VOLUME", { x = W2_4, y = 12, w = W2_4 - 2, h = 11 },
+    -- value("Volume VOLUME", { x = W2_4, y = 12, w = W2_4 - 2, h = valueH },
     --     { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
 end
 
 local function encodersMaster(group, track, y)
-    visibility("DrumSampleMaster", { x = 0, y = y, w = ScreenWidth, h = 50 }, { VISIBILITY_GROUP = group })
+    visibility("DrumSampleMaster", { x = 0, y = y, w = ScreenWidth, h = valueH * 2 + 2 }, { VISIBILITY_GROUP = group })
 
-    value("VolumeDrive VOLUME", { x = 0, y = 0, w = W2_4 - 2, h = 11 },
+    value("VolumeDrive VOLUME", { x = 0, y = 0, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "tertiary", BAR_COLOR = TertiaryBar, ENCODER_ID = 0, TRACK = track, GROUP = group })
-    value("VolumeDrive DRIVE", { x = 0, y = 12, w = W2_4 - 2, h = 11 },
+    value("VolumeDrive DRIVE", { x = 0, y = 12, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "primary", BAR_COLOR = PrimaryBar, ENCODER_ID = 1, TRACK = track, GROUP = group })
-    value("MMFilter CUTOFF", { x = W2_4, y = 0, w = W2_4 - 2, h = 11 },
+    value("MMFilter CUTOFF", { x = W2_4, y = 0, w = W2_4 - 2, h = valueH },
         {
             LABEL_COLOR = "secondary",
             BAR_COLOR = SecondaryBar,
@@ -100,7 +101,7 @@ local function encodersMaster(group, track, y)
             TRACK = track,
             GROUP = group
         })
-    value("MMFilter RESONANCE", { x = W2_4, y = 12, w = W2_4 - 2, h = 11 },
+    value("MMFilter RESONANCE", { x = W2_4, y = 12, w = W2_4 - 2, h = valueH },
         { LABEL_COLOR = "quaternary", BAR_COLOR = QuaternaryBar, ENCODER_ID = 3, TRACK = track, GROUP = group })
 end
 
