4ecc7634973510511d33dac06e35a796d358b672
apiel <alexandre.piel@gmail.com>
2024-12-28 20:49:15 +0100

    fonts

--- draw/ST7789/fonts/fonts.h
+++ draw/ST7789/fonts/fonts.h
@@ -6,7 +6,6 @@
 #include "BigFont.h"
 #include "MusicNote.h"
 #include "Sinclair_M.h"
-#include "Sinclair_12.h"
 #include "Sinclair_S.h"
 #include "Ubuntu.h"
 #include "UbuntuBold.h"
@@ -29,8 +28,6 @@ uint8_t* getFontPtr(const char* name = nullptr)
         return Sinclair_M;
     } else if (strcmp(name, "Sinclair_S") == 0) {
         return Sinclair_S;
-    } else if (strcmp(name, "Sinclair_12") == 0) {
-        return Sinclair_12;
     } else if (strcmp(name, "MusicNote") == 0) {
         return FontMusicNote;
     } else if (strcmp(name, "Ubuntu") == 0) {
