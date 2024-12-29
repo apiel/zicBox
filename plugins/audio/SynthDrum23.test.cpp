c252c6b152d39a4c853dad1a2cf07d00c2480000
apiel <alexandre.piel@gmail.com>
2024-12-29 18:20:01 +0100

    test

--- plugins/audio/SynthDrum23.test.cpp
+++ plugins/audio/SynthDrum23.test.cpp
@@ -5,13 +5,20 @@
 #define MAX_TRACKS 16
 #define TWO_PI 6.283185307179586
 
-// g++ test.cpp -o test -I../../.. -pthread -D_REENTRANT -lpulse-simple -lpulse -pthread && ./test
-// g++ plugins/audio/test.cpp -o test -I./ -pthread -D_REENTRANT -lpulse-simple -lpulse -pthread && ./test
-
-// g++ plugins/audio/test.cpp -o test -I./ -pthread -D_REENTRANT -lpulse-simple -lpulse -I/usr/include/opus -I/usr/include/x86_64-linux-gnu -lsndfile  && ./test
+// g++ plugins/audio/SynthDrum23.test.cpp -o test -I./ -pthread -D_REENTRANT -lpulse-simple -lpulse -I/usr/include/opus -I/usr/include/x86_64-linux-gnu -lsndfile  && ./test
 
 
 int noDebug(const char* format, ...) { return 0; }
+
+void hydate(AudioPlugin& plugin, std::string configs) {
+    std::istringstream stream(configs);
+    std::string line;
+    while (std::getline(stream, line)) {
+        if (!line.empty()) {
+        plugin.hydrate(line);
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -28,18 +35,19 @@ int main(int argc, char* argv[])
     AudioOutputPulse audioOutput(props, (char*)"zicAudioOutputPulse");
     SynthDrum23 drum23(props, (char*)"zicSynthDrum23");
 
-    drum23.hydrate("WAVEFORM_TYPE 1.000000");
-    drum23.hydrate("SHAPE 0.000000");
-    drum23.hydrate("MACRO 0.000000");
-    drum23.hydrate("PITCH -29.000000");
-    drum23.hydrate("DURATION 1550.000000");
-    drum23.hydrate("GAIN_CLIPPING 20.000000");
-    drum23.hydrate("CLICK 25.000000");
-    drum23.hydrate("CLICK_DURATION 7.000000");
-    drum23.hydrate("CLICK_CUTOFF 74.000000");
-    drum23.hydrate("CLICK_RESONANCE 66.000000");
-    drum23.hydrate("ENV_AMP 0.000000:0.000000 1.000000:0.000000 0.990000:0.110000 0.290000:0.560000 0.000000:1.000000");
-    drum23.hydrate("ENV_FREQ 1.000000:0.000000 0.560000:0.020000 0.240000:0.130000 0.140000:0.410000 0.090000:1.000000");
+    hydate(drum23, R"(
+WAVEFORM_TYPE 1.000000
+SHAPE 0.000000
+MACRO 0.000000
+PITCH -29.000000
+DURATION 1550.000000
+GAIN_CLIPPING 20.000000
+CLICK 25.000000
+CLICK_DURATION 7.000000
+CLICK_CUTOFF 74.000000
+CLICK_RESONANCE 66.000000
+ENV_AMP 0.000000:0.000000 1.000000:0.000000 0.990000:0.110000 0.290000:0.560000 0.000000:1.000000
+ENV_FREQ 1.000000:0.000000 0.560000:0.020000 0.240000:0.130000 0.140000:0.410000 0.090000:1.000000)");
 
     float buffer[MAX_TRACKS] = { 0.0f };
 
