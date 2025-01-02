39bcfb71ca0d744c90b4d48cc793d302f05b605d
apiel <alexandre.piel@gmail.com>
2025-01-02 15:43:29 +0100

    projects

--- config/pixel/240x320/audio/drumSample_track.lua
+++ config/pixel/240x320/audio/drumSample_track.lua
@@ -3,7 +3,7 @@ local audio = require("config/pixel/libs/audio")
 local function synth(track)
     audio.plugin("DrumSample", { TRACK = track })
     audio.plugin("Sequencer", { TRACK = track })
-    audio.plugin("SerializeTrack", { TRACK = track, FILEPATH = "serializedPixel2/track_" .. track .. ".cfg", MAX_VARIATION = MaxVariation })
+    audio.plugin("SerializeTrack", { TRACK = track, FILENAME = "track_" .. track, MAX_VARIATION = MaxVariation })
     audio.plugin("EffectSampleRateReducer", { NAME = "SampleRateReducer", TRACK = track })
     audio.plugin("EffectVolumeClipping", { NAME = "Volume", TRACK = track })
 end
