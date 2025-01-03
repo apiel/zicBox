5d7c6abd7eacbff4822b778862719fb09aad1841
apiel <alexandre.piel@gmail.com>
2025-01-03 15:17:32 +0100

    ts2lua

--- config/pixel/ts/react.ts
+++ config/pixel/ts/react.ts
@@ -1,15 +1,7 @@
 
 
 export function createElement(component: Function, props?: Object, ...children: any[]) {
-    // console.log(typeof component, typeof props, typeof children);
     const element = component(props);
-    if (!element) {
-        return;
-    }
-    if (typeof element.component === 'function') {
-        return createElement(element.component, element.props, ...children);
-    }
-
     return [...element, ...children.flat()];
 }
 
