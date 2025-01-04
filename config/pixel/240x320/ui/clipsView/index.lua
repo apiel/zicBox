0f2c65c5454a9e4091887e6bbb8343d2c4d27e51
apiel <alexandre.piel@gmail.com>
2025-01-04 19:37:45 +0100

    tslua

--- config/pixel/240x320/ui/clipsView/index.lua
+++ config/pixel/240x320/ui/clipsView/index.lua
@@ -18,6 +18,9 @@ local ColorTrack7 = ____constants.ColorTrack7
 local ColorTrack8 = ____constants.ColorTrack8
 local ____Clips = require("config.pixel.240x320.ui.clipsView.Clips")
 local Clips = ____Clips.Clips
+local ____TextGrid = require("config.pixel.240x320.ui.clipsView.TextGrid")
+local TextGridClips = ____TextGrid.TextGridClips
+local TextGridClipsShifted = ____TextGrid.TextGridClipsShifted
 function ____exports.ClipsView(____bindingPattern0)
     local name
     name = ____bindingPattern0.name
@@ -26,14 +29,16 @@ function ____exports.ClipsView(____bindingPattern0)
         {name = name},
         React.createElement(ProgressBar, {y = 5}),
         React.createElement(ValueBpm, nil),
+        React.createElement(TextGridClips, nil),
+        React.createElement(TextGridClipsShifted, nil),
         React.createElement(Clips, {color = ColorTrack1, track = 1, group = 0}),
         React.createElement(Clips, {color = ColorTrack2, track = 2, group = 1}),
         React.createElement(Clips, {color = ColorTrack3, track = 3, group = 2}),
         React.createElement(Clips, {color = ColorTrack4, track = 4, group = 3}),
-        React.createElement(Clips, {color = ColorTrack5, track = 5, group = 3}),
-        React.createElement(Clips, {color = ColorTrack6, track = 6, group = 4}),
-        React.createElement(Clips, {color = ColorTrack7, track = 7, group = 4}),
-        React.createElement(Clips, {color = ColorTrack8, track = 8, group = 5})
+        React.createElement(Clips, {color = ColorTrack5, track = 5, group = 4}),
+        React.createElement(Clips, {color = ColorTrack6, track = 6, group = 5}),
+        React.createElement(Clips, {color = ColorTrack7, track = 7, group = 6}),
+        React.createElement(Clips, {color = ColorTrack8, track = 8, group = 7})
     )
 end
 return ____exports
