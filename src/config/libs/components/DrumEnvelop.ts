37de7a13c52194e385ba664911629c48811d434e
apiel <alexandre.piel@gmail.com>
2025-01-04 23:07:44 +0100

    tslua

--- src/config/libs/components/DrumEnvelop.ts
+++ src/config/libs/components/DrumEnvelop.ts
@@ -4,14 +4,12 @@ import { ComponentProps } from './component';
 export type Props = ComponentProps & {
     plugin: string;
     envelop_data_id: string;
-    encoders: string[];
 };
 
-export function DrumEnvelop({ position, track, plugin, envelop_data_id, encoders, ...props }: Props) {
+export function DrumEnvelop({ position, track, plugin, envelop_data_id, ...props }: Props) {
     return getComponent('DrumEnvelop', position, [
         { track },
         { plugin, envelop_data_id },
-        encoders.map((encoder) => ({ encoder })),
         props,
     ]);
 }
