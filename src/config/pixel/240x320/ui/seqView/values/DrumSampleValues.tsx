b9cc4cc4b7ab6da6df5b0d7fd02aae812433c8d1
apiel <alexandre.piel@gmail.com>
2025-01-04 21:55:22 +0100

    tslua

--- src/config/pixel/240x320/ui/seqView/values/DrumSampleValues.tsx
+++ src/config/pixel/240x320/ui/seqView/values/DrumSampleValues.tsx
@@ -5,16 +5,17 @@ import { Rect } from '@/libs/components/Rect';
 import { Value } from '@/libs/components/Value';
 import { VisibilityContainer } from '@/libs/components/VisibilityContainer';
 import { VisibilityContext } from '@/libs/components/VisibilityContext';
-import { VisibilityGroup } from '@/libs/components/VisibilityGroup';
 import { ScreenWidth } from '@/pixel/240x320/ui/constants';
 import {
     bottomLeft,
     bottomRight,
     height,
     posY,
+    primary,
     quaternary,
     tertiary,
     topLeft,
+    topRight,
     valueH,
 } from './constants';
 
@@ -26,10 +27,60 @@ export type Props = Omit<ComponentProps, 'position'> & {
 
 export function DrumSampleValues({ group, track, context }: Props) {
     return (
-        <VisibilityContainer position={[0, posY, ScreenWidth, valueH * 2 + 2]}>
-            <VisibilityGroup condition="SHOW_WHEN" group={group} />
+        <>
+            <Edit group={group} track={track} context={context} />
+            <Main group={group} track={track} context={context} />
+        </>
+    );
+}
+
+function Main({ group, track, context }: Props) {
+    return (
+        <VisibilityContainer position={[0, posY, ScreenWidth, valueH * 2 + 2]} group={group}>
             <VisibilityContext index={10} condition="SHOW_WHEN" value={0} />
             <VisibilityContext index={context} condition="SHOW_WHEN" value={0} />
+
+            <Value
+                value="Volume VOLUME"
+                position={topLeft}
+                group={group}
+                track={track}
+                encoderId={0}
+                {...tertiary}
+            />
+            <Value
+                value="Volume GAIN_CLIPPING"
+                position={bottomLeft}
+                group={group}
+                track={track}
+                encoderId={1}
+                {...primary}
+            />
+            <Value
+                value="Volume DRIVE"
+                position={topRight}
+                group={group}
+                track={track}
+                encoderId={2}
+                {...quaternary}
+            />
+            <Value
+                value="SampleRateReducer SAMPLE_STEP"
+                position={bottomRight}
+                group={group}
+                track={track}
+                encoderId={3}
+                {...quaternary}
+            />
+        </VisibilityContainer>
+    );
+}
+
+function Edit({ group, track, context }: Props) {
+    return (
+        <VisibilityContainer position={[0, posY, ScreenWidth, valueH * 2 + 2]} group={group}>
+            <VisibilityContext index={10} condition="SHOW_WHEN" value={0} />
+            <VisibilityContext index={context} condition="SHOW_WHEN" value={1} />
 
             <Rect position={[0, 0, ScreenWidth, height]} />
 
