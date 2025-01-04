afe3726224889edafdc0fc42bba906cabf5c6e0f
apiel <alexandre.piel@gmail.com>
2025-01-04 22:13:54 +0100

    tslua

---
+++ src/config/pixel/240x320/ui/seqView/values/SeqValues.tsx
@@ -0,0 +1,64 @@
+import * as React from '@/libs/react';
+
+import { ComponentProps } from '@/libs/components/component';
+import { Value } from '@/libs/components/Value';
+import { VisibilityContainer } from '@/libs/components/VisibilityContainer';
+import { VisibilityContext } from '@/libs/components/VisibilityContext';
+import { ScreenWidth } from '@/pixel/240x320/ui/constants';
+import {
+    bottomLeft,
+    bottomRight,
+    posY,
+    primary,
+    quaternary,
+    secondary,
+    tertiary,
+    topLeft,
+    topRight,
+    valueH
+} from './constants';
+
+export type Props = Omit<ComponentProps, 'position'> & {
+    group: number;
+    track: number;
+};
+
+export function SeqValues({ group, track }: Props) {
+    return (
+        <VisibilityContainer position={[0, posY, ScreenWidth, valueH * 2 + 2]} group={group}>
+            <VisibilityContext index={10} condition="SHOW_WHEN_OVER" value={0} />
+            <Value
+                value="Sequencer STEP_NOTE"
+                position={topLeft}
+                group={group}
+                track={track}
+                encoderId={0}
+                {...tertiary}
+            />
+            <Value
+                value="Sequencer STEP_CONDITION"
+                position={bottomLeft}
+                group={group}
+                track={track}
+                encoderId={1}
+                {...primary}
+            />
+            <Value
+                value="Sequencer STEP_VELOCITY"
+                position={topRight}
+                group={group}
+                track={track}
+                encoderId={2}
+                {...secondary}
+            />
+            <Value
+                value="Sequencer STEP_MOTION"
+                position={bottomRight}
+                group={group}
+                track={track}
+                encoderId={3}
+                {...quaternary}
+            />
+        </VisibilityContainer>
+    );
+}
