3eddac9707e2e6b356b48627ea24eaa5fd649172
apiel <alexandre.piel@gmail.com>
2025-01-04 22:42:07 +0100

    tslua

---
+++ src/config/libs/components/GraphEncoder.ts
@@ -0,0 +1,17 @@
+import { getComponent } from '@/libs/ui';
+import { ComponentProps } from './component';
+
+export type Props = ComponentProps & {
+    plugin: string;
+    data_id: string;
+    encoders: string[];
+};
+
+export function GraphEncoder({ position, track, plugin, data_id, encoders, ...props }: Props) {
+    return getComponent('GraphEncoder', position, [
+        { track },
+        { plugin, data_id },
+        encoders.map((encoder) => ({ encoder })),
+        props,
+    ]);
+}
