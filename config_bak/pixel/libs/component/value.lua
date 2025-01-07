b163c1dfe850e5d5a8afd1fa93ed1e81582c736a
apiel <alexandre.piel@gmail.com>
2025-01-07 16:09:51 +0100

    big cleanup

--- config_bak/pixel/libs/component/value.lua
+++
@@ -1,8 +0,0 @@
-local ui = require "config/pixel/libs/ui"
-
---- Value component is used to display an audio plugin value.
-local function component(value, position, options)
-    ui.component("Value", {"VALUE"}, { VALUE = value }, position, options)
-end
-
-return component
