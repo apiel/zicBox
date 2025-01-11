3044f8de1a7e2b9b542038027c1d98379411256c
apiel <alexandre.piel@gmail.com>
2025-01-11 22:06:43 +0100

    kick module

--- config/pixel/240x320_kick/ui/Page1View/FrequencyView.lua
+++ config/pixel/240x320_kick/ui/Page1View/FrequencyView.lua
@@ -1,10 +1,10 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 local React = require("config.libs.react")
+local ____DrumEnvelop = require("config.libs.components.DrumEnvelop")
+local DrumEnvelop = ____DrumEnvelop.DrumEnvelop
 local ____Encoder3 = require("config.libs.components.Encoder3")
 local Encoder3 = ____Encoder3.Encoder3
-local ____GraphEncoder = require("config.libs.components.GraphEncoder")
-local GraphEncoder = ____GraphEncoder.GraphEncoder
 local ____View = require("config.libs.components.View")
 local View = ____View.View
 local ____constants = require("config.pixel.240x320_kick.ui.constants")
@@ -17,20 +17,22 @@ local ____TextGridCommon = require("config.pixel.240x320_kick.ui.TextGridCommon"
 local TextGridCommon = ____TextGridCommon.TextGridCommon
 local ____TextGridPage1 = require("config.pixel.240x320_kick.ui.Page1View.TextGridPage1")
 local TextGridPage1 = ____TextGridPage1.TextGridPage1
-function ____exports.WaveformView(____bindingPattern0)
+function ____exports.FrequencyView(____bindingPattern0)
     local name
     name = ____bindingPattern0.name
     return React.createElement(
         View,
         {name = name},
-        React.createElement(GraphEncoder, {
+        React.createElement(DrumEnvelop, {
             position = {0, 0, W3_4 - 2, height},
             plugin = "Drum23",
-            data_id = "WAVEFORM",
+            envelop_data_id = "4",
             RENDER_TITLE_ON_TOP = false,
-            encoders = {"0 WAVEFORM_TYPE", "2 MACRO", "1 SHAPE"}
+            encoder_time = 0,
+            encoder_phase = 1,
+            encoder_modulation = 2
         }),
-        React.createElement(Encoder3, {position = {W3_4, (height - encoderH) * 0.5, W1_4, encoderH}, value = "Drum23 DURATION", encoder_id = 3, color = "quaternary"}),
+        React.createElement(Encoder3, {position = {W3_4, (height - encoderH) * 0.5, W1_4, encoderH}, value = "Drum23 PITCH", encoder_id = 3, color = "secondary"}),
         React.createElement(TextGridPage1, nil),
         React.createElement(TextGridCommon, {selected = 0})
     )
