9902c1b380adc9e080398cbd6a9eb803acc23b2c
apiel <alexandre.piel@gmail.com>
2025-01-12 14:06:03 +0100

    rgb

--- styles.h
+++ styles.h
@@ -2,6 +2,7 @@
 #define _STYLES_H_
 
 #include "plugins/components/drawInterface.h"
+#include "plugins/components/utils/color.h"
 
 Styles styles = {
     .screen = { 800, 480 },
@@ -11,13 +12,13 @@ Styles styles = {
         "/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Bold.ttf",
         "/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Italic.ttf" },
     .colors = {
-        .background = { 0x21, 0x25, 0x2b, 255 }, // #21252b
-        .white = { 0xff, 0xff, 0xff, 255 }, // #ffffff
-        .text = { 0xff, 0xff, 0xff, 255 }, // #ffffff
-        .primary = { 0xad, 0xcd, 0xff, 255 }, // #adcdff
-        .secondary = { 0xff, 0x8d, 0x99, 255 }, // #ff8d99
-        .tertiary = { 0x8d, 0xDD, 0x99, 255 }, // #8dDD99
-        .quaternary = { 0xff, 0x99, 0x00, 255 }, // #ff9900
+        .background =rgb(33, 37, 43),
+        .white = rgb(255, 255, 255),
+        .text = rgb(255, 255, 255),
+        .primary = rgb(173, 205, 255),
+        .secondary = rgb(255, 141, 153),
+        .tertiary = rgb(141, 221, 153),
+        .quaternary = rgb(255, 153, 0),
     },
 };
 
