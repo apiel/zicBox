9f8510ced598f63226c6cd2d72421dfe6d68dc20
apiel <alexandre.piel@gmail.com>
2025-01-12 13:20:34 +0100

    kick module

--- config/pixel/240x320_kick/ui/components/Common.lua
+++ config/pixel/240x320_kick/ui/components/Common.lua
@@ -1,12 +1,12 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 local React = require("config.libs.react")
+local ____SeqProgressBar = require("config.libs.components.SeqProgressBar")
+local SeqProgressBar = ____SeqProgressBar.SeqProgressBar
 local ____Spectrogram = require("config.libs.components.Spectrogram")
 local Spectrogram = ____Spectrogram.Spectrogram
 local ____constants = require("config.pixel.240x320_kick.ui.constants")
 local ScreenWidth = ____constants.ScreenWidth
-local ____ProgressBar = require("config.pixel.240x320_kick.ui.components.ProgressBar")
-local ProgressBar = ____ProgressBar.ProgressBar
 local ____TextGridCommon = require("config.pixel.240x320_kick.ui.components.TextGridCommon")
 local TextGridCommon = ____TextGridCommon.TextGridCommon
 function ____exports.Common(____bindingPattern0)
@@ -15,8 +15,14 @@ function ____exports.Common(____bindingPattern0)
     return React.createElement(
         React.Fragment,
         nil,
-        React.createElement(Spectrogram, {position = {0, 100, ScreenWidth, 70}, data = "Spectrogram BUFFER", text = "Pixel", raw_buffer = true}),
-        React.createElement(ProgressBar, nil),
+        React.createElement(Spectrogram, {position = {0, 230, ScreenWidth, 50}, data = "Spectrogram BUFFER", text = "Pixel"}),
+        React.createElement(SeqProgressBar, {
+            position = {0, 285, ScreenWidth, 5},
+            seq_plugin = "Sequencer 0",
+            active_color = "#23a123",
+            selection_color = "#23a123",
+            volume_plugin = "Volume VOLUME"
+        }),
         React.createElement(TextGridCommon, {selected = selected})
     )
 end
--- src/config/pixel/240x320_kick/ui/components/Common.tsx
+++ src/config/pixel/240x320_kick/ui/components/Common.tsx
@@ -1,20 +1,26 @@
 import * as React from '@/libs/react';
 
+import { SeqProgressBar } from '@/libs/components/SeqProgressBar';
 import { Spectrogram } from '@/libs/components/Spectrogram';
 import { ScreenWidth } from '../constants';
-import { ProgressBar } from './ProgressBar';
 import { TextGridCommon } from './TextGridCommon';
 
 export function Common({ selected }: { selected: number }) {
     return (
         <>
             <Spectrogram
-                position={[0, 100, ScreenWidth, 70]}
+                position={[0, 230, ScreenWidth, 50]}
                 data="Spectrogram BUFFER"
                 text="Pixel"
-                raw_buffer
+                // raw_buffer
             />
-            <ProgressBar />
+            <SeqProgressBar
+                position={[0, 285, ScreenWidth, 5]}
+                seq_plugin="Sequencer 0"
+                active_color="#23a123"
+                selection_color="#23a123"
+                volume_plugin="Volume VOLUME"
+            />
             <TextGridCommon selected={selected} />
         </>
     );
