1f89f86e3e11a5c3f4b27c0537d35ea4af1d8c70
apiel <alexandre.piel@gmail.com>
2025-01-12 20:50:00 +0100

    StepEdit2

--- src/config/pixel/240x320_kick/ui/Seq/index.tsx
+++ src/config/pixel/240x320_kick/ui/Seq/index.tsx
@@ -4,6 +4,7 @@ import { StepEdit2 } from '@/libs/components/StepEdit2';
 import { View } from '@/libs/components/View';
 import { rgb } from '@/libs/ui';
 import { Common } from '../components/Common';
+import { ScreenWidth } from '../constants';
 import { TextGridSeq } from './TextGridSeq';
 
 export type Props = {
@@ -11,22 +12,30 @@ export type Props = {
 };
 
 export function SeqView({ name }: Props) {
+    let y = 0;
     return (
         <View name={name}>
-            {Array.from({ length: 32 }, (_, i) => (
-                <StepEdit2
-                    position={[0, i * 8, 0, 0]}
-                    data={`Sequencer ${i}`}
-                    group={i}
-                    playing_color={rgb(35, 161, 35)}
-                    // bar_color={
-                    //     // i % 8 == 0 || i % 8 == 1 || i % 8 == 2 || i % 8 == 3
-                    //     i % 4 == 0
-                    //         ? rgb(104, 149, 160)
-                    //         : rgb(197, 216, 178)
-                    // }
-                />
-            ))}
+            {Array.from({ length: 32 }, (_, i) => {
+                const yy = y;
+                y += 8 + (i % 4 == 3 ? 3 : 0);
+                return (
+                    <StepEdit2
+                        position={[0, yy, ScreenWidth, 8]}
+                        data={`Sequencer ${i}`}
+                        group={i}
+                        playing_color={rgb(35, 161, 35)}
+                        background_color={
+                            i % 8 == 0 || i % 8 == 1 || i % 8 == 2 || i % 8 == 3
+                                ? rgb(42, 54, 56)
+                                : "background"
+                        }
+                    />
+                );
+            })}
+
+            {/* {Array.from({ length: 7 }, (_, i) => (
+                <Rect position={[0, ((i + 1) * 35) - 2, ScreenWidth, 1]} color={rgb(58, 74, 78)} />
+            ))} */}
 
             <TextGridSeq />
             <Common selected={0} hideSpectrogram hideSequencer />
