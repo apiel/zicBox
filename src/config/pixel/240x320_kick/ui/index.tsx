6a280f45e387f5e58516244a701324b3d12ea1cf
apiel <alexandre.piel@gmail.com>
2025-01-14 18:50:28 +0100

    reduce refresh rate

--- src/config/pixel/240x320_kick/ui/index.tsx
+++ src/config/pixel/240x320_kick/ui/index.tsx
@@ -2,13 +2,7 @@ import { applyZic } from '@/libs/core';
 import * as React from '@/libs/react';
 import { addZoneEncoder, setScreenSize, setWindowPosition } from '@/libs/ui';
 import { ScreenHeight, ScreenWidth, W1_2, W1_4, W3_4 } from './constants';
-import { SeqView } from './Seq';
-import { AmpView } from './Synth/AmpView';
-import { ClickView } from './Synth/ClickView';
-import { DistortionView } from './Synth/DistortionView';
-import { FrequencyView } from './Synth/FrequencyView';
 import { MasterView } from './Synth/MasterView';
-import { WaveformView } from './Synth/WaveformView';
 
 setWindowPosition(400, 500);
 setScreenSize(ScreenWidth, ScreenHeight);
@@ -19,10 +13,10 @@ addZoneEncoder([W1_2, 0, W1_4, ScreenHeight]);
 addZoneEncoder([W3_4, 0, W1_4, ScreenHeight]);
 
 applyZic(<MasterView name="Master" />);
-applyZic(<DistortionView name="Distortion" />);
-applyZic(<WaveformView name="Waveform" />);
-applyZic(<FrequencyView name="Frequency" />);
-applyZic(<AmpView name="Amplitude" />);
-applyZic(<ClickView name="Click" />);
+// applyZic(<DistortionView name="Distortion" />);
+// applyZic(<WaveformView name="Waveform" />);
+// applyZic(<FrequencyView name="Frequency" />);
+// applyZic(<AmpView name="Amplitude" />);
+// applyZic(<ClickView name="Click" />);
 
-applyZic(<SeqView name="Sequencer" />);
+// applyZic(<SeqView name="Sequencer" />);
