ef9bb0905bdc29200d63d1e162d57137c97f9224
apiel <alexandre.piel@gmail.com>
2025-01-15 16:34:35 +0100

    remove spectrogram

--- src/config/pixel/240x320_kick/ui/components/Common.tsx
+++ src/config/pixel/240x320_kick/ui/components/Common.tsx
@@ -2,33 +2,19 @@ import * as React from '@/libs/react';
 
 import { SeqProgressBar } from '@/libs/components/SeqProgressBar';
 import { SeqSynthBar } from '@/libs/components/SeqSynthBar';
-import { Spectrogram } from '@/libs/components/Spectrogram';
-import { rgb, rgba } from '@/libs/ui';
+import { rgb } from '@/libs/ui';
 import { ScreenWidth } from '../constants';
 import { TextGridCommon } from './TextGridCommon';
 
 export function Common({
     selected,
-    hideSpectrogram,
     hideSequencer,
 }: {
     selected: number;
-    hideSpectrogram?: boolean;
     hideSequencer?: boolean;
 }) {
     return (
         <>
-            {!hideSpectrogram && (
-                <Spectrogram
-                    position={[0, 230, ScreenWidth, 35]}
-                    data="Spectrogram BUFFER"
-                    text="Pixel"
-                    // wave_color={rgb(35, 73, 117)}
-                    // wave_color={rgba(35, 73, 117, 0.6)}
-                    wave_color={rgba(35, 105, 117, 0.7)}
-                    raw_buffer
-                />
-            )}
             {!hideSequencer && (
                 <SeqSynthBar position={[0, 270, ScreenWidth, 10]} seq_plugin="Sequencer" />
             )}
