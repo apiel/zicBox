1046539d896b4d870c45a9c236656e8735f0e846
apiel <alexandre.piel@gmail.com>
2025-01-19 12:45:56 +0100

    Title

--- src/config/pixel/240x320_kick/ui/Snare/TextGridSnare.tsx
+++ src/config/pixel/240x320_kick/ui/Snare/TextGridSnare.tsx
@@ -1,22 +1,36 @@
 import * as React from '@/libs/react';
 
 import { TextGridSel } from '../components/TextGridSel';
+import { Title } from '../components/Title';
 
-export function TextGridSnare({ selected, viewName }: { selected: number, viewName: string }) {
+export function TextGridSnare({ selected, viewName }: { selected: number; viewName: string }) {
     return (
-        <TextGridSel
-            items={['Snare', 'Seq.', '...', 'Amp/Click', 'Freq.', '&icon::musicNote::pixelated']}
-            keys={[
+        <>
+            <Title title="Snare" />
+            <TextGridSel
+                items={[
+                    'Snare',
+                    'Seq.',
+                    '...',
+                    'Amp/Click',
+                    'Freq.',
+                    '&icon::musicNote::pixelated',
+                ]}
+                keys={[
                     { key: 'q', action: viewName === 'Snare' ? 'setView:Snare2' : 'setView:Snare' },
                     { key: 'w', action: 'setView:Snare2' },
                     { key: 'e', action: 'contextToggle:254:1:0' },
 
-                    { key: 'a', action: viewName === 'Amplitude' ? 'setView:Click' : 'setView:Amplitude' },
+                    {
+                        key: 'a',
+                        action: viewName === 'Amplitude' ? 'setView:Click' : 'setView:Amplitude',
+                    },
                     { key: 's', action: 'setView:Frequency' },
                     { key: 'd', action: 'noteOn:Snare:60' },
-            ]}
-            selected={selected}
-            contextValue={0}
-        />
+                ]}
+                selected={selected}
+                contextValue={0}
+            />
+        </>
     );
 }
