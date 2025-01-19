8aa713a690ca2d3729d9fe8312bf111bfaba5af2
apiel <alexandre.piel@gmail.com>
2025-01-19 12:45:56 +0100

    Title

--- config/pixel/240x320_kick/ui/Drum23/TextGridDrum23.lua
+++ config/pixel/240x320_kick/ui/Drum23/TextGridDrum23.lua
@@ -3,25 +3,32 @@ local ____exports = {}
 local React = require("config.libs.react")
 local ____TextGridSel = require("config.pixel.240x320_kick.ui.components.TextGridSel")
 local TextGridSel = ____TextGridSel.TextGridSel
+local ____Title = require("config.pixel.240x320_kick.ui.components.Title")
+local Title = ____Title.Title
 function ____exports.TextGridDrum23(____bindingPattern0)
     local viewName
     local selected
     selected = ____bindingPattern0.selected
     viewName = ____bindingPattern0.viewName
-    return React.createElement(TextGridSel, {items = {
-        "Fx1/Fx2",
-        "Wave/Freq",
-        "...",
-        "Amp/Click",
-        "Sequencer",
-        "&icon::musicNote::pixelated"
-    }, keys = {
-        {key = "q", action = viewName == "Drum23" and "setView:Distortion" or "setView:Drum23"},
-        {key = "w", action = viewName == "Waveform" and "setView:Frequency" or "setView:Waveform"},
-        {key = "e", action = "contextToggle:254:1:0"},
-        {key = "a", action = viewName == "Amplitude" and "setView:Click" or "setView:Amplitude"},
-        {key = "s", action = "setView:Sequencer"},
-        {key = "d", action = "noteOn:Drum23:60"}
-    }, selected = selected, contextValue = 0})
+    return React.createElement(
+        React.Fragment,
+        nil,
+        React.createElement(Title, {title = "Kick"}),
+        React.createElement(TextGridSel, {items = {
+            "Fx1/Fx2",
+            "Wave/Freq",
+            "...",
+            "Amp/Click",
+            "Sequencer",
+            "&icon::musicNote::pixelated"
+        }, keys = {
+            {key = "q", action = viewName == "Drum23" and "setView:Distortion" or "setView:Drum23"},
+            {key = "w", action = viewName == "Waveform" and "setView:Frequency" or "setView:Waveform"},
+            {key = "e", action = "contextToggle:254:1:0"},
+            {key = "a", action = viewName == "Amplitude" and "setView:Click" or "setView:Amplitude"},
+            {key = "s", action = "setView:Sequencer"},
+            {key = "d", action = "noteOn:Drum23:60"}
+        }, selected = selected, contextValue = 0})
+    )
 end
 return ____exports
--- src/config/pixel/240x320_kick/ui/Drum23/TextGridDrum23.tsx
+++ src/config/pixel/240x320_kick/ui/Drum23/TextGridDrum23.tsx
@@ -1,22 +1,42 @@
 import * as React from '@/libs/react';
 
 import { TextGridSel } from '../components/TextGridSel';
+import { Title } from '../components/Title';
 
-export function TextGridDrum23({ selected, viewName }: { selected: number, viewName: string }) {
+export function TextGridDrum23({ selected, viewName }: { selected: number; viewName: string }) {
     return (
-        <TextGridSel
-            items={['Fx1/Fx2', 'Wave/Freq', '...', 'Amp/Click', 'Sequencer', '&icon::musicNote::pixelated']}
-            keys={[
-                    { key: 'q', action: viewName === 'Drum23' ? 'setView:Distortion' : 'setView:Drum23' },
-                    { key: 'w', action: viewName === 'Waveform' ? 'setView:Frequency' : 'setView:Waveform' },
+        <>
+            <Title title="Kick" />
+            <TextGridSel
+                items={[
+                    'Fx1/Fx2',
+                    'Wave/Freq',
+                    '...',
+                    'Amp/Click',
+                    'Sequencer',
+                    '&icon::musicNote::pixelated',
+                ]}
+                keys={[
+                    {
+                        key: 'q',
+                        action: viewName === 'Drum23' ? 'setView:Distortion' : 'setView:Drum23',
+                    },
+                    {
+                        key: 'w',
+                        action: viewName === 'Waveform' ? 'setView:Frequency' : 'setView:Waveform',
+                    },
                     { key: 'e', action: 'contextToggle:254:1:0' },
 
-                    { key: 'a', action: viewName === 'Amplitude' ? 'setView:Click' : 'setView:Amplitude' },
+                    {
+                        key: 'a',
+                        action: viewName === 'Amplitude' ? 'setView:Click' : 'setView:Amplitude',
+                    },
                     { key: 's', action: 'setView:Sequencer' },
                     { key: 'd', action: 'noteOn:Drum23:60' },
-            ]}
-            selected={selected}
-            contextValue={0}
-        />
+                ]}
+                selected={selected}
+                contextValue={0}
+            />
+        </>
     );
 }
