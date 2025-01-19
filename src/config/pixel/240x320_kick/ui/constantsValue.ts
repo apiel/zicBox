81a1375a39c8a0b05351f06b428dfc99c02fdcf7
apiel <alexandre.piel@gmail.com>
2025-01-19 12:48:06 +0100

    Title

--- src/config/pixel/240x320_kick/ui/constantsValue.ts
+++ src/config/pixel/240x320_kick/ui/constantsValue.ts
@@ -1,9 +1,10 @@
 import { ScreenWidth, W2_4 } from '@/pixel/240x320/ui/constants';
 
-export const fullValues = [0, 50, ScreenWidth - 1, 180];
-export const topValues = [0, 50, ScreenWidth - 1, 80];
-export const bottomValues = [0, 150, ScreenWidth - 1, 80];
-export const topLeftKnob = [0, 50, W2_4 - 2, 80];
-export const topRightKnob = [W2_4, 50, W2_4 - 2, 80];
-export const bottomLeftKnob = [0, 150, W2_4 - 2, 80];
-export const bottomRightKnob = [W2_4, 150, W2_4 - 2, 80];
+const top = 70;
+export const fullValues = [0, top, ScreenWidth - 1, 180];
+export const topValues = [0, top, ScreenWidth - 1, 80];
+export const bottomValues = [0, top + 100, ScreenWidth - 1, 80];
+export const topLeftKnob = [0, top, W2_4 - 2, 80];
+export const topRightKnob = [W2_4, top, W2_4 - 2, 80];
+export const bottomLeftKnob = [0, top + 100, W2_4 - 2, 80];
+export const bottomRightKnob = [W2_4, top + 100, W2_4 - 2, 80];
