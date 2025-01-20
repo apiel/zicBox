aa156a5f027378aa97a24c46aa70d0742d5a490a
apiel <alexandre.piel@gmail.com>
2025-01-20 12:54:40 +0100

    stop reverb

--- plugins/audio/SynthFmDrum.h
+++ plugins/audio/SynthFmDrum.h
@@ -5,6 +5,8 @@
 #include "mapping.h"
 #include "plugins/audio/utils/utils.h"
 
+#include <climits>
+
 /*md
 ## SynthFmDrum
 
@@ -139,8 +141,23 @@ public:
 
             buf[track] = output;
             i++;
-        } else {
+        } else if (i != INT_MAX) {
             buf[track] = applyReverb(buf[track]);
+            // Stop processing reverb if empty
+            if (buf[track] == 0.0f) {
+                // check that buffer is empty
+                bool isEmpty = true;
+                for (int j = 0; j < REVERB_BUFFER_SIZE; j++) {
+                    if (reverbBuffer[j] != 0.0f) {
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (isEmpty) {
+                    i = INT_MAX;
+                    printf("stop reverb process\n");
+                }
+            }
         }
     }
 
