79069d34b905be6eafed43c511892435897c8407
apiel <alexandre.piel@gmail.com>
2025-01-23 21:26:58 +0100

    focus on kick!!

--- src/config/pixel/240x320_kick/ui/components/Common.tsx
+++ src/config/pixel/240x320_kick/ui/components/Common.tsx
@@ -30,12 +30,13 @@ export function Common({
             )}
 
             <TextGridSel
-                items={['Kick', 'Seq.', '^...', 'Seq.', 'Clips', '&icon::play::filled']}
+                items={['Kick', 'Sample', '^...', 'Sequencer', 'Clips', '&icon::play::filled']}
                 keys={[
                     { key: 'q', action: 'setView:Drum23' },
-                    { key: 'w', action: `setView:Sequencer` },
+                    { key: 'w', action: `setView:Sample` },
                     { key: 'e', action: 'contextToggle:254:1:0' },
-                    { key: 'a', action: `setView:DrumsSeq` },
+
+                    { key: 'a', action: 'setView:Sequencer' },
                     { key: 's', action: `setView:Clips` },
                     { key: 'd', action: 'playPause' },
                 ]}
