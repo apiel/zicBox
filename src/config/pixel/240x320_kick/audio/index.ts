305429efbc16d9636f6343c31c11a4a2109ede54
apiel <alexandre.piel@gmail.com>
2025-01-25 20:58:55 +0100

    seq sample view

--- src/config/pixel/240x320_kick/audio/index.ts
+++ src/config/pixel/240x320_kick/audio/index.ts
@@ -26,14 +26,14 @@ plugin('MMFilter EffectFilterMultiMode', [{ STRING_CUTOFF_FORMAT, track }]);
 plugin('TrackFx EffectGainVolume', [{ track }]);
 plugin('SerializeTrack', [{ track, filename: 'drum23', MAX_VARIATION, WORKSPACE_FOLDER }]);
 
-// track = SampleTrack;
-// plugin('Sample', [{ track }]);
-// plugin('Sequencer', [{ track }]);
-// plugin('TrackFx EffectGainVolume', [{ track }]);
-// plugin('SerializeTrack', [{ track, filename: 'sample', MAX_VARIATION, WORKSPACE_FOLDER }]);
+track = SampleTrack;
+plugin('Sample', [{ track }]);
+plugin('Sequencer', [{ track }]);
+plugin('TrackFx EffectGainVolume', [{ track }]);
+plugin('SerializeTrack', [{ track, filename: 'sample', MAX_VARIATION, WORKSPACE_FOLDER }]);
 
-plugin('SampleSequencer', [{ track: SampleTrack }]);
-plugin('SerializeTrack', [{ track: SampleTrack, filename: 'sampleSeq', MAX_VARIATION, WORKSPACE_FOLDER }]);
+// plugin('SampleSequencer', [{ track: SampleTrack }]);
+// plugin('SerializeTrack', [{ track: SampleTrack, filename: 'sampleSeq', MAX_VARIATION, WORKSPACE_FOLDER }]);
 
 plugin('Mixer');
 plugin('Volume EffectGainVolume');
