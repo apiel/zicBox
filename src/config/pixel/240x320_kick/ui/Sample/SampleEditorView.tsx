21b51b1280a25fdc6482c5bec221afed7502fda5
apiel <alexandre.piel@gmail.com>
2025-01-26 12:17:10 +0100

    sample seq

--- src/config/pixel/240x320_kick/ui/Sample/SampleEditorView.tsx
+++ src/config/pixel/240x320_kick/ui/Sample/SampleEditorView.tsx
@@ -1,6 +1,7 @@
 import * as React from '@/libs/react';
 
 import { StepEditSample } from '@/libs/components/StepEditSample';
+import { Text } from '@/libs/components/Text';
 import { View } from '@/libs/components/View';
 import { rgb } from '@/libs/ui';
 import { Common } from '../components/Common';
@@ -15,6 +16,10 @@ export function SampleEditorView({ name }: Props) {
     let y = 0;
     return (
         <View name={name}>
+            <Text text="Vel." position={[0, 0, 25, 9]} />
+            <Text text="Sample" position={[60, 0, 100, 9]} />
+            <Text text="Start" position={[ScreenWidth - 84, 0, 50, 9]} />
+            <Text text="End" position={[ScreenWidth - 38, 0, 50, 9]} />
             {Array.from({ length: 32 }, (_, i) => {
                 const yy = y + 12;
                 y += 8 + (i % 4 == 3 ? 3 : 0);
