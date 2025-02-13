c2dfcec8413c03b12af97a88cc984ecd74a300ac
apiel <alexandre.piel@gmail.com>
2025-02-13 17:45:38 +0100

    cleanup

--- host/config.h
+++ host/config.h
@@ -14,19 +14,10 @@ extern "C" void hostScriptCallback(char* key, char* value, const char* filename,
     } else if (strcmp(key, "MIDIOUT") == 0) {
         // NOTE note used for the moments...
         loadMidiOutput(midiOut, value);
-    } else if (strcmp(key, "AUDIO_OUTPUT") == 0) {
-        strcpy(audioOutputName, value);
-        logInfo("Audio output set: %s", audioOutputName);
-    } else if (strcmp(key, "AUDIO_INPUT") == 0) {
-        strcpy(audioInputName, value);
-        logInfo("Audio input set: %s", audioInputName);
     } else if (strcmp(key, "AUDIO_PLUGIN") == 0) {
         AudioPluginHandler::get().loadPlugin(value);
     } else if (strcmp(key, "AUDIO_PLUGIN_ALIAS") == 0) {
         AudioPluginHandler::get().loadPluginAlias(value, filename);
-    } else if (strcmp(key, "GAIN_OUTPUT") == 0) {
-        // float gain = AudioPluginHandler::get().gainVolume.setGain(atof(value)).gain;
-        // logInfo("Gain output set: %f", gain);
     } else if (strcmp(key, "AUTO_SAVE") == 0) {
         uint32_t msInterval = atoi(value);
         if (msInterval > 0) {
