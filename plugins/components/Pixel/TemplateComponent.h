ffd61bc93d9315b09bb3b65b9eb45adf88455a42
apiel <alexandre.piel@gmail.com>
2025-02-15 19:29:17 +0100

    json component

--- plugins/components/Pixel/TemplateComponent.h
+++ plugins/components/Pixel/TemplateComponent.h
@@ -4,12 +4,6 @@
 
 #include <string>
 
-/*md
-## Rect
-
-Rect components to draw a rectangle.
-*/
-
 class RectComponent : public Component {
 protected:
     Color background;
@@ -25,17 +19,5 @@ public:
         if (updatePosition()) {
             draw.filledRect(relativePosition, size, { background });
         }
-    }
-
-    /*md **Config**: */
-    bool config(char* key, char* value)
-    {
-        /*md - `BACKGROUND_COLOR: color` is the color of the component. */
-        if (strcmp(key, "BACKGROUND_COLOR") == 0) {
-            background = draw.getColor(value);
-            return true;
-        }
-
-        return false;
     }
 };
