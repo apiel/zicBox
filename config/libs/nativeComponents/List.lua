55c6ed5483ae9cd06c880de9c0a5aa87a373e2de
apiel <alexandre.piel@gmail.com>
2025-02-16 10:37:11 +0100

    list component

--- config/libs/nativeComponents/List.lua
+++ config/libs/nativeComponents/List.lua
@@ -1,28 +1,6 @@
-local ____lualib = require("lualib_bundle")
-local __TS__ArrayMap = ____lualib.__TS__ArrayMap
-local __TS__ObjectRest = ____lualib.__TS__ObjectRest
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 local ____ui = require("config.libs.ui")
-local getComponent = ____ui.getComponent
-local initializePlugin = ____ui.initializePlugin
-function ____exports.List(____bindingPattern0)
-    local props
-    local items
-    local bounds
-    bounds = ____bindingPattern0.bounds
-    items = ____bindingPattern0.items
-    props = __TS__ObjectRest(____bindingPattern0, {bounds = true, items = true})
-    initializePlugin("List", "libzic_ListComponent.so")
-    return getComponent(
-        "List",
-        bounds,
-        {
-            __TS__ArrayMap(
-                items,
-                function(____, item) return {ADD_ITEM = item} end
-            ),
-            props
-        }
-    )
-end
+local getJsonComponent = ____ui.getJsonComponent
+____exports.List = getJsonComponent("List", "libzic_ListComponent.so")
 return ____exports
