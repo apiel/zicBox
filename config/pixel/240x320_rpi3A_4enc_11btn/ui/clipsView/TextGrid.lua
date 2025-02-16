c9582b56b0bc58037370eedbb78510440129e071
apiel <alexandre.piel@gmail.com>
2025-02-16 14:22:39 +0100

    backward compatibility with old component

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/TextGrid.lua
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/TextGrid.lua
@@ -1,10 +1,6 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 local React = require("config.libs.react")
-local ____Keymaps = require("config.libs.nativeComponents.Keymaps")
-local Keymaps = ____Keymaps.Keymaps
-local ____VisibilityContext = require("config.libs.nativeComponents.VisibilityContext")
-local VisibilityContext = ____VisibilityContext.VisibilityContext
 local ____TextGrid = require("config.libs.tsComponents.TextGrid")
 local TextGrid = ____TextGrid.TextGrid
 local ____constants = require("config.pixel.240x320_rpi3A_4enc_11btn.ui.constants")
@@ -14,19 +10,9 @@ local btn7 = ____constants.btn7
 local btnShift = ____constants.btnShift
 local KeyInfoPosition = ____constants.KeyInfoPosition
 function ____exports.TextGridClips()
-    return React.createElement(
-        TextGrid,
-        {bounds = KeyInfoPosition, rows = {"&icon::toggle::rect &icon::arrowUp::filled ...", "&icon::arrowLeft::filled &icon::arrowDown::filled &icon::arrowRight::filled"}},
-        React.createElement(VisibilityContext, {index = 254, condition = "SHOW_WHEN", value = 0}),
-        React.createElement(Keymaps, {keys = {{key = btnShift, action = "contextToggle:254:1:0"}, {key = btn5, action = "incGroup:-1"}, {key = btn7, action = "incGroup:+1"}}})
-    )
+    return React.createElement(TextGrid, {bounds = KeyInfoPosition, rows = {"&icon::toggle::rect &icon::arrowUp::filled ...", "&icon::arrowLeft::filled &icon::arrowDown::filled &icon::arrowRight::filled"}, keys = {{key = btnShift, action = "contextToggle:254:1:0"}, {key = btn5, action = "incGroup:-1"}, {key = btn7, action = "incGroup:+1"}}, contextValue = 0})
 end
 function ____exports.TextGridClipsShifted()
-    return React.createElement(
-        TextGrid,
-        {bounds = KeyInfoPosition, rows = {"Menu Save ^...", "&icon::trash Back &icon::play::filled"}},
-        React.createElement(VisibilityContext, {index = 254, condition = "SHOW_WHEN", value = 1}),
-        React.createElement(Keymaps, {keys = {{key = btn7, action = "playPause"}, {key = btnShift, action = "contextToggle:254:1:0"}, {key = btn1, action = "setView:Menu"}}})
-    )
+    return React.createElement(TextGrid, {bounds = KeyInfoPosition, rows = {"Menu Save ^...", "&icon::trash Back &icon::play::filled"}, keys = {{key = btn7, action = "playPause"}, {key = btnShift, action = "contextToggle:254:1:0"}, {key = btn1, action = "setView:Menu"}}, contextValue = 1})
 end
 return ____exports
