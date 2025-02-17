839fc3e6e53b4b597bbfd8dd0e8c53015880ed78
apiel <alexandre.piel@gmail.com>
2025-02-17 20:58:17 +0100

    seq

--- plugins/components/Pixel/SequencerComponent.h
+++ plugins/components/Pixel/SequencerComponent.h
@@ -2,6 +2,7 @@
 
 #include "helpers/midiNote.h"
 #include "host/constants.h" // TODO instead load from plugin seq
+#include "plugins/audio/stepInterface.h"
 #include "plugins/components/component.h"
 #include "plugins/components/utils/color.h"
 
@@ -34,6 +35,11 @@ protected:
     Color noteColor;
     Color note2Color;
 
+    Color stepColor;
+
+    AudioPlugin* plugin;
+    std::vector<Step>* steps = NULL;
+
 public:
     SequencerComponent(ComponentInterface::Props props)
         : Component(props)
@@ -43,9 +49,23 @@ public:
         , rowSeparatorColor(lighten(styles.colors.background, 0.4))
         , noteColor(darken(styles.colors.white, 0.2))
         , note2Color(lighten(styles.colors.background, 1.5))
+        , stepColor(styles.colors.primary)
     {
         /*md md_config:Sequencer */
         nlohmann::json config = props.config;
+
+        // TODO make a getter function
+        /*md   // The audio plugin sequencer. */
+        /*md   audioPlugin="audio_plugin_name" */
+        if (!config.contains("audioPlugin")) {
+            logWarn("Sequencer component is missing audioPlugin parameter.");
+            return;
+        }
+        plugin = &getPlugin(config["audioPlugin"].get<std::string>().c_str(), track);
+
+        /*md   // The data id to get steps from audio plugin sequencer. */
+        /*md   stepsDataId="STEPS" */
+        steps = (std::vector<Step>*)plugin->data(plugin->getDataId(config.value("stepsDataId", "STEPS")));
 
         /*md   // The number of piano notes to display on the screen. */
         /*md   numNotes=24 */
@@ -86,6 +106,10 @@ public:
         /*md   // The color of the other notes. */
         /*md   note2Color="#000000" */
         note2Color = draw.getColor(config["note2Color"], note2Color);
+
+        /*md   // The color of the steps. */
+        /*md   stepColor="#000000" */
+        stepColor = draw.getColor(config["stepColor"], stepColor);
 
         /*md md_config_end */
 
@@ -144,6 +168,25 @@ public:
                     color = colSeparatorColor;
 
                 draw.line({ x, y }, { x, y + size.h }, { color });
+            }
+
+            if (steps != NULL) {
+                // Draw MIDI Notes
+                for (const auto& step : *steps) {
+                    if (step.note >= midiStartNote && step.note < midiStartNote + numNotes) {
+                        int x = step.position * stepWidth;
+                        int y = (numNotes - (step.note - midiStartNote) - 1) * stepHeight;
+                        int width = step.len * stepWidth;
+                        int height = stepHeight;
+
+                        draw.filledRect({ x, y }, { width, height }, { stepColor });
+
+                        // if (selectedNote == &step) {
+                        //     g.setColour(juce::Colours::white);
+                        //     g.drawRect(x, y, width, height);
+                        // }
+                    }
+                }
             }
         }
     }
