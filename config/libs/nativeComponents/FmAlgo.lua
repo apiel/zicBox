e0c68b267bc4495e4e4f8420ded24d5e5f3ff335
apiel <alexandre.piel@gmail.com>
2025-02-17 17:16:09 +0100

    fmalgo

---
+++ config/libs/nativeComponents/FmAlgo.lua
@@ -0,0 +1,6 @@
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+local ____exports = {}
+local ____ui = require("config.libs.ui")
+local getJsonComponent = ____ui.getJsonComponent
+____exports.FmAlgo = getJsonComponent("FmAlgo", "libzic_FmAlgoComponent.so")
+return ____exports
