ac10a81de4659be8aa2a53082f2956b43a6dff86
apiel <alexandre.piel@gmail.com>
2025-02-20 22:16:10 +0100

    audio config

--- host/config.h
+++ host/config.h
@@ -1,19 +1,21 @@
-#ifndef _HOST_CONFIG_H_
-#define _HOST_CONFIG_H_
+#pragma once
 
-#include "../helpers/configPlugin.h"
+#include "libs/nlohmann/json.hpp"
+
+#include "helpers/configPlugin.h"
+#include "AudioPluginHandler.h"
 #include "def.h"
-#include "midi.h"
 
 extern "C" void hostScriptCallback(char* key, char* value, const char* filename, std::vector<Var> variables)
 {
-    if (strcmp(key, "print") == 0) {
-        printf(">> LOG: %s\n", value);
+    if (strcmp(key, "AUDIO") == 0) {
+        nlohmann::json config = nlohmann::json::parse(value);
+        AudioPluginHandler::get().config(config);
     } else if (strcmp(key, "MIDIIN") == 0) {
-        loadMidiInput(midiController, value, &midiControllerCallback);
+        AudioPluginHandler::get().loadMidiInput(value);
     } else if (strcmp(key, "MIDIOUT") == 0) {
         // NOTE note used for the moments...
-        loadMidiOutput(midiOut, value);
+        AudioPluginHandler::get().loadMidiOutput(value);
     } else if (strcmp(key, "AUDIO_PLUGIN") == 0) {
         AudioPluginHandler::get().loadPlugin(value);
     } else if (strcmp(key, "AUTO_SAVE") == 0) {
@@ -25,5 +27,3 @@ extern "C" void hostScriptCallback(char* key, char* value, const char* filename,
         AudioPluginHandler::get().config(key, value);
     }
 }
-
-#endif
