e7be8b69664bf48c8023c15285217371c7913096
apiel <alexandre.piel@gmail.com>
2025-02-23 14:56:00 +0100

    json config

--- src/config/pixel/240x320_rpi3A_4enc_11btn/main.tsx
+++ src/config/pixel/240x320_rpi3A_4enc_11btn/main.tsx
@@ -1,11 +1,17 @@
 import { writeFileSync } from 'fs';
+import 'tsconfig-paths/register'; // To solve importedalias
 
+console.log('starting...');
+
+import { stringifyWithCompactedKeys } from '@/libs/stringify';
 import { audio } from './audio';
 import { ui } from './ui';
 
-const output = JSON.stringify({ audio, ...ui }, null, 2);
-
-// console.log(JSON.stringify({ audio, ui }, null, 2));
-// write file to config.json
+// const output = JSON.stringify({ audio, ...ui }, null, 2);
+// const output = stringifyWithLimitedIndentation({ audio, ...ui });
+const output = stringifyWithCompactedKeys({ audio, ...ui });
 
 writeFileSync('config.json', output);
+
+console.log('done');
+
