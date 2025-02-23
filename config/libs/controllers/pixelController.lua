5f0f515fc5cc8ea00b0f76e5c29adcb2b73aaed0
apiel <alexandre.piel@gmail.com>
2025-02-23 13:50:29 +0100

    cleanup config

--- config/libs/controllers/pixelController.lua
+++
@@ -1,8 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local ____core = require("config.libs.core")
-local applyZic = ____core.applyZic
-function ____exports.pixelController(____type)
-    applyZic({{PIXEL_CONTROLLER = ____type}})
-end
-return ____exports
--- config/libs/nativeComponents/Workspaces.lua
+++
@@ -1,6 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local ____ui = require("config.libs.ui")
-local getJsonComponent = ____ui.getJsonComponent
-____exports.Workspaces = getJsonComponent("Workspaces")
-return ____exports
