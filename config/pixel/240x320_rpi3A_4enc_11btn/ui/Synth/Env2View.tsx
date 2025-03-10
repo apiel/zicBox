8393989ca1581ae67937fe6003a80d07a2188f3e
apiel <alexandre.piel@gmail.com>
2025-03-10 21:57:53 +0100

    filter mod

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/Env2View.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/Env2View.tsx
@@ -1,8 +1,10 @@
 import * as React from '@/libs/react';
 
+import { KnobValue } from '@/libs/nativeComponents/KnobValue';
 import { View } from '@/libs/nativeComponents/View';
 import { Common } from '../components/Common';
-import { ColorTrack4, SynthTrack } from '../constants';
+import { ColorTrack4, encTopLeft, encTopRight, SynthTrack } from '../constants';
+import { topLeftKnob, topRightKnob } from '../constantsValue';
 import { TextGridSynth } from './TextGridSynth';
 
 export type Props = {
@@ -12,11 +14,36 @@ export type Props = {
 export function SynthEnv2View({ name }: Props) {
     return (
         <View name={name}>
-            {/* <Adsr
-                bounds={halfFullValues}
+            <KnobValue
                 audioPlugin="Synth"
-                values={['ATTACK_2', 'DECAY_2', 'SUSTAIN_2', 'RELEASE_2']}
-            /> */}
+                param="CUTOFF_MOD"
+                bounds={topLeftKnob}
+                encoderId={encTopLeft}
+                color="tertiary"
+                track={SynthTrack}
+            />
+            <KnobValue
+                audioPlugin="Synth"
+                param="RESONANCE_MOD"
+                bounds={topRightKnob}
+                encoderId={encTopRight}
+                color="primary"
+                track={SynthTrack}
+            />
+            {/* <KnobValue
+                            audioPlugin="Synth" param="FREQUENCY_MOD"
+                            bounds={bottomLeftKnob}
+                            encoderId={encBottomLeft}
+                            color="quaternary"
+                            track={SynthTrack}
+                        /> */}
+            {/* <KnobValue
+                            audioPlugin="Synth" param="OSC_MOD"
+                            bounds={bottomRightKnob}
+                            encoderId={encBottomRight}
+                            color="secondary"
+                            track={SynthTrack}
+                        /> */}
 
             <TextGridSynth selected={'Env2'} viewName={name} />
             <Common selected={'Synth'} track={SynthTrack} selectedBackground={ColorTrack4} />
