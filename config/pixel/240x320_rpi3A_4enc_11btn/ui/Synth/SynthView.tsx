521d5e156a5b0cbdd9fd61b0eb035de29e62cc88
apiel <alexandre.piel@gmail.com>
2025-03-14 21:01:02 +0100

    waveform

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/SynthView.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/SynthView.tsx
@@ -1,15 +1,12 @@
 import * as React from '@/libs/react';
 
+import { GraphEncoder } from '@/libs/nativeComponents/GraphEncoder';
 import { KnobValue } from '@/libs/nativeComponents/KnobValue';
+import { Value } from '@/libs/nativeComponents/Value';
 import { View } from '@/libs/nativeComponents/View';
 import { Common } from '../components/Common';
-import { ColorTrack4, encBottomLeft, encBottomRight, encTopLeft, encTopRight, SynthTrack } from '../constants';
-import {
-    bottomLeftKnob,
-    bottomRightKnob,
-    topLeftKnob,
-    topRightKnob
-} from '../constantsValue';
+import { ColorTrack4, encBottomLeft, encBottomRight, encTopLeft, encTopRight, ScreenWidth, SynthTrack } from '../constants';
+import { graphBottomValues, topLeftKnob, topRightKnob } from '../constantsValue';
 import { TextGridSynth } from './TextGridSynth';
 
 export type Props = {
@@ -20,32 +17,68 @@ export function SynthView({ name }: Props) {
     return (
         <View name={name}>
             <KnobValue
-                audioPlugin="TrackFx" param="VOLUME"
+                audioPlugin="TrackFx"
+                param="VOLUME"
                 bounds={topLeftKnob}
                 encoderId={encTopLeft}
                 color="tertiary"
                 track={SynthTrack}
             />
             <KnobValue
-                audioPlugin="Synth" param="PITCH"
+                audioPlugin="Synth"
+                param="PITCH"
                 bounds={topRightKnob}
                 encoderId={encTopRight}
                 color="primary"
                 track={SynthTrack}
             />
-            <KnobValue
-                audioPlugin="Synth" param="WAVE"
+            {/* <KnobValue
+                audioPlugin="Synth"
+                param="WAVE"
                 bounds={bottomLeftKnob}
                 encoderId={encBottomLeft}
                 color="quaternary"
                 track={SynthTrack}
             />
             <KnobValue
-                audioPlugin="Synth" param="WAVE_EDIT"
+                audioPlugin="Synth"
+                param="WAVE_EDIT"
                 bounds={bottomRightKnob}
                 encoderId={encBottomRight}
                 color="secondary"
                 track={SynthTrack}
+            /> */}
+
+            <GraphEncoder
+                bounds={graphBottomValues}
+                audioPlugin="Synth"
+                dataId="WAVEFORM"
+                renderValuesOnTop={false}
+                values={['WAVE', 'WAVE_EDIT']}
+                track={SynthTrack}
+            />
+            <Value
+                bounds={[0, 240, ScreenWidth / 2 - 2, 22]}
+                audioPlugin="Synth"
+                param="WAVE"
+                track={SynthTrack}
+                fontLabel="PoppinsLight_6"
+                barHeight={1}
+                encoderId={encBottomLeft}
+                alignLeft
+                showLabelOverValue={0}
+            />
+
+            <Value
+                bounds={[ScreenWidth / 2 + 2, 240, ScreenWidth / 2, 22]}
+                audioPlugin="Synth"
+                param="WAVE_EDIT"
+                track={SynthTrack}
+                fontLabel="PoppinsLight_6"
+                barHeight={1}
+                encoderId={encBottomRight}
+                alignLeft
+                showLabelOverValue={0}
             />
 
             <TextGridSynth selected={'Main'} viewName={name} />
