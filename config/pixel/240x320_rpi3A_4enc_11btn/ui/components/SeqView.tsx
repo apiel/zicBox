86004d66952fe5365f7eda77eb28ec6c998358bd
apiel <alexandre.piel@gmail.com>
2025-03-15 22:23:07 +0100

    ux

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/components/SeqView.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/components/SeqView.tsx
@@ -6,7 +6,6 @@ import { rgb } from '@/libs/ui';
 import {
     btnShift,
     btnUp,
-    ColorTrack1,
     ScreenWidth
 } from '../constants';
 import { Common } from './Common';
@@ -31,28 +30,10 @@ export function SeqView({ name, track, synthName, color, textGrid }: Props) {
                     { key: btnShift, action: '.shift' },
                 ]}
                 selectedColor={rgb(196, 21, 152)}
-                stepColor={ColorTrack1}
+                stepColor={color}
                 maxStepLen={1}
             />
 
-            {/* <TextGrid
-                bounds={KeyInfoPosition}
-                rows={[
-                    '&icon::toggle::rect &icon::arrowUp::filled ...',
-                    'Seq./Kick &icon::arrowDown::filled &icon::musicNote::pixelated',
-                    // '!Seq/Bass &icon::arrowDown::filled &icon::musicNote::pixelated',
-                ]}
-                selectedBackground={color}
-                keys={[
-                    { key: btn2, action: 'incGroup:-1' },
-                    { key: btnShift, action: 'contextToggle:254:1:0' },
-
-                    { key: btn5, action: `setView:${synthName}` },
-                    { key: btn6, action: 'incGroup:+1' },
-                    { key: btn7, action: `noteOn:${synthName}:60` },
-                ]}
-                contextValue={0}
-            /> */}
             {textGrid}
             <Common
                 selected={name}
