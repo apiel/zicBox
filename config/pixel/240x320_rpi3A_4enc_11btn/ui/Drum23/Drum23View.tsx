c253c2a334e961fb50146d8a91ffb68d5ecf9a47
apiel <alexandre.piel@gmail.com>
2025-03-15 22:09:31 +0100

    ux

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/Drum23/Drum23View.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/Drum23/Drum23View.tsx
@@ -6,6 +6,7 @@ import { ClickView } from './ClickView';
 import { DistortionView } from './DistortionView';
 import { Drum23MainView } from './Drum23MainView';
 import { FrequencyView } from './FrequencyView';
+import { TextGridDrum23 } from './TextGridDrum23';
 import { WaveformView } from './WaveformView';
 
 export type Props = {
@@ -18,12 +19,43 @@ export function Drum23View({ track, synthName, color }: Props) {
     return (
         <>
             <Drum23MainView name={synthName} track={track} synthName={synthName} color={color} />
-            <DistortionView name={`${synthName}Distortion`} track={track} synthName={synthName} color={color} />
-            <WaveformView name={`${synthName}Waveform`} track={track} synthName={synthName} color={color} />
-            <FrequencyView name={`${synthName}Frequency`} track={track} synthName={synthName} color={color} />
-            <AmpView name={`${synthName}Amplitude`} track={track} synthName={synthName} color={color} />
-            <ClickView name={`${synthName}Click`} track={track} synthName={synthName} color={color} />
-            <SeqView name={`${synthName}Sequencer`} track={track} synthName={synthName} color={color} />
+            <DistortionView
+                name={`${synthName}Distortion`}
+                track={track}
+                synthName={synthName}
+                color={color}
+            />
+            <WaveformView
+                name={`${synthName}Waveform`}
+                track={track}
+                synthName={synthName}
+                color={color}
+            />
+            <FrequencyView
+                name={`${synthName}Frequency`}
+                track={track}
+                synthName={synthName}
+                color={color}
+            />
+            <AmpView
+                name={`${synthName}Amplitude`}
+                track={track}
+                synthName={synthName}
+                color={color}
+            />
+            <ClickView
+                name={`${synthName}Click`}
+                track={track}
+                synthName={synthName}
+                color={color}
+            />
+            <SeqView
+                name={`${synthName}Sequencer`}
+                track={track}
+                synthName={synthName}
+                color={color}
+                textGrid={(<TextGridDrum23 selected={'Seq.'} color={color} synthName={synthName} hideTitle />)}
+            />
         </>
     );
 }
