59207686a19d31883c11cbc417cb274f48cc2e9c
apiel <alexandre.piel@gmail.com>
2025-03-16 14:57:29 +0100

    EffectFilter

--- plugins/audio/EffectFilter.h
+++ plugins/audio/EffectFilter.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "helpers/range.h"
 #include "filter.h"
+#include "helpers/range.h"
 #include "mapping.h"
 
 // #include <math.h>
@@ -14,107 +14,63 @@ EffectFilter is a simple resonant filter.
 */
 class EffectFilter : public Mapping {
 protected:
-    EffectFilterData data;
+    EffectFilterData filter;
 
 public:
     /*md **Values**: */
-    /*md - `CUTOFF` set cutoff. */
-    Val& cutoff = val(50.0, "CUTOFF", { "Cutoff", .unit = "%" }, [&](auto p) { setCutoff(p.value); });
-    /*md - `RESONANCE` set resonance. */
-    Val& resonance = val(0.0, "RESONANCE", { "Resonance", .unit = "%" }, [&](auto p) { setResonance(p.value); });
+    /*md - `FILTER_TYPE` Select filter type.*/
+    enum FilterType {
+        FILTER_OFF,
+        LP,
+        BP,
+        HP,
+        FILTER_COUNT
+    };
+    Val& filterType = val(1, "FILTER_TYPE", { "Filter", VALUE_STRING, .max = EffectFilter::FilterType::FILTER_COUNT - 1 }, [&](auto p) {
+        p.val.setFloat(p.value);
+        if (p.val.get() == EffectFilter::FilterType::FILTER_OFF) {
+            p.val.setString("OFF");
+        } else if (p.val.get() == EffectFilter::FilterType::LP) {
+            p.val.setString("LPF");
+            filter.setType(EffectFilterData::Type::LP);
+        } else if (p.val.get() == EffectFilter::FilterType::BP) {
+            p.val.setString("BPF");
+            filter.setType(EffectFilterData::Type::BP);
+        } else if (p.val.get() == EffectFilter::FilterType::HP) {
+            p.val.setString("HPF");
+            filter.setType(EffectFilterData::Type::HP);
+        }
+        filterCutoff.set(filterCutoff.get());
+        filterResonance.set(filterResonance.get());
+    });
 
-    enum Mode {
-        OFF,
-        LPF,
-        HPF,
-        BPF,
-        MODE_COUNT,
-    } mode
-        = OFF;
-    /*md - `MODE` set filter mode. */
-    Val& mode_value = val(0.0, "MODE", { "Mode", VALUE_STRING, .max = MODE_COUNT - 1, }, [&](auto p) { setMode(p.value); });
+    /*md - `FILTER_CUTOFF` set the filter cutoff frequency.*/
+    Val& filterCutoff = val(0, "FILTER_CUTOFF", { "Cutoff", .unit = "%" }, [&](auto p) {
+        p.val.setFloat(p.value);
+        filter.set(p.val.pct(), filterResonance.pct());
+    });
+
+    /*md - `FILTER_RESONANCE` set the filter resonance.*/
+    Val& filterResonance = val(0, "FILTER_RESONANCE", { "Resonance", .unit = "%" }, [&](auto p) {
+        p.val.setFloat(p.value);
+        filter.setResonance(p.val.pct());
+    });
 
     EffectFilter(AudioPlugin::Props& props, AudioPlugin::Config& config)
         : Mapping(props, config)
     {
     }
 
-    float sample(float inputValue)
+    float process(float input)
     {
-        if (inputValue == 0 || mode == OFF) {
-            return inputValue;
+        if (input == 0 || filterType.get() == EffectFilter::FilterType::FILTER_OFF) {
+            return input;
         }
-
-        data.setSampleData(inputValue);
-
-        if (mode == HPF) {
-            return data.hp;
-        } else if (mode == BPF) {
-            return data.bp;
-        }
-        // LPF
-        return data.lp;
+        return filter.process(input);
     }
 
     void sample(float* buf)
     {
-        buf[track] = sample(buf[track]);
-    }
-
-    std::string getMode(enum Mode _mode)
-    {
-        switch (_mode) {
-        case OFF:
-            return "OFF";
-        case LPF:
-            return "LPF";
-        case HPF:
-            return "HPF";
-        case BPF:
-            return "BPF";
-        default:
-            return "UNKNOWN";
-        }
-    }
-
-    EffectFilter& setCutoff(float value)
-    {
-        cutoff.setFloat(value);
-        cutoff.setString(getMode(mode));
-
-        if (mode == LPF) {
-            data.setRawCutoff(0.85 * cutoff.pct() + 0.1);
-        } else if (mode == BPF) {
-            data.setRawCutoff(0.85 * cutoff.pct() + 0.1);
-        } else { // HPF
-            data.setRawCutoff((0.20 * cutoff.pct()) + 0.00707);
-        }
-
-        return *this;
-    }
-
-    EffectFilter& setResonance(float value)
-    {
-        resonance.setFloat(value);
-        data.setResonance(resonance.pct());
-
-        return *this;
-    };
-
-    EffectFilter& setMode(EffectFilter::Mode _mode)
-    {
-        mode_value.setFloat(_mode);
-        mode = (Mode)mode_value.get();
-        mode_value.setString(getMode(mode));
-        setCutoff(cutoff.get());
-        return *this;
-    };
-
-    EffectFilter& setMode(float value)
-    {
-        mode_value.setFloat(value);
-        mode = (Mode)range((uint8_t)mode_value.get(), 0, (uint8_t)MODE_COUNT);
-        mode_value.setString(getMode(mode));
-        return setMode(mode);
+        buf[track] = process(buf[track]);
     }
 };
