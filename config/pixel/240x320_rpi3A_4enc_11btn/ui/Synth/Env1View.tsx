bd2910b0fbbab9cf6258cb70d9697f3208ac9993
apiel <alexandre.piel@gmail.com>
2025-03-16 16:01:42 +0100

    ux

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/Env1View.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/Synth/Env1View.tsx
@@ -3,8 +3,8 @@ import * as React from '@/libs/react';
 import { KnobValue } from '@/libs/nativeComponents/KnobValue';
 import { View } from '@/libs/nativeComponents/View';
 import { Common } from '../components/Common';
-import { encTopLeft, encTopRight } from '../constants';
-import { topLeftKnob, topRightKnob } from '../constantsValue';
+import { encBottomLeft, encBottomRight, encTopLeft, encTopRight } from '../constants';
+import { bottomLeftKnob, bottomRightKnob, topLeftKnob, topRightKnob } from '../constantsValue';
 import { ViewSelector } from './ViewSelector';
 
 export type Props = {
@@ -18,33 +18,37 @@ export function Env1View({ name, track, synthName, color }: Props) {
     return (
         <View name={name}>
             <KnobValue
-                audioPlugin={synthName} param="ATTACK"
+                audioPlugin={synthName}
+                param="ATTACK"
                 bounds={topLeftKnob}
                 encoderId={encTopLeft}
                 color="tertiary"
                 track={track}
             />
             <KnobValue
-                audioPlugin={synthName} param="RELEASE"
+                audioPlugin={synthName}
+                param="RELEASE"
                 bounds={topRightKnob}
                 encoderId={encTopRight}
                 color="primary"
                 track={track}
             />
-            {/* <KnobValue
-                audioPlugin={synthName} param="FREQUENCY_MOD"
+            <KnobValue
+                audioPlugin="TrackFx"
+                param="FX_TYPE"
                 bounds={bottomLeftKnob}
                 encoderId={encBottomLeft}
                 color="quaternary"
                 track={track}
-            /> */}
-            {/* <KnobValue
-                audioPlugin={synthName} param="OSC_MOD"
+            />
+            <KnobValue
+                audioPlugin="TrackFx"
+                param="FX_AMOUNT"
                 bounds={bottomRightKnob}
                 encoderId={encBottomRight}
                 color="secondary"
                 track={track}
-            /> */}
+            />
 
             <ViewSelector selected={'Env'} viewName={name} synthName={synthName} color={color} />
             <Common track={track} selectedBackground={color} selected={synthName} />
