de5e780bbcb55a8d8978476130a5c2ec13f18d2e
apiel <alexandre.piel@gmail.com>
2025-03-18 18:44:03 +0100

    keys

--- config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/ClipsView.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/ClipsView.tsx
@@ -1,12 +1,14 @@
 import * as React from '@/libs/react';
 
 import { View } from '@/libs/nativeComponents/View';
+import { ShiftedTextGrid } from '../components/ShiftedTextGrid';
 import {
     ColorTrack1,
+    ColorTrackMaster,
     Drum23Track
 } from '../constants';
 import { Clips } from './Clips';
-import { TextGridClips, TextGridClipsShifted } from './TextGrid';
+import { TextGridClips } from './TextGrid';
 import { Drum23Values, MasterValues } from './Values';
 
 export type Props = {
@@ -16,9 +18,6 @@ export type Props = {
 export function ClipsView({ name }: Props) {
     return (
         <View name={name}>
-            <TextGridClips />
-            <TextGridClipsShifted />
-
             <Clips color={ColorTrack1} track={Drum23Track} group={0} title="Kick" group_all={2} />
             {/* <Clips color={ColorTrack2} track={BassTrack} group={1} title="Bass" group_all={2} /> */}
 
@@ -26,6 +25,9 @@ export function ClipsView({ name }: Props) {
             {/* <SampleValues group={1} track={SampleTrack} /> */}
 
             <MasterValues group={2} track={0} />
+
+            <TextGridClips />
+            <ShiftedTextGrid selectedBackground={ColorTrackMaster} selected="Clips" />
         </View>
     );
 }
--- config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/TextGrid.tsx
+++ config/pixel/240x320_rpi3A_4enc_11btn/ui/clipsView/TextGrid.tsx
@@ -1,38 +1,41 @@
 import * as React from '@/libs/react';
 
 import { TextGrid } from '@/libs/tsComponents/TextGrid';
-import { btn1, btn5, btn6, btn7, btnShift, KeyInfoPosition } from '../constants';
+import { btn5, btn7, btnShift, btnUp, KeyInfoPosition } from '../constants';
 
 export function TextGridClips() {
     return (
         <TextGrid
             bounds={KeyInfoPosition}
             rows={[
-                '&icon::toggle::rect &icon::arrowUp::filled ...',
-                '&icon::arrowLeft::filled &icon::arrowDown::filled &icon::arrowRight::filled',
+                '&icon::toggle::rect &icon::arrowUp::filled &empty &empty',
+                '&icon::arrowLeft::filled &icon::arrowDown::filled &icon::arrowRight::filled &empty',
             ]}
             keys={[
-                { key: btnShift, action: 'contextToggle:254:1:0' },
                 { key: btn5, action: 'incGroup:-1' },
                 { key: btn7, action: 'incGroup:+1' },
+
+                { key: btnUp, action: 'contextToggle:253:1:0' },
+                // { key: btnDown, action: 'contextToggleOnRelease:252:1:0' },
+                { key: btnShift, action: 'contextToggle:254:1:0' },
             ]}
             contextValue={[0]}
         />
     );
 }
 
-export function TextGridClipsShifted() {
-    return (
-        <TextGrid
-            bounds={KeyInfoPosition}
-            rows={['Menu Save ^...', '&icon::trash Back &icon::play::filled']}
-            keys={[
-                { key: btn6, action: 'setView:Kick' },
-                { key: btn7, action: 'playPause' },
-                { key: btnShift, action: 'contextToggle:254:1:0' },
-                { key: btn1, action: 'setView:Menu' },
-            ]}
-            contextValue={[1]}
-        />
-    );
-}
+// export function TextGridClipsShifted() {
+//     return (
+//         <TextGrid
+//             bounds={KeyInfoPosition}
+//             rows={['Menu Save ^...', '&icon::trash Back &icon::play::filled']}
+//             keys={[
+//                 { key: btn6, action: 'setView:Kick' },
+//                 { key: btn7, action: 'playPause' },
+//                 { key: btnShift, action: 'contextToggle:254:1:0' },
+//                 { key: btn1, action: 'setView:Menu' },
+//             ]}
+//             contextValue={[1]}
+//         />
+//     );
+// }
