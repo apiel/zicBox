6041a6ec0a9c951d6288b056dda96af1065304fa
apiel <alexandre.piel@gmail.com>
2025-03-21 21:46:21 +0100

    clips

--- config/libs/tsComponents/TextGrid.tsx
+++ config/libs/tsComponents/TextGrid.tsx
@@ -14,6 +14,7 @@ export function TextGrid({
     selectedBackground,
     bgColor = 'background',
     textColor,
+    shiftedTextColor,
 }: {
     bounds: Bounds;
     selected?: string;
@@ -23,6 +24,7 @@ export function TextGrid({
     selectedBackground?: string;
     bgColor?: string;
     textColor?: string;
+    shiftedTextColor?: string;
 }) {
     if (selected) {
         for (let i = 0; i < rows.length; i++) {
@@ -51,6 +53,7 @@ export function TextGrid({
                 context={context}
                 bgColor={bgColor}
                 textColor={textColor}
+                shiftedTextColor={shiftedTextColor}
             />
         </>
     );
