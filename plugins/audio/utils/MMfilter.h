55d7b9a9ede7e633f28f03fb473035e8669f1c57
apiel <alexandre.piel@gmail.com>
2025-03-29 12:10:55 +0100

    filter

---
+++ plugins/audio/utils/MMfilter.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "filter.h"
+
+class MMfilter {
+protected:
+    EffectFilterData filter;
+    float mix = 0.0;
+
+public:
+    void setResonance(float amount) { filter.resonance = amount; }
+
+    void setCutoff(float amount)
+    {
+        if (amount > 0.0) {
+            filter.setType(EffectFilterData::Type::HP);
+            filter.setHp(amount, filter.resonance);
+            if (amount < 0.5) {
+                mix = amount * 2;
+            }
+        } else {
+            filter.setType(EffectFilterData::Type::LP);
+            filter.setLp(-amount, filter.resonance);
+            if (amount > -0.20) {
+                mix = -amount * 5;
+            }
+            // if (amount > -0.10) {
+            //     mix = -amount * 10;
+            // }
+        }
+    }
+
+    float process(float inputValue)
+    {
+        if (inputValue == 0 || mix == 0.0) {
+            return inputValue;
+        }
+
+        if (mix < 1.0) {
+            return inputValue * (1.0 - mix) + filter.process(inputValue) * mix;
+        }
+
+        return filter.process(inputValue);
+    }
+};
