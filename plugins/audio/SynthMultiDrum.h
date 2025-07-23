65ef961749059f23f9f2ebf7d086ba38c72c3383
apiel <alexandre.piel@gmail.com>
2025-07-23 16:39:40 +0200

    BassEngine

--- plugins/audio/SynthMultiDrum.h
+++ plugins/audio/SynthMultiDrum.h
@@ -30,16 +30,16 @@ public:
     /*md **Values**: */
 
     Val* values[10] = {
-        &val(0.0f, "VAL_1", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[0]->set(p.val.get()); }),
-        &val(0.0f, "VAL_2", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[1]->set(p.val.get()); }),
-        &val(0.0f, "VAL_3", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[2]->set(p.val.get()); }),
-        &val(0.0f, "VAL_4", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[3]->set(p.val.get()); }),
-        &val(0.0f, "VAL_5", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[4]->set(p.val.get()); }),
-        &val(0.0f, "VAL_6", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[5]->set(p.val.get()); }),
-        &val(0.0f, "VAL_7", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[6]->set(p.val.get()); }),
-        &val(0.0f, "VAL_8", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[7]->set(p.val.get()); }),
-        &val(0.0f, "VAL_9", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[8]->set(p.val.get()); }),
-        &val(0.0f, "VAL_10", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[9]->set(p.val.get()); }),
+        &val(0.0f, "VAL_1", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[0]->set(p.val.get()); p.val.setString(drumEngine->mapping[0]->string()); }),
+        &val(0.0f, "VAL_2", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[1]->set(p.val.get()); p.val.setString(drumEngine->mapping[1]->string()); }),
+        &val(0.0f, "VAL_3", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[2]->set(p.val.get()); p.val.setString(drumEngine->mapping[2]->string()); }),
+        &val(0.0f, "VAL_4", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[3]->set(p.val.get()); p.val.setString(drumEngine->mapping[3]->string()); }),
+        &val(0.0f, "VAL_5", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[4]->set(p.val.get()); p.val.setString(drumEngine->mapping[4]->string()); }),
+        &val(0.0f, "VAL_6", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[5]->set(p.val.get()); p.val.setString(drumEngine->mapping[5]->string()); }),
+        &val(0.0f, "VAL_7", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[6]->set(p.val.get()); p.val.setString(drumEngine->mapping[6]->string()); }),
+        &val(0.0f, "VAL_8", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[7]->set(p.val.get()); p.val.setString(drumEngine->mapping[7]->string()); }),
+        &val(0.0f, "VAL_9", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[8]->set(p.val.get()); p.val.setString(drumEngine->mapping[8]->string()); }),
+        &val(0.0f, "VAL_10", {}, [&](auto p) { p.val.setFloat(p.value); drumEngine->mapping[9]->set(p.val.get()); p.val.setString(drumEngine->mapping[9]->string()); }),
     };
 
     /*md - `ENGINE` select the drum engine. */
