522ccfdf6a4fe09499fea613451eb450e07908ac
apiel <alexandre.piel@gmail.com>
2025-11-15 16:01:35 +0100

    desktop

--- draw/drawWithSDL.h
+++ draw/drawWithSDL.h
@@ -2,123 +2,28 @@
 
 #include <SDL2/SDL.h>
 
-#include "./draw.h"
+#include "draw/drawDesktop.h"
 #include "helpers/getTicks.h"
-#include "motion.h"
-#include "plugins/components/EventInterface.h"
 
-#ifndef MAX_SCREEN_MOTION
-// The current display only support 5 touch point
-#define MAX_SCREEN_MOTION 5
-#endif
-
-class DrawWithSDL : public Draw {
+class DrawWithSDL : public DrawDesktop {
 protected:
     SDL_Texture* texture = NULL;
     SDL_Renderer* renderer = NULL;
     SDL_Window* window = NULL;
 
-    int windowX = SDL_WINDOWPOS_CENTERED;
-    int windowY = SDL_WINDOWPOS_CENTERED;
-
-    // bool needRendering = false;
-
-    Motion motions[MAX_SCREEN_MOTION];
-
-#if SDL_MINOR_VERSION <= 24
-    uint8_t emulateEncoderId = 0;
-#endif
-
-    MotionInterface* getMotion(int id)
-    {
-        for (int i = 0; i < MAX_SCREEN_MOTION; ++i) {
-            if (motions[i].id == id) {
-                return &motions[i];
-            }
-        }
-        return NULL;
-    }
-
-    MotionInterface* getOldestMotion()
-    {
-        MotionInterface* oldest = &motions[0];
-        for (int i = 1; i < MAX_SCREEN_MOTION; ++i) {
-            if (motions[i].id < oldest->id) {
-                oldest = &motions[i];
-            }
-        }
-        return oldest;
-    }
-
-    void handleMotion(EventInterface* view, int x, int y, int id)
-    {
-        if (id < 0) {
-            return;
-        }
-
-#ifndef IS_RPI
-#if SDL_MINOR_VERSION <= 24
-        emulateEncoderId = getEmulatedEncoderId(x, y);
-#endif
-#endif
-
-        MotionInterface* motion = getMotion(id);
-        if (motion) {
-            motion->move(x, y);
-            view->onMotion(*motion);
-        }
-    }
-
-    void handleMotionUp(EventInterface* view, int x, int y, int id)
-    {
-        if (id < 0) {
-            return;
-        }
-
-        MotionInterface* motion = getMotion(id);
-        if (motion) {
-            motion->move(x, y);
-            view->onMotionRelease(*motion);
-            motion->setId(-1);
-        }
-    }
-
-    void handleMotionDown(EventInterface* view, int x, int y, int id)
-    {
-        if (id < 0) {
-            return;
-        }
-
-        MotionInterface* motion = getOldestMotion();
-        motion->init(id, x, y);
-        view->onMotion(*motion);
-    }
-
-    std::vector<Rect> zoneEncoders = {};
-    uint8_t getEmulatedEncoderId(int32_t x, int32_t y)
-    {
-        for (uint8_t i = 0; i < zoneEncoders.size(); i++) {
-            // if (x >= zoneEncoders[i].position.x && x < zoneEncoders[i].position.x + zoneEncoders[i].size.w &&
-            //     y >= zoneEncoders[i].position.y && y < zoneEncoders[i].position.y + zoneEncoders[i].size.h) {
-            if (inRect(zoneEncoders[i], { x, y })) {
-                return i;
-            }
-        }
-
-        return 255;
-    }
-
     void emulateEncoder(EventInterface* view, SDL_MouseWheelEvent wheel)
     {
 #if SDL_MINOR_VERSION > 24
         uint8_t emulateEncoderId = getEmulatedEncoderId(wheel.mouseX, wheel.mouseY);
+#else
+        uint8_t emulateEncoderId = 0;
 #endif
         view->onEncoder(emulateEncoderId, wheel.y, getTicks());
     }
 
 public:
     DrawWithSDL(Styles& styles)
-        : Draw(styles)
+        : DrawDesktop(styles)
     {
     }
 
