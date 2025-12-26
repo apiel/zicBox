dbf9e2fb7f559dbb97d655a4011f93b6b9fb0c49
apiel <alexandre.piel@gmail.com>
2025-12-26 15:20:58 +0100

    xy

--- config/xy/ui/MultiSynth/MultiSynthLayout.tsx
+++ config/xy/ui/MultiSynth/MultiSynthLayout.tsx
@@ -6,21 +6,55 @@ import { Layout } from '../components/Layout';
 import { ShiftLayout, unshiftVisibilityContext } from '../components/ShiftLayout';
 import { Track } from '../components/Track';
 import { Val } from '../components/Val';
-import {
-    enc10mini,
-    enc11mini,
-    enc12mini,
-    enc1mini,
-    enc2mini,
-    enc3mini,
-    enc4mini,
-    enc5mini,
-    enc6mini,
-    enc7mini,
-    enc8mini,
-    enc9mini,
-    moveUp
-} from '../constantsValue';
+import { ScreenWidth } from '../constants';
+
+const top = 40;
+const rowHeight = 60;
+const colNum = 4;
+const valHeight = 30;
+const margin = 4;
+const width = ScreenWidth / colNum - margin;
+
+export type ValGraphProps = {
+    track: number;
+    synthName: string;
+    color: string;
+    fillColor: string;
+    isActive: boolean;
+    param: string;
+    row: number;
+    col: number;
+};
+
+function ValGraph({ track, synthName, color, fillColor, isActive, param, row, col }: ValGraphProps) {
+    const encoderId = (col % colNum) + 1;
+
+    const graphBounds = [col * (width + margin), top + row * rowHeight, width, 40];
+    const bounds = [graphBounds[0], graphBounds[1] + graphBounds[3] / 2 + 4, graphBounds[2], valHeight];
+
+    return (
+        <>
+            <GraphValue
+                audioPlugin={synthName}
+                param={param}
+                outlineColor={color}
+                fillColor={fillColor}
+                track={track}
+                visibilityContext={[unshiftVisibilityContext]} // always visible now
+                extendEncoderIdArea={encoderId}
+                bounds={graphBounds}
+            />
+            <Val
+                encoderId={encoderId}
+                bounds={bounds}
+                audioPlugin={synthName}
+                param={param}
+                track={track}
+                color={isActive ? color : undefined}
+            />
+        </>
+    );
+}
 
 export type Props = {
     name: string;
@@ -30,56 +64,29 @@ export type Props = {
     title: string;
 };
 
-const graphHeight = 40;
-function getEnc(encVal: { encoderId: number; bounds: number[] }, isOriginalPos: boolean) {
-    return isOriginalPos ? encVal : moveUp(encVal, 30);
-}
-
-function ValGraph({
-    track,
-    synthName,
-    color,
-    fillColor,
-    up,
-    isActive,
-    param,
-    enc,
-}: {
-    track: number;
-    synthName: string;
-    color: string;
-    fillColor: string;
-    up: boolean;
-    isActive: boolean;
-    param: string;
-    enc: { encoderId: number; bounds: number[] };
-}) {
-    if (!up) {
-        enc = {...moveUp(enc, 30)};
-    }
-    return (
-        <>
-            {isActive && (
-                <GraphValue
-                    audioPlugin={synthName}
-                    param={param}
-                    outlineColor={color}
-                    fillColor={fillColor}
-                    track={track}
-                    visibilityContext={[unshiftVisibilityContext]}
-                    extendEncoderIdArea={enc.encoderId}
-                    bounds={[enc.bounds[0], enc.bounds[1] - 24, enc.bounds[2], graphHeight]}
-                />
-            )}
-            <Val {...enc} audioPlugin={synthName} param={param} track={track} color={isActive ? color : undefined} />
-        </>
-    );
-}
-
 export function MultiSynthLayout({ name, track, synthName, color, title }: Props) {
     const isPage1 = !name.includes(':page');
     const isPage2 = name.includes(':page2');
     const isPage3 = name.includes(':page3');
+
+    // Prepare all values in a grid
+    const valConfigs = [
+        { param: 'VAL_1', pageActive: isPage1, color: 'secondary', fillColor: '#ad6565' },
+        { param: 'VAL_2', pageActive: isPage1, color: 'quaternary', fillColor: '#c2af6b' },
+        { param: 'VAL_3', pageActive: isPage1, color: 'tertiary', fillColor: '#235e3e' },
+        { param: 'VAL_4', pageActive: isPage1, color: 'primary', fillColor: '#315c79' },
+
+        { param: 'VAL_5', pageActive: isPage2, color: 'secondary', fillColor: '#ad6565' },
+        { param: 'VAL_6', pageActive: isPage2, color: 'quaternary', fillColor: '#c2af6b' },
+        { param: 'VAL_7', pageActive: isPage2, color: 'tertiary', fillColor: '#235e3e' },
+        { param: 'VAL_8', pageActive: isPage2, color: 'primary', fillColor: '#315c79' },
+
+        { param: 'VAL_9', pageActive: isPage3, color: 'secondary', fillColor: '#ad6565' },
+        { param: 'VAL_10', pageActive: isPage3, color: 'quaternary', fillColor: '#c2af6b' },
+        { param: 'VAL_11', pageActive: isPage3, color: 'tertiary', fillColor: '#235e3e' },
+        { param: 'VAL_12', pageActive: isPage3, color: 'primary', fillColor: '#315c79' },
+    ];
+
     return (
         <Layout
             viewName={name}
@@ -101,126 +108,22 @@ export function MultiSynthLayout({ name, track, synthName, color, title }: Props
 
                     <StringVal audioPlugin={synthName} param="ENGINE" bounds={[90, 3, 60, 20]} fontLabel="PoppinsLight_12" />
 
-                    <Val
-                        {...getEnc(enc1mini, isPage1)}
-                        audioPlugin={synthName}
-                        param="VAL_1"
-                        track={track}
-                        color={isPage1 ? 'secondary' : undefined}
-                    />
-
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="quaternary"
-                        fillColor="#c2af6b"
-                        up={isPage1}
-                        isActive={isPage1}
-                        enc={enc2mini}
-                        param="VAL_2"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="tertiary"
-                        fillColor="#235e3e"
-                        up={isPage1}
-                        isActive={isPage1}
-                        enc={enc3mini}
-                        param="VAL_3"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="primary"
-                        fillColor="#315c79"
-                        up={isPage1}
-                        isActive={isPage1}
-                        enc={enc4mini}
-                        param="VAL_4"
-                    />
-
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="secondary"
-                        fillColor="#ad6565ff"
-                        up={isPage1 || isPage2}
-                        isActive={isPage2}
-                        enc={enc5mini}
-                        param="VAL_5"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="quaternary"
-                        fillColor="#c2af6b"
-                        up={isPage1 || isPage2}
-                        isActive={isPage2}
-                        enc={enc6mini}
-                        param="VAL_6"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="tertiary"
-                        fillColor="#235e3e"
-                        up={isPage1 || isPage2}
-                        isActive={isPage2}
-                        enc={enc7mini}
-                        param="VAL_7"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="primary"
-                        fillColor="#315c79"
-                        up={isPage1 || isPage2}
-                        isActive={isPage2}
-                        enc={enc8mini}
-                        param="VAL_8"
-                    />
-
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="secondary"
-                        fillColor="#ad6565ff"
-                        up
-                        isActive={isPage3}
-                        enc={enc9mini}
-                        param="VAL_9"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="quaternary"
-                        fillColor="#c2af6b"
-                        up
-                        isActive={isPage3}
-                        enc={enc10mini}
-                        param="VAL_10"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="tertiary"
-                        fillColor="#235e3e"
-                        up
-                        isActive={isPage3}
-                        enc={enc11mini}
-                        param="VAL_11"
-                    />
-                    <ValGraph
-                        track={track}
-                        synthName={synthName}
-                        color="primary"
-                        fillColor="#315c79"
-                        up
-                        isActive={isPage3}
-                        enc={enc12mini}
-                        param="VAL_12"
-                    />
+                    {valConfigs.map((v, index) => {
+                        const row = Math.floor(index / colNum);
+                        const col = index % colNum;
+                        return (
+                            <ValGraph
+                                track={track}
+                                synthName={synthName}
+                                color={v.color}
+                                fillColor={v.fillColor}
+                                isActive={v.pageActive}
+                                param={v.param}
+                                row={row}
+                                col={col}
+                            />
+                        );
+                    })}
 
                     <Track synthName={synthName} viewName={name} track={track} color={color} />
                 </>
