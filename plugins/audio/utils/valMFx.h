9914930082ded2b80f8e5fafafbb8ee054179499
apiel <alexandre.piel@gmail.com>
2026-02-21 19:45:04 +0100

    mfx

--- plugins/audio/utils/valMFx.h
+++ plugins/audio/utils/valMFx.h
@@ -3,15 +3,6 @@
 #include "plugins/audio/mapping.h"
 #include "audio/MFx.h"
 
-// auto valMFx(MFx& fx) 
-// {
-//     return [&fx](Val::CallbackProps& p) {
-//         p.val.setFloat(p.value);
-//         fx.setEffect(static_cast<int>(p.value));
-//         p.val.setString(fx.getEffectName());
-//     };
-// }
-
 inline Val::CallbackFn valMFx(MFx& fx) 
 {
     return [&fx](Val::CallbackProps p) {
@@ -20,32 +11,3 @@ inline Val::CallbackFn valMFx(MFx& fx)
         p.val.setString(fx.getEffectName());
     };
 }
-
-// inline Val::CallbackFn valMFx(MFx& fx) 
-// {
-//     auto callback = [&fx](Val::CallbackProps& p) {
-//         p.val.setFloat(p.value);
-//         fx.setEffect(static_cast<int>(p.value));
-//         p.val.setString(fx.getEffectName());
-//     };
-//     return callback;
-// }
-
-// inline Val::CallbackFn valMFx(MFx& fx) 
-// {
-//     // Remove the '&' from CallbackProps
-//     return [&fx](Val::CallbackProps p) {
-//         p.val.setFloat(p.value);
-//         fx.setEffect(static_cast<int>(p.value));
-//         p.val.setString(fx.getEffectName());
-//     };
-// }
-
-// inline Val::CallbackFn valMFx(MFx& fx) 
-// {
-//     return Val::CallbackFn([&fx](auto& p) {
-//         p.val.setFloat(p.value);
-//         fx.setEffect(static_cast<int>(p.value));
-//         p.val.setString(fx.getEffectName());
-//     });
-// }
