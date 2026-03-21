5eb1e802b3847747dbd8aa04e54633fda5d9d28f
apiel <alexandre.piel@gmail.com>
2026-03-21 21:28:59 +0100

    zic23

--- zic23/studio.h
+++ zic23/studio.h
@@ -13,7 +13,6 @@
 #include "audio/engines/DrumSnare23.h"
 #include "audio/engines/Synth23.h"
 #include "audio/engines/SynthFm23.h"
-#include "audio/engines/SynthWavetable23.h"
 #include "draw/draw.h"
 #include "helpers/random.h"
 #include "zic23/generator.h"
@@ -100,8 +99,8 @@ public:
         tracks.push_back(std::make_unique<Track>(TRACK_TYPE_DRUM, std::make_unique<DrumSnare23>(SAMPLE_RATE, createFxBuffer()), 0.7f, palette[i++], Generator::generateSnare));
         tracks.push_back(std::make_unique<Track>(TRACK_TYPE_DRUM, std::make_unique<DrumHiClap23>(SAMPLE_RATE, createFxBuffer()), 0.7f, palette[i++], Generator::generateClap));
         tracks.push_back(std::make_unique<Track>(TRACK_TYPE_SYNTH, std::make_unique<Synth23>(SAMPLE_RATE, createFxBuffer(), createFxBuffer()), 0.7f, palette[i++], Generator::generateBass));
+        tracks.push_back(std::make_unique<Track>(TRACK_TYPE_SYNTH, std::make_unique<Synth23>(SAMPLE_RATE, createFxBuffer(), createFxBuffer()), 0.7f, palette[i++], Generator::generateBass));
         tracks.push_back(std::make_unique<Track>(TRACK_TYPE_SYNTH, std::make_unique<SynthFm23>(SAMPLE_RATE, createFxBuffer(), createFxBuffer()), 0.7f, palette[i++], Generator::generateBass));
-        tracks.push_back(std::make_unique<Track>(TRACK_TYPE_SYNTH, std::make_unique<SynthWavetable23>(SAMPLE_RATE, createFxBuffer(), createFxBuffer()), 0.7f, palette[i++], Generator::generateBass));
         updateClock();
     }
 
