f84b0be9a89fdfaef06a90d7b6388d00c3bbd020
apiel <alexandre.piel@gmail.com>
2026-06-17 21:26:24 +0200

    zicXYv2

--- zicXYv2/uiTopBar.h
+++ zicXYv2/uiTopBar.h
@@ -14,6 +14,25 @@ uint32_t lastBpmTick = 0;
 int btnH = 16;
 int height = btnH * 2 + 2;
 
+void drawTracks(Draw& d, int y, int btnW, int halfBtnW, Icon& icon)
+{
+    int currentX = 0;
+
+    for (int i = 0; i < MAX_TRACKS; i++) {
+        if (studio.tracks[i] == nullptr) break;
+        Track& trk = *studio.tracks[i];
+        trackRect[i] = { { currentX, y }, { btnW, btnH } };
+        d.filledRect(trackRect[i].position, trackRect[i].size, { .color = { 50, 50, 50 } });
+        d.textCentered({ trackRect[i].position.x + halfBtnW, trackRect[i].position.y + 4 }, std::to_string(i + 1), 8, { .color = trk.themeColor, .font = &PoppinsLight_8 });
+        if (studio.selTrack == i && studio.currentView == ViewTrack) d.filledRect(trackRect[i].position, { trackRect[i].size.w, 1 }, { .color = trk.themeColor });
+        if (trk.isMuted) {
+            icon.mute({ trackRect[i].position.x + halfBtnW + 7, trackRect[i].position.y + 7 }, { 8, 8 }, { 155, 155, 155 }, true);
+        }
+        currentX += trackRect[i].size.w + 2;
+    }
+
+}
+
 bool draw(Draw& d, const int winW, bool needFullRedraw, int& currentY)
 {
     int y = currentY;
@@ -55,22 +74,7 @@ bool draw(Draw& d, const int winW, bool needFullRedraw, int& currentY)
     d.textRight({ winW - 10, y + 4 }, bss.str(), 8, { .color = { 255, 255, 255 }, .font = &PoppinsLight_8 });
 
     y += btnH + 2;
-    currentX = 0;
-
-    for (int i = 0; i < MAX_TRACKS; i++) {
-        if (studio.tracks[i] == nullptr) break;
-        Track& trk = *studio.tracks[i];
-        trackRect[i] = { { currentX, y }, { btnW, btnH } };
-        d.filledRect(trackRect[i].position, trackRect[i].size, { .color = { 50, 50, 50 } });
-        d.textCentered({ trackRect[i].position.x + halfBtnW, trackRect[i].position.y + 4 }, std::to_string(i + 1), 8, { .color = trk.themeColor, .font = &PoppinsLight_8 });
-        if (studio.selTrack == i && studio.currentView == ViewTrack) d.filledRect(trackRect[i].position, { trackRect[i].size.w, 1 }, { .color = trk.themeColor });
-        if (trk.isMuted) {
-            icon.mute({ trackRect[i].position.x + halfBtnW + 7, trackRect[i].position.y + 7 }, { 8, 8 }, { 155, 155, 155 }, true);
-        }
-        currentX += trackRect[i].size.w + 2;
-    }
-
-    y += btnH + 2;
+    drawTracks(d, y, btnW, halfBtnW, icon);
 
     return true;
 }
