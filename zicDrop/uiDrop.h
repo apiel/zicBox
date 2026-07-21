d897103bbf1a8745738c8daa1cb1854c0758d762
apiel <alexandre.piel@gmail.com>
2026-07-21 21:26:54 +0200

    drop

--- zicDrop/uiDrop.h
+++ zicDrop/uiDrop.h
@@ -58,13 +58,8 @@ public:
             "follow", "1", "2", "2-off", "4", "4-off", "4-rumble", "8", "8-off", "8-rumble", "16", "16-off", "16-rumble", "32", "32-off", "32-rumble"
         };
 
-        std::vector<std::string> repeatDisplayStrings = {
-            "None", "On Press", "On Release", "Both"
-        };
-
         knobs.push_back({"BASE PITCH", &audio.synthBasePitch.value, 24.0f, 72.0f, 70.0f, 210.0f, 20.0f, false, 0.0f, 0.0f, "", SECTION_BRAIN});
         knobs.push_back({"TRIG STEP", &brain.synthTriggerStep, 0.0f, 15.0f, 170.0f, 210.0f, 20.0f, false, 0.0f, 0.0f, "", SECTION_BRAIN, 0, stepDisplayStrings});
-        knobs.push_back({"SPACE REPEAT", &brain.spacebarRepeat, 0.0f, 3.0f, 270.0f, 210.0f, 20.0f, false, 0.0f, 0.0f, "", SECTION_BRAIN, 0, repeatDisplayStrings});
 
         // 2. Kick knobs
         knobs.push_back({"TUNE", &audio.kickTune.value, 30.0f, 150.0f, 420.0f, 100.0f, 22.0f, false, 0.0f, 0.0f, " Hz", SECTION_KICK});
