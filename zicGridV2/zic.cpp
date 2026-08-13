f1c7a9fbda0e5b5ffdc0706c95b4ae0874621967
apiel <alexandre.piel@gmail.com>
2026-08-13 21:05:02 +0200

    grid

--- zicGridV2/zic.cpp
+++ zicGridV2/zic.cpp
@@ -69,13 +69,12 @@ int main(int argc, char* argv[])
     }
 
     std::cout << "[System] Zic Grid V2 clean shutdown completed." << std::endl;
-
-    if (system_halt_requested) {
 #if defined(IS_RPI)
+    if (system_halt_requested) {
         std::cout << "Shutting down RPi..." << std::endl;
         int exitCode = std::system("halt");
-        (void)exitCode;
-#endif
+        (void)exitCode;    
     }
+#endif
     return 0;
 }
