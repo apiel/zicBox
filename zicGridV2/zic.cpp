74224a201f2104367365c253f801a833ec64f619
apiel <alexandre.piel@gmail.com>
2026-08-13 21:54:32 +0200

    grid

--- zicGridV2/zic.cpp
+++ zicGridV2/zic.cpp
@@ -69,12 +69,15 @@ int main(int argc, char* argv[])
     }
 
     std::cout << "[System] Zic Grid V2 clean shutdown completed." << std::endl;
-#if defined(IS_RPI)
+
     if (system_halt_requested) {
+#if defined(IS_RPI)
         std::cout << "Shutting down RPi..." << std::endl;
-        int exitCode = std::system("halt");
-        (void)exitCode;    
-    }
+        int exitCode = std::system("sync && (halt || /sbin/halt || /bin/halt)");
+        std::cout << "[System] halt exit code: " << exitCode << std::endl;
+#else
+        std::cout << "[System] IS_RPI not defined, skipping halt command." << std::endl;
 #endif
+    }
     return 0;
 }
